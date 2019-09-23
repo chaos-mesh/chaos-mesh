3aac47656dcc63fa4126468d7c45d4078787508f
cwen0 <cwenyin0@gmail.com>
2019-09-23 19:43:55 +0800

    support more modes fro pod-kill action
    Signed-off-by: cwen0 <cwenyin0@gmail.com>

--- pkg/manager/selector.go
+++ pkg/manager/selector.go
@@ -15,6 +15,7 @@ package manager
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
 
 	"github.com/pingcap/chaos-operator/pkg/apis/pingcap.com/v1alpha1"
@@ -87,6 +88,35 @@ func SelectPods(
 	pods = filterByPhase(pods, v1.PodRunning)
 
 	return pods, nil
+}
+
+// RandomFixedIndexes returns the `count` random indexes between `start` and `end`.
+func RandomFixedIndexes(start, end, count int) []int {
+	var indexes []int
+	m := make(map[int]int, count)
+
+	if count > end-start {
+		for i := start; i < end; i++ {
+			indexes = append(indexes, i)
+		}
+
+		return indexes
+	}
+
+	for i := 0; i < count; {
+		index := rand.Intn(end-start) + start
+
+		_, exist := m[index]
+		if exist {
+			continue
+		}
+
+		m[index] = index
+		indexes = append(indexes, index)
+		i++
+	}
+
+	return indexes
 }
 
 // filterByAnnotations filters a list of pods by a given annotation selector.
