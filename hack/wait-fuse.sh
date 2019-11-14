dd557c023612805d0872f071f521ba935c440df6
cwen0 <cwenyin0@gmail.com>
2019-11-14 20:43:39 +0800

    update check-fuse scripts
    Signed-off-by: cwen0 <cwenyin0@gmail.com>

--- hack/wait-fuse.sh
+++ hack/wait-fuse.sh
@@ -3,51 +3,57 @@
 set -eo pipefail
 #set -x # For debugging
 
-TARGET_IP="127.0.0.1"
-TARGET_PORT="65534"
+FUSE_PID_FILE="/tmp/fuse/pid"
+DELAY=5
+RETRY=60
 
 usage() {
 cat << EOF
 USAGE: $0 [-a <host>] [-p <port>]
 Waiting for fuse server ready
 OPTIONS:
-   -h <host>            Set the target host
-   -p <port>            Set the target port
+   -h                   Show this message
+   -f <host>            Set the target file
+   -d <delay>           Set the delay time
+   -r <retry>           Set the retry count
 EXAMPLES:
-   wait-fuse.sh -h 127.0.0.1 -p 65534
+   wait-fuse.sh -f /tmp/fuse/pid -d 5 -r 60
 EOF
 }
 
-while getopts h:p: o
+while getopts h:f:d:r: o
 do	case "$o" in
-	h)      TARGET_IP=$OPTARG;;
-	p)      TARGET_PORT=$OPTARG;;
+	h)      usage
+            exit 1;;
+	f)      FUSE_PID_FILE=$OPTARG;;
+	d)      DELAY=$OPTARG;;
+	r)      RETRY=$OPTARG;;
 	[?])	usage
 		exit 1;;
 	esac
 done
 
+
 wait_for() {
-    local host=${1?ERROR: A host is reqiured}
-    local port=${2?ERROR: A port is reqiured}
-    local delay=${3:-2} # Default is 2 seconds
-    local retry=${4:-100}
+    local file=$1
+    local delay=${2:-5} # Default is 2 seconds
+    local retry=${3:-50}
+    local coord_done=
     local count=0
-    while true; do
-        local telnet_count=`echo "exit" | telnet $host $port | grep -v "Connection refused" | grep "Connected to" | grep -v grep | wc -l`
-        if [ ${telnet_count} -eq 1 ] ; then
+
+    while [ -z "$coord_done" ]; do
+        if [ -f "${file}" ]; then
             sleep 2
-            break
-        elif [ ${count} -eq ${retry} ] ; then
-            each "Waiting for Fuse server ready timeout"
+            coord_done=1
+        elif [ ${count} -eq ${retry} ]; then
+            echo "waiting for fuse server ready timeout"
             exit 1
         else
-            echo "Cannot connect to Fuse server at ${host}:${port}, retry..."
+            echo "fuse server not running, ${file} not found, retry..."
+            sleep ${delay}
+            let count=$count+1
         fi
-        sleep ${delay}
-        let count=$count+1
     done
 }
 
-
-wait_for ${TARGET_IP} ${TARGET_PORT} 5 60
+wait_for ${FUSE_PID_FILE} ${DELAY} ${RETRY}
