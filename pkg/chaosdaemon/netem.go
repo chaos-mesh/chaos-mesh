b95e9c0617809ff76cd17a2bb13d5bb5fb577881
cwen0 <cwenyin0@gmail.com>
2019-11-25 14:13:55 +0800

    fix get network ns path
    Signed-off-by: cwen0 <cwenyin0@gmail.com>

--- pkg/chaosdaemon/netem.go
+++ pkg/chaosdaemon/netem.go
@@ -12,15 +12,11 @@ import (
 	pb "github.com/pingcap/chaos-operator/pkg/chaosdaemon/pb"
 )
 
-const (
-	defaultProcPrefix = "/mnt/proc"
-)
-
 // Apply applies a netem on eth0 in pid related namespace
 func Apply(netem *pb.Netem, pid uint32) error {
 	glog.Infof("Apply netem on PID: %d", pid)
-	nsPath := fmt.Sprintf("%s/%d/ns/net", defaultProcPrefix, pid)
-	ns, err := netns.GetFromPath(nsPath)
+
+	ns, err := netns.GetFromPid(int(pid))
 	if err != nil {
 		glog.Errorf("error while finding network namespace %s", nsPath)
 		return errors.Trace(err)
