ab85cbebc4014b313f243d86cc9947cf57159542
cwen0 <cwenyin0@gmail.com>
2019-12-03 15:45:53 +0800

    add more unit tests
    Signed-off-by: cwen0 <cwenyin0@gmail.com>

--- controllers/networkchaos_controller_test.go
+++ controllers/networkchaos_controller_test.go
@@ -12,3 +12,69 @@
 // limitations under the License.
 
 package controllers
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/pingcap/chaos-operator/api/v1alpha1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("NetworkChaos Controller", func() {
+	BeforeEach(func() {
+		// Add any setup steps that needs to be executed before each test
+	})
+
+	AfterEach(func() {
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	// Add Tests for OpenAPI validation (or additional CRD features) specified in
+	// your API definition.
+	// Avoid adding tests for vanilla CRUD operations because they would
+	// test Kubernetes API server, which isn't the goal here.
+	Context("NetworkChaos Item", func() {
+		It("should create successfully", func() {
+			key := types.NamespacedName{
+				Name:      "network-chaos" + "-" + randomStringWithCharset(10, charset),
+				Namespace: "default",
+			}
+
+			created := &v1alpha1.NetworkChaos{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      key.Name,
+					Namespace: key.Namespace,
+				},
+				Spec: v1alpha1.NetworkChaosSpec{
+					Selector: v1alpha1.SelectorSpec{
+						Namespaces: []string{"default"},
+					},
+					Scheduler: v1alpha1.SchedulerSpec{
+						Cron: "@every 2m",
+					},
+					Action: v1alpha1.DelayAction,
+					Mode:   v1alpha1.OnePodMode,
+					Delay: &v1alpha1.DelaySpec{
+						Latency:     "10ms",
+						Correlation: "0.15",
+						Jitter:      "2ms",
+					},
+				},
+			}
+
+			By("creating an API obj")
+			Expect(k8sClient.Create(context.TODO(), created)).Should(Succeed())
+
+			By("deleting the created object")
+			Expect(k8sClient.Delete(context.TODO(), created)).To(Succeed())
+			time.Sleep(1 * time.Second)
+			Expect(k8sClient.Get(context.TODO(), key, created)).ToNot(Succeed())
+		})
+	})
+})
