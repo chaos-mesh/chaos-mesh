fa6b4b02e577fe1de9164dec4a14e6c967d669a0
CWen <cwenyin0@gmail.com>
2019-12-03 17:25:42 +0800

    add unit tests (#28)
    * apis: add unit tests
    
    Signed-off-by: cwen0 <cwenyin0@gmail.com>

---
+++ controllers/podchaos_controller_test.go
@@ -0,0 +1,109 @@
+// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controllers
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/pingcap/chaos-operator/api/v1alpha1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("PodChaos Controller", func() {
+	BeforeEach(func() {
+		// Add any setup steps that needs to be executed before each test
+	})
+
+	AfterEach(func() {
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	// Add Tests for OpenAPI validation (or additional CRD features) specified in
+	// your API definition.
+	// Avoid adding tests for vanilla CRUD operations because they would
+	// test Kubernetes API server, which isn't the goal here.
+	Context("PodChaos Item", func() {
+		It("should create PodKill successfully", func() {
+			key := types.NamespacedName{
+				Name:      "podchaos-kill" + "-" + randomStringWithCharset(10, charset),
+				Namespace: "default",
+			}
+
+			created := &v1alpha1.PodChaos{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      key.Name,
+					Namespace: key.Namespace,
+				},
+				Spec: v1alpha1.PodChaosSpec{
+					Selector: v1alpha1.SelectorSpec{
+						Namespaces: []string{"default"},
+					},
+					Scheduler: v1alpha1.SchedulerSpec{
+						Cron: "@every 2m",
+					},
+					Action: v1alpha1.PodKillAction,
+					Mode:   v1alpha1.OnePodMode,
+				},
+			}
+
+			By("creating an API obj")
+			Expect(k8sClient.Create(context.TODO(), created)).Should(Succeed())
+
+			By("deleting the created object")
+			Expect(k8sClient.Delete(context.TODO(), created)).To(Succeed())
+			time.Sleep(1 * time.Second)
+			Expect(k8sClient.Get(context.TODO(), key, created)).ToNot(Succeed())
+		})
+
+		It("should create PodFailure successfully", func() {
+			key := types.NamespacedName{
+				Name:      "podchaos-failure" + "-" + randomStringWithCharset(10, charset),
+				Namespace: "default",
+			}
+
+			created := &v1alpha1.PodChaos{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      key.Name,
+					Namespace: key.Namespace,
+				},
+				Spec: v1alpha1.PodChaosSpec{
+					Selector: v1alpha1.SelectorSpec{
+						Namespaces: []string{"default"},
+					},
+					Scheduler: v1alpha1.SchedulerSpec{
+						Cron: "@every 2m",
+					},
+					Action:   v1alpha1.PodFailureAction,
+					Mode:     v1alpha1.FixedPodMode,
+					Value:    "2",
+					Duration: "60s",
+				},
+			}
+
+			By("creating an API obj")
+			Expect(k8sClient.Create(context.TODO(), created)).Should(Succeed())
+
+			By("deleting the created object")
+			Expect(k8sClient.Delete(context.TODO(), created)).To(Succeed())
+			time.Sleep(1 * time.Second)
+			Expect(k8sClient.Get(context.TODO(), key, created)).ToNot(Succeed())
+		})
+	})
+})
--- go.mod
+++ go.mod
@@ -1,48 +1,44 @@
 module github.com/pingcap/chaos-operator
 
 require (
-	cloud.google.com/go v0.38.0 // indirect
+	github.com/AlekSi/gocov-xml v0.0.0-20190121064608-3a14fb1c4737 // indirect
 	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/docker v0.7.3-0.20190327010347-be7ac8be2ae0
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/ethercflow/hookfs v0.3.0
-	github.com/ghodss/yaml v0.0.0-20180820084758-c7ce16629ff4
+	github.com/ghodss/yaml v1.0.0
 	github.com/go-logr/logr v0.1.0
-	github.com/gogo/protobuf v1.2.2-0.20190723190241-65acae22fc9d // indirect
 	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
 	github.com/golang/protobuf v1.3.2
 	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/hanwen/go-fuse v0.0.0-20190111173210-425e8d5301f6
 	github.com/hashicorp/golang-lru v0.5.3 // indirect
-	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/jstemmer/go-junit-report v0.9.1 // indirect
 	github.com/juju/errors v0.0.0-20190930114154-d42613fe1ab9
 	github.com/juju/loggo v0.0.0-20190526231331-6e530bcce5d8 // indirect
 	github.com/juju/testing v0.0.0-20191001232224-ce9dec17d28b // indirect
+	github.com/matm/gocov-html v0.0.0-20191111163307-9ee104d84c82 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
-	github.com/onsi/ginkgo v1.6.0
-	github.com/onsi/gomega v1.4.2
+	github.com/onsi/ginkgo v1.10.3
+	github.com/onsi/gomega v1.7.0
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/robfig/cron/v3 v3.0.0
-	github.com/spf13/pflag v1.0.3 // indirect
 	github.com/vishvananda/netlink v1.0.0
 	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df
-	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8 // indirect
-	golang.org/x/net v0.0.0-20190812203447-cdfb69ac37fc
-	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
-	golang.org/x/sync v0.0.0-20190423024810-112230192c58
-	golang.org/x/sys v0.0.0-20190616124812-15dcb6c0061f // indirect
-	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	golang.org/x/net v0.0.0-20191004110552-13f9640d40b9
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	golang.org/x/tools v0.0.0-20191130070609-6e064ea0cf2d // indirect
 	google.golang.org/genproto v0.0.0-20191028173616-919d9bdd9fe6 // indirect
 	google.golang.org/grpc v1.24.0
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
-	k8s.io/api v0.0.0-20190918195907-bd6ac527cfd2
-	k8s.io/apimachinery v0.0.0-20190817020851-f2f3a405f61d
-	k8s.io/client-go v12.0.0+incompatible
-	k8s.io/kube-openapi v0.0.0-20190709113604-33be087ad058 // indirect
-	sigs.k8s.io/controller-runtime v0.3.0
+	k8s.io/api v0.0.0-20191121015604-11707872ac1c
+	k8s.io/apiextensions-apiserver v0.0.0-20191121021419-88daf26ec3b8 // indirect
+	k8s.io/apimachinery v0.0.0-20191121015412-41065c7a8c2a
+	k8s.io/client-go v0.0.0-20191121015835-571c0ef67034
+	sigs.k8s.io/controller-runtime v0.4.0
+	sigs.k8s.io/controller-tools v0.2.4 // indirect
 )
 
 go 1.13
