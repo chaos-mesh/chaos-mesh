261f79b3dfdab367d2035b9a64e5081a0e1b854c
YangKeao <keao.yang@yahoo.com>
2020-02-17 17:13:22 +0800

    support timechaos (#222)

--- pkg/chaosdaemon/util.go
+++ pkg/chaosdaemon/util.go
@@ -35,7 +35,7 @@ const (
 	containerdProtocolPrefix = "containerd://"
 	containerdDefaultNS      = "k8s.io"
 
-	defaultProcPrefix = "/mnt/proc"
+	defaultProcPrefix = "/proc"
 )
 
 // ContainerRuntimeInfoClient represents a struct which can give you information about container runtime
