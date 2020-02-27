c0eb9aa1aff84a2a3654b768e5b66e448e9bf376
Song Gao <disxiaofei@163.com>
2020-02-27 22:08:06 +0800

    Add validate admission webhook for chaos api (#279)

--- cmd/controller-manager/main.go
+++ cmd/controller-manager/main.go
@@ -168,6 +168,7 @@ func main() {
 	hookServer.Register("/inject-v1-pod", &webhook.Admission{Handler: &apiWebhook.PodInjector{
 		Config: webhookConfig,
 	}})
+	hookServer.Register("/validate-v1alpha1-chaos", &webhook.Admission{Handler: &apiWebhook.ChaosValidator{}})
 
 	// +kubebuilder:scaffold:builder
 
