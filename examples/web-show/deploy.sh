b25e4f5fbb4c2b02a8b4fa5e036c25b8e0261eba
cwen0 <cwenyin0@gmail.com>
2020-03-02 22:34:21 +0800

    add docker mirror
    Signed-off-by: cwen0 <cwenyin0@gmail.com>

--- examples/web-show/deploy.sh
+++ examples/web-show/deploy.sh
@@ -2,11 +2,50 @@
 
 set -e
 
+usage() {
+    cat << EOF
+This script is used to install wob-show.
+USAGE:
+    install.sh [FLAGS] [OPTIONS]
+FLAGS:
+    -h, --help              Prints help information
+        --docker-mirror     Use docker mirror to pull image
+EOF
+}
+
+DOCKER_MIRROR=false
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --docker-mirror)
+        DOCKER_MIRROR=true
+        shift
+        ;;
+    -h|--help)
+        usage
+        exit 0
+        ;;
+    *)
+        echo "unknown option: $key"
+        usage
+        exit 1
+        ;;
+esac
+done
+
 TARGET_IP=$(kubectl get pod -n kube-system -o wide| grep kube-controller | head -n 1 | awk '{print $6}')
 
 sed "s/TARGETIP/$TARGET_IP/g" deployment.yaml > deployment-target.yaml
 
-docker pull 
+if [ ${DOCKER_MIRROR} == "true" ]; then
+    docker pull dockerhub.azk8s.cn/pingcap/web-show || true
+    docker tag dockerhub.azk8s.cn/pingcap/web-show pingcap/web-show  || true
+    kind load docker-image pingcap/web-show > /dev/null 2>&1 || true
+fi
+
 kubectl apply -f service.yaml
 kubectl apply -f deployment-target.yaml
 
