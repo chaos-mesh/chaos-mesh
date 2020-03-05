f41d3f33c7bf69d8838b02ff49cb73c69acc7479
Yichen Yan <oraluben@outlook.com>
2020-03-05 22:30:19 +0800

    MockChaosDaemonClient and very first test case

---
+++ controllers/timechaos/timechaos_suite_test.go
@@ -0,0 +1,131 @@
+package timechaos_test
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/pingcap/chaos-mesh/api/v1alpha1"
+	. "github.com/pingcap/chaos-mesh/controllers/timechaos"
+	"github.com/pingcap/chaos-mesh/pkg/chaosdaemon/pb"
+	"github.com/pingcap/chaos-mesh/pkg/mock"
+)
+
+func TestTimechaos(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecsWithDefaultAndCustomReporters(t,
+		"TimeChaos Suite",
+		[]Reporter{envtest.NewlineReporter{}})
+}
+
+var _ = BeforeSuite(func(done Done) {
+	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
+	close(done)
+}, 60)
+
+var _ = AfterSuite(func() {
+})
+
+// Assert *MockChaosDaemonClient implements chaosdaemon.ChaosDaemonClientInterface.
+var _ ChaosDaemonClientInterface = (*MockChaosDaemonClient)(nil)
+
+type MockChaosDaemonClient struct{}
+
+func (c *MockChaosDaemonClient) SetNetem(ctx context.Context, in *chaosdaemon.NetemRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	panic("implement me")
+}
+
+func (c *MockChaosDaemonClient) DeleteNetem(ctx context.Context, in *chaosdaemon.NetemRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	panic("implement me")
+}
+
+func (c *MockChaosDaemonClient) FlushIpSet(ctx context.Context, in *chaosdaemon.IpSetRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	panic("implement me")
+}
+
+func (c *MockChaosDaemonClient) FlushIptables(ctx context.Context, in *chaosdaemon.IpTablesRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	panic("implement me")
+}
+
+func (c *MockChaosDaemonClient) SetTimeOffset(ctx context.Context, in *chaosdaemon.TimeRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	panic("implement me")
+}
+
+func (c *MockChaosDaemonClient) RecoverTimeOffset(ctx context.Context, in *chaosdaemon.TimeRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	panic("implement me")
+}
+
+func (c *MockChaosDaemonClient) ContainerKill(ctx context.Context, in *chaosdaemon.ContainerRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	panic("implement me")
+}
+
+func (c *MockChaosDaemonClient) Close() error {
+	if err := mock.On("CloseChaosDaemonClient"); err != nil {
+		return err.(error)
+	}
+	return nil
+}
+
+var _ = Describe("TimeChaos", func() {
+	BeforeEach(func() {
+		// Add any setup steps that needs to be executed before each test
+	})
+
+	AfterEach(func() {
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	// Add Tests for OpenAPI validation (or additional CRD features) specified in
+	// your API definition.
+	// Avoid adding tests for vanilla CRUD operations because they would
+	// test Kubernetes API server, which isn't the goal here.
+	Context("TimeChaos", func() {
+		It("TimeChaos Apply", func() {
+			mock.With("MockSelectAndGeneratePods", nil)
+
+			duration := "invalid_duration"
+
+			timechaos := v1alpha1.TimeChaos{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "TimeChaos",
+					APIVersion: "v1",
+				},
+				Spec: v1alpha1.TimeChaosSpec{
+					Mode:  "FixedPodMode",
+					Value: "0",
+					Selector: v1alpha1.SelectorSpec{
+						Namespaces: []string{"namespace"},
+					},
+					TimeOffset: v1alpha1.TimeOffset{},
+					Duration:   &duration,
+					Scheduler:  nil,
+				},
+			}
+
+			r := Reconciler{
+				Client:        fake.NewFakeClient(),
+				EventRecorder: &record.FakeRecorder{},
+				Log:           ctrl.Log.WithName("controllers").WithName("TimeChaos"),
+			}
+
+			err := r.Apply(context.TODO(), ctrl.Request{}, &timechaos)
+
+			Expect(err).To(HaveOccurred())
+			// fixme: incorrect Pods mock
+			Expect(err.Error()).To(ContainSubstring("no pod is selected"))
+		})
+	})
+})
--- controllers/timechaos/types.go
+++ controllers/timechaos/types.go
@@ -17,6 +17,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"google.golang.org/grpc"
 
 	"k8s.io/client-go/tools/record"
 
@@ -34,8 +35,10 @@ import (
 	"github.com/pingcap/chaos-mesh/controllers/common"
 	"github.com/pingcap/chaos-mesh/controllers/reconciler"
 	"github.com/pingcap/chaos-mesh/controllers/twophase"
+	"github.com/pingcap/chaos-mesh/pkg/mock"
 	"github.com/pingcap/chaos-mesh/pkg/utils"
 
+	chaosdaemon "github.com/pingcap/chaos-mesh/pkg/chaosdaemon/pb"
 	pb "github.com/pingcap/chaos-mesh/pkg/chaosdaemon/pb"
 )
 
@@ -76,6 +79,39 @@ func (r *Reconciler) commonTimeChaos(timechaos *v1alpha1.TimeChaos, req ctrl.Req
 func (r *Reconciler) scheduleTimeChaos(timechaos *v1alpha1.TimeChaos, req ctrl.Request) (ctrl.Result, error) {
 	sr := twophase.NewReconciler(r, r.Client, r.Log)
 	return sr.Reconcile(req)
+}
+
+// for convenient unit testing
+type ChaosDaemonClientInterface interface {
+	chaosdaemon.ChaosDaemonClient
+	Close() error
+}
+
+type GrpcChaosDaemonClient struct {
+	chaosdaemon.ChaosDaemonClient
+	conn *grpc.ClientConn
+}
+
+func (c *GrpcChaosDaemonClient) Close() error {
+	return c.conn.Close()
+}
+
+func NewChaosDaemonClient(ctx context.Context, c client.Client, pod *v1.Pod) (ChaosDaemonClientInterface, error) {
+	if cli := mock.On("MockChaosDaemonClient"); cli != nil {
+		return cli.(ChaosDaemonClientInterface), nil
+	}
+	if err := mock.On("NewChaosDaemonClientError"); err != nil {
+		return nil, err.(error)
+	}
+
+	cc, err := utils.CreateGrpcConnection(ctx, c, pod)
+	if err != nil {
+		return nil, err
+	}
+	return &GrpcChaosDaemonClient{
+		ChaosDaemonClient: pb.NewChaosDaemonClient(cc),
+		conn:              cc,
+	}, nil
 }
 
 // Apply applies time-chaos
@@ -178,13 +214,11 @@ func (r *Reconciler) cleanFinalizersAndRecover(ctx context.Context, chaos *v1alp
 func (r *Reconciler) recoverPod(ctx context.Context, pod *v1.Pod, chaos *v1alpha1.TimeChaos) error {
 	r.Log.Info("Try to recover pod", "namespace", pod.Namespace, "name", pod.Name)
 
-	c, err := utils.CreateGrpcConnection(ctx, r.Client, pod)
+	pbClient, err := NewChaosDaemonClient(ctx, r.Client, pod)
 	if err != nil {
 		return err
 	}
-	defer c.Close()
-
-	pbClient := pb.NewChaosDaemonClient(c)
+	defer pbClient.Close()
 
 	if len(pod.Status.ContainerStatuses) == 0 {
 		return fmt.Errorf("%s %s can't get the state of container", pod.Namespace, pod.Name)
@@ -232,13 +266,11 @@ func (r *Reconciler) applyAllPods(ctx context.Context, pods []v1.Pod, chaos *v1a
 func (r *Reconciler) applyPod(ctx context.Context, pod *v1.Pod, chaos *v1alpha1.TimeChaos) error {
 	r.Log.Info("Try to shift time on pod", "namespace", pod.Namespace, "name", pod.Name)
 
-	c, err := utils.CreateGrpcConnection(ctx, r.Client, pod)
+	pbClient, err := NewChaosDaemonClient(ctx, r.Client, pod)
 	if err != nil {
 		return err
 	}
-	defer c.Close()
-
-	pbClient := pb.NewChaosDaemonClient(c)
+	defer pbClient.Close()
 
 	if len(pod.Status.ContainerStatuses) == 0 {
 		return fmt.Errorf("%s %s can't get the state of container", pod.Namespace, pod.Name)
