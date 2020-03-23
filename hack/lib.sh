8f063e6ccd725db72781b7ff5cd5e884feee5723
Song Gao <disxiaofei@163.com>
2020-03-23 23:52:37 +0800

    Init e2e framework (#322)

---
+++ hack/lib.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 PingCAP, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ -z "$ROOT" ]; then
+    echo "error: ROOT should be initialized"
+    exit 1
+fi
+
+OS=$(go env GOOS)
+ARCH=$(go env GOARCH)
+OUTPUT=${ROOT}/output
+OUTPUT_BIN=${OUTPUT}/bin
+TERRAFORM_BIN=${OUTPUT_BIN}/terraform
+TERRAFORM_VERSION=0.12.12
+KUBECTL_VERSION=1.12.10
+KUBECTL_BIN=$OUTPUT_BIN/kubectl
+HELM_BIN=$OUTPUT_BIN/helm
+#
+# Don't ugprade to 2.15.x/2.16.x until this issue
+# (https://github.com/helm/helm/issues/6361) has been fixed.
+#
+HELM_VERSION=2.9.1
+KIND_VERSION=0.7.0
+KIND_BIN=$OUTPUT_BIN/kind
+
+test -d "$OUTPUT_BIN" || mkdir -p "$OUTPUT_BIN"
+
+function hack::verify_terraform() {
+    if test -x "$TERRAFORM_BIN"; then
+        local v=$($TERRAFORM_BIN version | awk '/^Terraform v.*/ { print $2 }' | sed 's#^v##')
+        [[ "$v" == "$TERRAFORM_VERSION" ]]
+        return
+    fi
+    return 1
+}
+
+function hack::install_terraform() {
+    echo "Installing terraform v$TERRAFORM_VERSION..."
+    local tmpdir=$(mktemp -d)
+    trap "test -d $tmpdir && rm -r $tmpdir" RETURN
+    pushd $tmpdir > /dev/null
+    wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip
+    unzip terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip
+    mv terraform $TERRAFORM_BIN
+    popd > /dev/null
+    chmod +x $TERRAFORM_BIN
+}
+
+function hack::ensure_terraform() {
+    if ! hack::verify_terraform; then
+        hack::install_terraform
+    fi
+}
+
+function hack::verify_kubectl() {
+    if test -x "$KUBECTL_BIN"; then
+        [[ "$($KUBECTL_BIN version --client --short | grep -o -E '[0-9]+\.[0-9]+\.[0-9]+')" == "$KUBECTL_VERSION" ]]
+        return
+    fi
+    return 1
+}
+
+function hack::ensure_kubectl() {
+    if hack::verify_kubectl; then
+        return 0
+    fi
+    echo "Installing kubectl v$KUBECTL_VERSION..."
+    tmpfile=$(mktemp)
+    trap "test -f $tmpfile && rm $tmpfile" RETURN
+    curl --retry 10 -L -o $tmpfile https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/${OS}/${ARCH}/kubectl
+    mv $tmpfile $KUBECTL_BIN
+    chmod +x $KUBECTL_BIN
+}
+
+function hack::verify_helm() {
+    if test -x "$HELM_BIN"; then
+        local v=$($HELM_BIN version --short --client | grep -o -E '[0-9]+\.[0-9]+\.[0-9]+')
+        [[ "$v" == "$HELM_VERSION" ]]
+        return
+    fi
+    return 1
+}
+
+function hack::ensure_helm() {
+    if hack::verify_helm; then
+        return 0
+    fi
+    local HELM_URL=http://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-${OS}-${ARCH}.tar.gz
+    curl --retry 10 -L -s "$HELM_URL" | tar --strip-components 1 -C $OUTPUT_BIN -zxf - ${OS}-${ARCH}/helm
+}
+
+function hack::verify_kind() {
+    if test -x "$KIND_BIN"; then
+        [[ "$($KIND_BIN --version 2>&1 | cut -d ' ' -f 3)" == "$KIND_VERSION" ]]
+        return
+    fi
+    return 1
+}
+
+function hack::ensure_kind() {
+    if hack::verify_kind; then
+        return 0
+    fi
+    echo "Installing kind v$KIND_VERSION..."
+    tmpfile=$(mktemp)
+    trap "test -f $tmpfile && rm $tmpfile" RETURN
+    curl --retry 10 -L -o $tmpfile https://github.com/kubernetes-sigs/kind/releases/download/v${KIND_VERSION}/kind-$(uname)-amd64
+    mv $tmpfile $KIND_BIN
+    chmod +x $KIND_BIN
+}
+
+# hack::version_ge "$v1" "$v2" checks whether "v1" is greater or equal to "v2"
+function hack::version_ge() {
+    [ "$(printf '%s\n' "$1" "$2" | sort -V | head -n1)" = "$2" ]
+}
---
+++ test/e2e/chaos/pod.go
@@ -0,0 +1,216 @@
+package chaos
+
+import (
+	"context"
+	"time"
+
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/klog"
+
+	"github.com/onsi/ginkgo"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	restClient "k8s.io/client-go/rest"
+	"k8s.io/kubernetes/test/e2e/framework"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/pingcap/chaos-mesh/api/v1alpha1"
+	chaosmeshv1alpha1 "github.com/pingcap/chaos-mesh/api/v1alpha1"
+	"github.com/pingcap/chaos-mesh/test/pkg/fixture"
+
+	// load pprof
+	_ "net/http/pprof"
+)
+
+const (
+	pauseImage = "gcr.io/google-containers/pause:latest"
+)
+
+var _ = ginkgo.Describe("[chaos-mesh] Basic", func() {
+	f := framework.NewDefaultFramework("chaos-mesh")
+	var ns string
+	var fwCancel context.CancelFunc
+	var kubeCli kubernetes.Interface
+	var config *restClient.Config
+	var cli client.Client
+
+	ginkgo.BeforeEach(func() {
+		ns = f.Namespace.Name
+		_, cancel := context.WithCancel(context.Background())
+		fwCancel = cancel
+		kubeCli = f.ClientSet
+		var err error
+		config, err = framework.LoadConfig()
+		framework.ExpectNoError(err, "config error")
+		scheme := runtime.NewScheme()
+		_ = clientgoscheme.AddToScheme(scheme)
+		_ = chaosmeshv1alpha1.AddToScheme(scheme)
+		cli, err = client.New(config, client.Options{Scheme: scheme})
+
+	})
+
+	ginkgo.AfterEach(func() {
+		if fwCancel != nil {
+			fwCancel()
+		}
+	})
+
+	ginkgo.It("PodFailure", func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		nd := fixture.NewCommonNginxDeployment("nginx", ns, 1)
+		_, err := kubeCli.AppsV1().Deployments(ns).Create(nd)
+		framework.ExpectNoError(err, "create nginx deployment error")
+		err = waitDeploymentReady("nginx", ns, kubeCli)
+		framework.ExpectNoError(err, "wait nginx deployment ready error")
+
+		podFailureChaos := &v1alpha1.PodChaos{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "nginx-failure",
+				Namespace: ns,
+			},
+			Spec: v1alpha1.PodChaosSpec{
+				Selector: v1alpha1.SelectorSpec{
+					Namespaces: []string{
+						ns,
+					},
+					LabelSelectors: map[string]string{
+						"app": "nginx",
+					},
+				},
+				Action: v1alpha1.PodFailureAction,
+				Mode:   v1alpha1.OnePodMode,
+			},
+		}
+		err = cli.Create(ctx, podFailureChaos)
+		framework.ExpectNoError(err, "create pod failure chaos error")
+
+		err = wait.PollImmediate(3*time.Second, 5*time.Minute, func() (done bool, err error) {
+
+			listOption := metav1.ListOptions{
+				LabelSelector: labels.SelectorFromSet(map[string]string{
+					"app": "nginx",
+				}).String(),
+			}
+			pods, err := kubeCli.CoreV1().Pods(ns).List(listOption)
+			if err != nil {
+				return false, nil
+			}
+			if len(pods.Items) != 1 {
+				return false, nil
+			}
+			pod := pods.Items[0]
+			for _, c := range pod.Spec.Containers {
+				if c.Image == pauseImage {
+					return true, nil
+				}
+			}
+			return false, nil
+		})
+
+		err = cli.Delete(ctx, podFailureChaos)
+		framework.ExpectNoError(err, "failed to delete pod failure chaos")
+
+		klog.Infof("success to perform pod failure")
+		err = wait.PollImmediate(3*time.Second, 5*time.Minute, func() (done bool, err error) {
+			listOption := metav1.ListOptions{
+				LabelSelector: labels.SelectorFromSet(map[string]string{
+					"app": "nginx",
+				}).String(),
+			}
+			pods, err := kubeCli.CoreV1().Pods(ns).List(listOption)
+			if err != nil {
+				return false, nil
+			}
+			if len(pods.Items) != 1 {
+				return false, nil
+			}
+			pod := pods.Items[0]
+			for _, c := range pod.Spec.Containers {
+				if c.Image == "nginx:latest" {
+					return true, nil
+				}
+			}
+			return false, nil
+		})
+		framework.ExpectNoError(err, "pod failure recover failed")
+
+		cancel()
+	})
+
+	ginkgo.It("PodKill", func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		bpod := fixture.NewCommonNginxPod("nginx", ns)
+		_, err := kubeCli.CoreV1().Pods(ns).Create(bpod)
+		framework.ExpectNoError(err, "create nginx pod error")
+		err = waitPodRunning("nginx", ns, kubeCli)
+		framework.ExpectNoError(err, "wait nginx running error")
+
+		podKillChaos := &v1alpha1.PodChaos{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "nginx-kill",
+				Namespace: ns,
+			},
+			Spec: v1alpha1.PodChaosSpec{
+				Selector: v1alpha1.SelectorSpec{
+					Namespaces: []string{
+						ns,
+					},
+					LabelSelectors: map[string]string{
+						"app": "nginx",
+					},
+				},
+				Action: v1alpha1.PodKillAction,
+				Mode:   v1alpha1.OnePodMode,
+				Scheduler: &v1alpha1.SchedulerSpec{
+					Cron: "@every 10s",
+				},
+			},
+		}
+		err = cli.Create(ctx, podKillChaos)
+		framework.ExpectNoError(err, "create pod chaos error")
+
+		err = wait.Poll(5*time.Second, 5*time.Minute, func() (done bool, err error) {
+			_, err = kubeCli.CoreV1().Pods(ns).Get("nginx", metav1.GetOptions{})
+			if err != nil && errors.IsNotFound(err) {
+				return true, nil
+			}
+			return false, nil
+		})
+		framework.ExpectNoError(err, "Pod kill chaos perform failed")
+		cancel()
+	})
+})
+
+func waitPodRunning(name, namespace string, cli kubernetes.Interface) error {
+	return wait.Poll(5*time.Second, 5*time.Minute, func() (done bool, err error) {
+		pod, err := cli.CoreV1().Pods(namespace).Get(name, metav1.GetOptions{})
+		if err != nil {
+			return false, nil
+		}
+		if pod.Status.Phase != corev1.PodRunning {
+			return false, nil
+		}
+		return true, nil
+	})
+}
+
+func waitDeploymentReady(name, namespace string, cli kubernetes.Interface) error {
+	return wait.Poll(5*time.Second, 5*time.Minute, func() (done bool, err error) {
+		d, err := cli.AppsV1().Deployments(namespace).Get(name, metav1.GetOptions{})
+		if err != nil {
+			return false, nil
+		}
+		if d.Status.AvailableReplicas != *d.Spec.Replicas {
+			return false, nil
+		}
+		if d.Status.UpdatedReplicas != *d.Spec.Replicas {
+			return false, nil
+		}
+		return true, nil
+	})
+}
