e861029ef771fb1419f856ed0fd16ddede88540a
Yang Keao <keao.yang@yahoo.com>
2020-03-26 15:08:12 +0800

    format
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- controllers/timechaos/util_test.go
+++ controllers/timechaos/util_test.go
@@ -1,15 +1,16 @@
 package timechaos
 
 import (
-	. "github.com/onsi/gomega"
 	"testing"
 	"time"
+
+	. "github.com/onsi/gomega"
 )
 
 type SecAndNSecFromDurationTestCase struct {
 	Duration time.Duration
-	Sec int64
-	NSec int64
+	Sec      int64
+	NSec     int64
 }
 
 func TestSecAndNSecFromDuration(t *testing.T) {
@@ -17,14 +18,14 @@ func TestSecAndNSecFromDuration(t *testing.T) {
 	cases := []SecAndNSecFromDurationTestCase{
 		{time.Second * 100, 100, 0},
 		{time.Second * -100, -100, 0},
-		{time.Second * -100 + time.Microsecond * -20, -100, -20000},
-		{time.Second * -100 + time.Microsecond * 20, -99,-999980000},
-		{time.Second * 100 + time.Microsecond * 20, 100 ,20000},
-		{time.Second * 100 + time.Microsecond * -20, 99, 999980000},
+		{time.Second*-100 + time.Microsecond*-20, -100, -20000},
+		{time.Second*-100 + time.Microsecond*20, -99, -999980000},
+		{time.Second*100 + time.Microsecond*20, 100, 20000},
+		{time.Second*100 + time.Microsecond*-20, 99, 999980000},
 	}
 
 	for _, c := range cases {
-		sec,nsec := secAndNSecFromDuration(c.Duration)
+		sec, nsec := secAndNSecFromDuration(c.Duration)
 		g.Expect(sec).Should(Equal(c.Sec))
 		g.Expect(nsec).Should(Equal(c.NSec))
 	}
