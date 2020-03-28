408fd6dfe2092e4257a1116d15cace45efe2f031
Yichen Yan <oraluben@outlook.com>
2020-03-28 21:05:58 +0800

    Format

--- controllers/podchaos_controller.go
+++ controllers/podchaos_controller.go
@@ -54,10 +54,10 @@ func (r *PodChaosReconciler) Reconcile(req ctrl.Request) (result ctrl.Result, er
 
 	result, err = reconciler.Reconcile(req, chaos)
 	if err != nil {
-		if !(chaos.IsDeleted() || chaos.IsPaused()) {
-			r.Event(chaos, v1.EventTypeWarning, utils.EventChaosInjectFailed, err.Error())
-		} else {
+		if chaos.IsDeleted() || chaos.IsPaused() {
 			r.Event(chaos, v1.EventTypeWarning, utils.EventChaosRecoverFailed, err.Error())
+		} else {
+			r.Event(chaos, v1.EventTypeWarning, utils.EventChaosInjectFailed, err.Error())
 		}
 	}
 
