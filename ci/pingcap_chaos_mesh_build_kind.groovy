c48b7980629c884d4dbcf284bf0514af5591258b
Song Gao <2695690803@qq.com>
2020-04-10 17:44:20 +0800

    update scripts

--- ci/pingcap_chaos_mesh_build_kind.groovy
+++ ci/pingcap_chaos_mesh_build_kind.groovy
@@ -190,6 +190,7 @@ def call(BUILD_BRANCH, CREDENTIALS_ID) {
 					stage("Build") {
 						ansiColor('xterm') {
 							sh """
+							make install-kubebuilder
 							make install-kustomize
 							DOCKER_REGISTRY=localhost:5000 make
 							make e2e-build
