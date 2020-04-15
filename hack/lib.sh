56051225a75d0b1a1cf2b9d337d8f91f607210ab
Song Gao <2695690803@qq.com>
2020-04-15 11:34:04 +0800

    remove unused bin

--- hack/lib.sh
+++ hack/lib.sh
@@ -22,8 +22,6 @@ OS=$(go env GOOS)
 ARCH=$(go env GOARCH)
 OUTPUT=${ROOT}/output
 OUTPUT_BIN=${OUTPUT}/bin
-TERRAFORM_BIN=${OUTPUT_BIN}/terraform
-TERRAFORM_VERSION=0.12.12
 KUBECTL_VERSION=1.12.10
 KUBECTL_BIN=$OUTPUT_BIN/kubectl
 HELM_BIN=$OUTPUT_BIN/helm
@@ -40,32 +38,6 @@ KUSTOMIZE_BIN=$OUTPUT_BIN/kustomize
 KUSTOMIZE_VERSION=3.5.4
 
 test -d "$OUTPUT_BIN" || mkdir -p "$OUTPUT_BIN"
-
-function hack::verify_terraform() {
-    if test -x "$TERRAFORM_BIN"; then
-        local v=$($TERRAFORM_BIN version | awk '/^Terraform v.*/ { print $2 }' | sed 's#^v##')
-        [[ "$v" == "$TERRAFORM_VERSION" ]]
-        return
-    fi
-    return 1
-}
-
-function hack::install_terraform() {
-    local tmpdir=$(mktemp -d)
-    trap "test -d $tmpdir && rm -r $tmpdir" RETURN
-    pushd $tmpdir > /dev/null
-    wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip
-    unzip terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip
-    mv terraform $TERRAFORM_BIN
-    popd > /dev/null
-    chmod +x $TERRAFORM_BIN
-}
-
-function hack::ensure_terraform() {
-    if ! hack::verify_terraform; then
-        hack::install_terraform
-    fi
-}
 
 function hack::verify_kubectl() {
     if test -x "$KUBECTL_BIN"; then
