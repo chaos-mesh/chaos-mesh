612464150e65d5364079834c84231f8c79315e44
AngleNet <AngleNet@users.noreply.github.com>
2020-04-26 18:54:03 +0800

    Add a new `StressChaos` to generate plenty of stresses (#332)
    * Add stress chaos type

---
+++ api/v1alpha1/stresschaos_webhook_test.go
@@ -0,0 +1,207 @@
+// Copyright 2020 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+var _ = Describe("stresschaos_webhook", func() {
+	Context("Defaulter", func() {
+		It("set default namespace selector", func() {
+			stresschaos := &StressChaos{
+				ObjectMeta: metav1.ObjectMeta{Namespace: metav1.NamespaceDefault},
+			}
+			stresschaos.Default()
+			Expect(stresschaos.Spec.Selector.Namespaces[0]).To(Equal(metav1.NamespaceDefault))
+		})
+	})
+	Context("ChaosValidator of stresschaos", func() {
+		It("Validate StressChaos", func() {
+
+			type TestCase struct {
+				name    string
+				chaos   StressChaos
+				execute func(chaos *StressChaos) error
+				expect  string
+			}
+			duration := "400s"
+			stressors := &Stressors{
+				MemoryStressor: &MemoryStressor{
+					Stressor: Stressor{Workers: 1},
+				},
+			}
+			tcs := []TestCase{
+				{
+					name: "simple ValidateCreate",
+					chaos: StressChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo1",
+						},
+						Spec: StressChaosSpec{
+							Stressors: stressors,
+						},
+					},
+					execute: func(chaos *StressChaos) error {
+						return chaos.ValidateCreate()
+					},
+					expect: "",
+				},
+				{
+					name: "simple ValidateUpdate",
+					chaos: StressChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo2",
+						},
+						Spec: StressChaosSpec{
+							Stressors: stressors,
+						},
+					},
+					execute: func(chaos *StressChaos) error {
+						return chaos.ValidateUpdate(chaos)
+					},
+					expect: "",
+				},
+				{
+					name: "simple ValidateDelete",
+					chaos: StressChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo3",
+						},
+						Spec: StressChaosSpec{
+							Stressors: stressors,
+						},
+					},
+					execute: func(chaos *StressChaos) error {
+						return chaos.ValidateDelete()
+					},
+					expect: "",
+				},
+				{
+					name: "only define the Scheduler",
+					chaos: StressChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo4",
+						},
+						Spec: StressChaosSpec{
+							Stressors: stressors,
+							Scheduler: &SchedulerSpec{
+								Cron: "@every 10m",
+							},
+						},
+					},
+					execute: func(chaos *StressChaos) error {
+						return chaos.ValidateCreate()
+					},
+					expect: "error",
+				},
+				{
+					name: "only define the Duration",
+					chaos: StressChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo5",
+						},
+						Spec: StressChaosSpec{
+							Stressors: stressors,
+							Duration:  &duration,
+						},
+					},
+					execute: func(chaos *StressChaos) error {
+						return chaos.ValidateCreate()
+					},
+					expect: "error",
+				},
+				{
+					name: "missing stressors",
+					chaos: StressChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo5",
+						},
+					},
+					execute: func(chaos *StressChaos) error {
+						return chaos.ValidateCreate()
+					},
+					expect: "error",
+				},
+			}
+
+			for _, tc := range tcs {
+				err := tc.execute(&tc.chaos)
+				if tc.expect == "error" {
+					Expect(err).To(HaveOccurred())
+				} else {
+					Expect(err).NotTo(HaveOccurred())
+				}
+			}
+		})
+
+		It("Validate Stressors", func() {
+			type TestCase struct {
+				name     string
+				stressor Validateable
+				errs     int
+			}
+			tcs := []TestCase{
+				{
+					name:     "missing workers",
+					stressor: &Stressor{},
+					errs:     1,
+				},
+				{
+					name: "default MemoryStressor",
+					stressor: &MemoryStressor{
+						Stressor: Stressor{Workers: 1},
+					},
+					errs: 0,
+				},
+				{
+					name: "default CPUStressor",
+					stressor: &CPUStressor{
+						Stressor: Stressor{Workers: 1},
+					},
+					errs: 0,
+				},
+			}
+			parent := field.NewPath("parent")
+			for _, tc := range tcs {
+				Expect(tc.stressor.Validate(parent)).To(HaveLen(tc.errs))
+			}
+		})
+
+		It("Parse MemoryStressor fields", func() {
+			vm := MemoryStressor{}
+			incorrectBytes := []string{"-1", "-1%", "101%", "x%", "-1Kb"}
+			for _, b := range incorrectBytes {
+				vm.Size = b
+				Expect(vm.tryParseBytes()).Should(HaveOccurred())
+			}
+			correctBytes := []string{"", "1%", "100KB", "100B"}
+			for _, b := range correctBytes {
+				vm.Size = b
+				Expect(vm.tryParseBytes()).ShouldNot(HaveOccurred())
+			}
+		})
+
+	})
+
+})
---
+++ doc/stress_chaos.md
@@ -0,0 +1,69 @@
+# Stress Chaos Document
+
+This document helps you to build stress chaos experiments.
+
+Stress chaos is a chaos to generate plenty of stresses over a collection of pods. The stressors is injected into the target pods via the `chaos-daemon` internally. 
+
+A `StressChaos` shares common configurations like other chaos, such as how to select pods, how to specify periodic chaos ... (You can refer to other docs for how to use them). It defines stressors in **either** of the following two ways:
+
+* `stressors`
+
+  Stressors defines plenty of stressors supported to stress system components out. You can use one or more of them to make up various kinds of stresses. At least one of the stressors should be specified. The following is supported stressors for now:
+
+  1. `memory`
+
+     A `memory` stressor will continuously stress virtual memory out. 
+
+     | Option    | Type    | Required | Description                                                  |
+     | --------- | ------- | -------- | ------------------------------------------------------------ |
+     | `workers` | Integer | True     | Specifies concurrent stressing instance                      |
+     | `size`   | String  | False    | Specifies memory size consumed per worker, default is the total available memory. One can also specify the size as *%* of total available memory or in units of *B, KB/KiB, MB/MiB, GB/GiB, TB/TiB*. |
+
+  2. `cpu`
+
+     A `cpu` stressor will continuously stress CPU out. 
+
+     | Option    | Type    | Required | Description                                                  |
+     | --------- | ------- | -------- | ------------------------------------------------------------ |
+     | `workers` | Integer | True     | Specifies concurrent stressing instance. Actually it specifies how many CPUs to stress when it's less than available CPUs. |
+     | `load`    | Integer | False    | Specifies  percent loading per worker. 0 is effectively a sleep (no load) and 100 is full loading |
+
+* `stressngStressors`
+
+  StressngStressors defines plenty of stressors just like `Stressors` except that it's an experimental feature and more powerful. You can define stressors in `stress-ng` (see also `man stress-ng`) dialect, however not all of the supported stressors are well tested (**You have been warned**). It may be retired in later releases. You should always use `Stressors` to define the stressors and use this only when you want more stressors unsupported by `Stressors`. When both `StressngStressors` and `Stressors` are defined, `StressngStressors` wins. 
+
+Let's try it out! An example `yaml` of `StressChaos` which burns 1 CPU for 30 seconds in every 2 minutes is listed below: 
+
+```yaml
+apiVersion: pingcap.com/v1alpha1
+kind: StressChaos
+metadata:
+  name: burn-cpu
+  namespace: chaos-testing
+spec:
+  mode: one
+  selector:
+    namespaces:
+      - tidb-cluster-demo
+  stressors:
+    cpu:
+      workers: 1
+  duration: "30s"
+  scheduler:
+    cron: "@every 2m"
+```
+
+Then we could apply it 
+
+```bash
+# If you do not have a namespace for your application, you can create one such as tidb-cluster-demo
+kubectl create ns tidb-cluster-demo
+# Create your pods in the target namespace such as tidb-cluster-demo
+kubectl apply -f your-pods.yaml
+# Inject a stress chaos
+kubectl apply -f your-stress-chaos.yaml
+# Your pod's cpu will burn for 30s
+```
+
+Have fun : )
+
