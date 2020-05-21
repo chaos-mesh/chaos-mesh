c080940af3ec3a6fac6fb18ea24cfdf87571998b
“fewdan” <fuyihao@syvip.com>
2020-05-21 22:18:54 +0800

    conflict

--- pkg/webhook/config/watcher/util.go
+++ pkg/webhook/config/watcher/util.go
@@ -13,24 +13,9 @@
 
 package watcher
 
-<<<<<<< HEAD:pkg/apiserver/status_code/status_code.go
-// StatusCode represents the code of api status
-type StatusCode int
-
-const (
-	// Success indicates the successful return of this API interface.
-	Success StatusCode = 0
-	// GetResourcesWrong indicates an error when getting resources
-	GetResourcesWrong StatusCode = 1001
-	// GetResourcesFromDBWrong indicates an error when getting resources from DB
-	GetResourcesFromDBWrong StatusCode = 1006
-	// IncompleteField indicates that some fields are missing.
-	IncompleteField StatusCode = 1007
-=======
 import (
 	"bytes"
 	"html/template"
->>>>>>> upstream/master:pkg/webhook/config/watcher/util.go
 )
 
 func renderTemplateWithArgs(tpl *template.Template, args map[string]string) ([]byte, error) {
