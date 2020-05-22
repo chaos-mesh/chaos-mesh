1462c039d4fde7db8c5ae5a7ab0769c5388a1da8
xuechun Lei <xuechunlei922@gmail.com>
2020-05-22 18:14:57 +0800

    feat: implement the ui of creating a new experiment

---
+++ ui/src/pages/Experiments/Steps/Target/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import VerticalTabs from '../../../../components/VerticalTabs'
+import Pod from './Pod'
+import Network from './Network'
+import { StepProps } from '../../types'
+
+const tabs = [
+  { label: 'Pod Lifecycle' },
+  { label: 'Network' },
+  { label: 'Fife system I/O', disabled: true },
+  { label: 'Linux Kernel', disabled: true },
+  { label: 'Clock', disabled: true },
+  { label: 'Stress CPU/Memory', disabled: true },
+]
+
+export default function TargetStep({ formProps }: StepProps) {
+  const tabPanels = [<Pod {...formProps} />, <Network {...formProps} />]
+
+  const props = {
+    tabs,
+    tabPanels,
+  }
+
+  return <VerticalTabs {...props} />
+}
