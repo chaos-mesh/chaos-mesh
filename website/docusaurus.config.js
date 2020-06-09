1c83efd85dffecf24d6a4f4eb726c8ffed220dd2
CWen <cwenyin0@gmail.com>
2020-06-09 21:31:54 +0800

    Add document website (#604)
    * init website
    
    Signed-off-by: cwen0 <cwenyin0@gmail.com>
    
    * fix comment
    
    Signed-off-by: cwen0 <cwenyin0@gmail.com>
    
    * address comment
    
    Signed-off-by: cwen0 <cwenyin0@gmail.com>
    
    * update readme
    
    Signed-off-by: cwen0 <cwenyin0@gmail.com>
    
    * add CNAME
    
    Signed-off-by: cwen0 <cwenyin0@gmail.com>
    
    Co-authored-by: pingcap-github-bot <sre-bot@pingcap.com>

---
+++ website/docusaurus.config.js
@@ -0,0 +1,110 @@
+module.exports = {
+  title: 'Chaos Mesh™',
+  tagline: 'A Powerful Chaos Engineering Platform for Kubernetes',
+  url: 'https://chaos-mesh.github.io',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'chaos-mesh', // Usually your GitHub org/user name.
+  projectName: 'chaos-mesh.github.io', // Usually your repo name.
+  themeConfig: {
+    defaultDarkMode: false,
+    algolia: {
+      apiKey: '1d330c3dae1c5376607ecee61b4e2271',
+      indexName: 'chaos mesh',
+    },
+    navbar: {
+      logo: {
+        alt: 'Chaos Mesh Logo',
+        src: 'img/logo.svg',
+        srcDark: 'img/logo_dark.png'
+      },
+      links: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/pingcap/chaos-mesh',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting started',
+              to: 'docs/installation/installation',
+            },
+            {
+              label: 'User Guide',
+              to: 'docs/user_guides/run_chaos_experiment',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/chaos_mesh',
+            },
+            {
+              label: 'Slack',
+              href: 'https://pingcap.com/tidbslack',
+            },
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/chaos-mesh',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/pingcap/chaos-mesh',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} PingCAP, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'overview',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/pingcap/chaos-mesh/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/pingcap/chaos-mesh/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ website/sidebars.js
@@ -0,0 +1,59 @@
+module.exports = {
+  docs: [
+    {
+      type:'doc',
+      id: 'overview',
+    },
+    {
+      type: "category",
+      label: "Getting Started",
+      items: [
+        "installation/installation",
+        "installation/get_started_on_kind",
+        "installation/get_started_on_minikube"
+      ]
+    },
+    {
+      type: "category",
+      label: "User Guide",
+      items:[
+        "user_guides/run_chaos_experiment",
+        "user_guides/pause_experiment",
+        {
+          type: "category",
+          label: "Configure Chaos",
+          items: [
+            "user_guides/podchaos_experiment",
+            "user_guides/networkchaos_experiment",
+            "user_guides/stresschaos_experiment",
+            "user_guides/timechaos_experiment",
+            "user_guides/iochaos_experiment",
+            "user_guides/kernelchaos_experiment"
+          ]
+        },
+        "user_guides/sidecar_configmap",
+        "user_guides/sidecar_template"
+      ]
+    },
+    {
+      type: "category",
+      label: "Development Guide",
+      items:[
+        "development_guides/development_overview",
+        "development_guides/set_up_the_development_environment",
+        "development_guides/develop_a_new_chaos"
+      ]
+    },
+    {
+      type:'doc',
+      id: 'faqs',
+    },
+    {
+      type: "category",
+      label: "Releases",
+      items:[
+        "releases/v0.8.0"
+      ]
+    }
+  ]
+};
