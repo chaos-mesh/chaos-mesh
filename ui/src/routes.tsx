6708ea667252a778211aee136b7e1f75ba568822
Yue Yang <g1enyy0ung@gmail.com>
2020-07-21 10:22:07 +0800

    chore: update overview and experiments related (new, detail) (#712)

--- ui/src/routes.tsx
+++ ui/src/routes.tsx
@@ -1,16 +1,20 @@
-import { RouteProps } from 'react-router'
-
-import Overview from './pages/Overview'
-import Experiments from './pages/Experiments'
-import ExperimentDetail from './pages/ExperimentDetail'
-import Events from './pages/Events'
 import Archives from './pages/Archives'
+import Events from './pages/Events'
+import ExperimentDetail from './pages/ExperimentDetail'
+import Experiments from './pages/Experiments'
+import NewExperiment from 'components/NewExperiment'
+import Overview from './pages/Overview'
+import { RouteProps } from 'react-router'
 
 const routes: RouteProps[] = [
   {
     component: Overview,
     path: '/overview',
     exact: true,
+  },
+  {
+    component: NewExperiment,
+    path: '/newExperiment',
   },
   {
     component: Experiments,
