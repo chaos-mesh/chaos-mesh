96daae041e4b5e911c25ee4ebd7eb3c34d04d07c
Yihao Fu <fewdan@hotmail.com>
2020-07-22 01:41:18 +0800

    [update] some experiments related APIs (#713)
    * update experiments
    
    Signed-off-by: “fewdan” <fewdan@hotmail.com>
    
    * update
    
    Signed-off-by: “fewdan” <fewdan@hotmail.com>
    
    * address comment
    
    Signed-off-by: “fewdan” <fewdan@hotmail.com>
    
    Co-authored-by: ti-srebot <66930949+ti-srebot@users.noreply.github.com>

--- pkg/store/experiment/experiment.go
+++ pkg/store/experiment/experiment.go
@@ -150,6 +150,9 @@ func (e *experimentStore) DeleteByFinishTime(_ context.Context, ttl time.Duratio
 	}
 	nowTime := time.Now()
 	for _, exp := range expList {
+		if exp.Archived == false {
+			continue
+		}
 		if exp.FinishTime.Add(ttl).Before(nowTime) {
 			if err := e.db.Table("archive_experiments").Unscoped().Delete(*exp).Error; err != nil {
 				return err
