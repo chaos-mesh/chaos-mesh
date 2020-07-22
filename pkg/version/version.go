7cb40f0611df794f67a0f73c397bf32c8eb9325b
CWen <cwenyin0@gmail.com>
2020-07-22 20:10:32 +0800

    Fix version script (#728)
    * update version script
    
    Signed-off-by: cwen0 <cwenyin0@gmail.com>
    
    * make clean
    
    Signed-off-by: cwen0 <cwenyin0@gmail.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -19,9 +19,8 @@ import (
 )
 
 var (
-	gitVersion   = "v0.0.0-master+$Format:%h$"
-	gitCommit    = "$Format:%H$" // sha1 from git, output of $(git rev-parse HEAD)
-	gitTreeState = ""            // state of git tree, either "clean" or "dirty"
+	gitVersion = "v0.0.0-master+$Format:%h$"
+	gitCommit  = "$Format:%H$" // sha1 from git, output of $(git rev-parse HEAD)
 
 	buildDate = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
 )
@@ -36,12 +35,11 @@ func PrintVersionInfo(name string) {
 func Get() Info {
 	// These variables typically come from -ldflags settings and in
 	return Info{
-		GitVersion:   gitVersion,
-		GitCommit:    gitCommit,
-		GitTreeState: gitTreeState,
-		BuildDate:    buildDate,
-		GoVersion:    runtime.Version(),
-		Compiler:     runtime.Compiler,
-		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+		GitVersion: gitVersion,
+		GitCommit:  gitCommit,
+		BuildDate:  buildDate,
+		GoVersion:  runtime.Version(),
+		Compiler:   runtime.Compiler,
+		Platform:   fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 	}
 }
