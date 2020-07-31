963fd71bb51fd9742815b9242017056453150e3a
YangKeao <keao.yang@yahoo.com>
2020-07-31 18:39:32 +0800

    Use iptables Chain to organize network partition (#721)
    * use iptables chain to org rules
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * format
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * refractor iptables chains
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * back to Always
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * replace copyright
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * lint code
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * fix ci test
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * reset output to none output
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * fix error in iptables test
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * Fix bugs to pass e2e test
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * rename IpSet to IPSet
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * add unit test for compressname
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    * modify test
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>
    
    Co-authored-by: ti-srebot <66930949+ti-srebot@users.noreply.github.com>

--- pkg/chaosdaemon/iptables_server.go
+++ pkg/chaosdaemon/iptables_server.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Chaos Mesh Authors.
+// Copyright 2020 Chaos Mesh Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@ package chaosdaemon
 import (
 	"context"
 	"fmt"
-	"os/exec"
 	"strings"
 
 	"github.com/golang/protobuf/ptypes/empty"
@@ -27,12 +26,13 @@ import (
 const (
 	iptablesCmd = "iptables"
 
-	iptablesBadRuleErr       = "Bad rule (does a matching rule exist in that chain?)."
-	iptablesIPSetNotExistErr = "doesn't exist."
+	iptablesBadRuleErr           = "Bad rule (does a matching rule exist in that chain?)."
+	iptablesIPSetNotExistErr     = "doesn't exist."
+	iptablesChainAlreadyExistErr = "iptables: Chain already exists."
 )
 
-func (s *daemonServer) FlushIptables(ctx context.Context, req *pb.IpTablesRequest) (*empty.Empty, error) {
-	log.Info("Flush iptables rules", "request", req)
+func (s *daemonServer) SetIptablesChains(ctx context.Context, req *pb.IptablesChainsRequest) (*empty.Empty, error) {
+	log.Info("Set iptables chains", "request", req)
 
 	pid, err := s.crClient.GetPidFromContainerID(ctx, req.ContainerId)
 	if err != nil {
@@ -41,31 +41,14 @@ func (s *daemonServer) FlushIptables(ctx context.Context, req *pb.IpTablesReques
 	}
 
 	nsPath := GetNsPath(pid, netNS)
-	rule := req.Rule
 
-	format := ""
-	switch rule.Direction {
-	case pb.Rule_INPUT:
-		format = "%s INPUT -m set --match-set %s src -j DROP -w 5"
-	case pb.Rule_OUTPUT:
-		format = "%s OUTPUT -m set --match-set %s dst -j DROP -w 5"
-	default:
-		return nil, fmt.Errorf("unknown rule direction")
+	iptables := buildIptablesClient(ctx, nsPath)
+	err = iptables.initializeEnv()
+	if err != nil {
+		return nil, err
 	}
 
-	switch rule.Action {
-	case pb.Rule_ADD:
-		command := fmt.Sprintf(format, "-A", rule.Set)
-		cmd := withNetNS(ctx, nsPath, iptablesCmd, strings.Split(command, " ")...)
-		err = s.addIptablesRules(ctx, cmd)
-	case pb.Rule_DELETE:
-		command := fmt.Sprintf(format, "-D", rule.Set)
-		cmd := withNetNS(ctx, nsPath, iptablesCmd, strings.Split(command, " ")...)
-		err = s.deleteIptablesRules(ctx, cmd)
-	default:
-		return nil, fmt.Errorf("unknown rule action")
-	}
-
+	err = iptables.setIptablesChains(req.Chains)
 	if err != nil {
 		return nil, err
 	}
@@ -73,29 +56,162 @@ func (s *daemonServer) FlushIptables(ctx context.Context, req *pb.IpTablesReques
 	return &empty.Empty{}, nil
 }
 
-func (s *daemonServer) addIptablesRules(ctx context.Context, cmd *exec.Cmd) error {
-	log.Info("Add iptables rules", "command", cmd.String())
-
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Error(err, "failed to add iptables rules", "command", cmd.String(), "stdout", string(out))
-		return err
-	}
-
-	return nil
+type iptablesClient struct {
+	ctx    context.Context
+	nsPath string
 }
 
-func (s *daemonServer) deleteIptablesRules(ctx context.Context, cmd *exec.Cmd) error {
-	log.Info("Delete iptables rules", "command", cmd.String())
+type iptablesChain struct {
+	Name  string
+	Rules []string
+}
 
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		output := string(out)
-		if !(strings.Contains(output, iptablesBadRuleErr) || strings.Contains(output, iptablesIPSetNotExistErr)) {
-			log.Error(err, "failed to delete iptables rules", "command", cmd.String(), "output", output)
+func buildIptablesClient(ctx context.Context, nsPath string) iptablesClient {
+	return iptablesClient{
+		ctx,
+		nsPath,
+	}
+}
+
+func (iptables *iptablesClient) setIptablesChains(chains []*pb.Chain) error {
+	for _, chain := range chains {
+		err := iptables.setIptablesChain(chain)
+		if err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func (iptables *iptablesClient) setIptablesChain(chain *pb.Chain) error {
+	var matchPart string
+	if chain.Direction == pb.Chain_INPUT {
+		matchPart = "src"
+	} else if chain.Direction == pb.Chain_OUTPUT {
+		matchPart = "dst"
+	} else {
+		return fmt.Errorf("unknown chain direction %d", chain.Direction)
+	}
+
+	rules := []string{}
+	for _, ipset := range chain.Ipsets {
+		rules = append(rules, fmt.Sprintf("-A %s -m set --match-set %s %s -j DROP -w 5", chain.Name, ipset, matchPart))
+	}
+	err := iptables.createNewChain(&iptablesChain{
+		Name:  chain.Name,
+		Rules: rules,
+	})
+	if err != nil {
+		return err
+	}
+
+	if chain.Direction == pb.Chain_INPUT {
+		err := iptables.ensureRule(&iptablesChain{
+			Name: "CHAOS-INPUT",
+		}, "-A CHAOS-INPUT -j "+chain.Name)
+		if err != nil {
+			return err
+		}
+	} else if chain.Direction == pb.Chain_OUTPUT {
+		iptables.ensureRule(&iptablesChain{
+			Name: "CHAOS-OUTPUT",
+		}, "-A CHAOS-OUTPUT -j "+chain.Name)
+		if err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("unknown direction %d", chain.Direction)
+	}
+	return nil
+}
+
+func (iptables *iptablesClient) initializeEnv() error {
+	for _, direction := range []string{"INPUT", "OUTPUT"} {
+		chainName := "CHAOS-" + direction
+
+		err := iptables.createNewChain(&iptablesChain{
+			Name:  chainName,
+			Rules: []string{},
+		})
+		if err != nil {
+			return err
+		}
+
+		iptables.ensureRule(&iptablesChain{
+			Name:  direction,
+			Rules: []string{},
+		}, "-A "+direction+" -j "+chainName)
+	}
+
+	return nil
+}
+
+// createNewChain will cover existing chain
+func (iptables *iptablesClient) createNewChain(chain *iptablesChain) error {
+	cmd := withNetNS(iptables.ctx, iptables.nsPath, iptablesCmd, "-N", chain.Name)
+	out, err := cmd.CombinedOutput()
+
+	if (err == nil && len(out) == 0) ||
+		(err != nil && strings.Contains(string(out), iptablesChainAlreadyExistErr)) {
+		// Successfully create a new chain
+		return iptables.deleteAndWriteRules(chain)
+	}
+
+	return encodeOutputToError(out, err)
+}
+
+// deleteAndWriteRules will remove all existing function in the chain
+// and replace with the new settings
+func (iptables *iptablesClient) deleteAndWriteRules(chain *iptablesChain) error {
+
+	// This chain should already exist
+	err := iptables.flushIptablesChain(chain)
+	if err != nil {
+		return err
+	}
+
+	for _, rule := range chain.Rules {
+		err := iptables.ensureRule(chain, rule)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (iptables *iptablesClient) ensureRule(chain *iptablesChain, rule string) error {
+	cmd := withNetNS(iptables.ctx, iptables.nsPath, iptablesCmd, "-S", chain.Name)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return encodeOutputToError(out, err)
+	}
+
+	if strings.Contains(string(out), rule) {
+		// The required rule already exist in chain
+		return nil
+	}
+
+	cmd = withNetNS(iptables.ctx, iptables.nsPath, iptablesCmd, strings.Split(rule, " ")...)
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		return encodeOutputToError(out, err)
+	}
+
+	return nil
+}
+
+func (iptables *iptablesClient) flushIptablesChain(chain *iptablesChain) error {
+	cmd := withNetNS(iptables.ctx, iptables.nsPath, iptablesCmd, "-F", chain.Name)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return encodeOutputToError(out, err)
+	}
+
+	return nil
+}
+
+func encodeOutputToError(output []byte, err error) error {
+	return fmt.Errorf("error code: %v, msg: %s", err, string(output))
 }
