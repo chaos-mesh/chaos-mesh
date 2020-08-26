66b7ead86081c8e7e3451db5924df39d2b0e143f
Yang Keao <keao.yang@yahoo.com>
2020-08-26 15:41:44 +0800

    add random rollme annotations
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- install.sh
+++ install.sh
@@ -1206,6 +1206,8 @@ spec:
         app.kubernetes.io/name: chaos-mesh
         app.kubernetes.io/instance: chaos-mesh
         app.kubernetes.io/component: controller-manager
+      annotations:
+        rollme: "SHW0n"
     spec:
       serviceAccount: chaos-controller-manager
       containers:
