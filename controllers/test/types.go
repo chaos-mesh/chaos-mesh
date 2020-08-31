7a34b8582dee85d717238318098bab52b1ac4cbc
xiang <xiang13225080@163.com>
2020-08-31 17:03:10 +0800

    add missed file
    Signed-off-by: xiang <xiang13225080@163.com>

--- controllers/test/types.go
+++ controllers/test/types.go
@@ -82,7 +82,7 @@ func (c *MockChaosDaemonClient) SetTcs(ctx context.Context, in *chaosdaemon.TcsR
 	return nil, mockError("SetTcs")
 }
 
-func (c *MockChaosDaemonClient) SetDNSServer(ctx context.Context, in *SetDNSServerRequest) (*empty.Empty, error) {
+func (c *MockChaosDaemonClient) SetDNSServer(ctx context.Context, in *chaosdaemon.SetDNSServerRequest) (*empty.Empty, error) {
 	return nil, mockError("SetDNSServer")
 }
 
