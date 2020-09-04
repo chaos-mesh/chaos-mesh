1707d9f4b13e91f1b3a776ff0a7089d878ec0bd6
Andrewmatilde <davis6813585853062@outlook.com>
2020-09-04 13:50:14 +0800

    add HTTPchaos CRD API
    Signed-off-by: Andrewmatilde <davis6813585853062@outlook.com>

---
+++ api/v1alpha1/httpchaos_types.go
@@ -0,0 +1,238 @@
+// Copyright 2020 Chaos Mesh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// KindHTTPChaos is the kind for http chaos
+const KindHTTPChaos = "HTTPChaos"
+
+func init() {
+	all.register(KindHTTPChaos, &ChaosKind{
+		Chaos:     &HTTPChaos{},
+		ChaosList: &HTTPChaosList{},
+	})
+}
+
+// HTTPAction represents the chaos action about I/O action.
+type HTTPAction string
+
+const (
+	HTTPDelayAction HTTPAction = "delay"
+	HTTPAbortAction            = "abort"
+	HTTPMixedAction            = "mixed"
+)
+
+type Matcher struct {
+	Name           string `json:"name"`
+	ExactMatch     string `json:"exact_match"`
+	RegexMatch     string `json:"regex_match"`
+	SafeRegexMatch string `json:"safe_regex_match"`
+	RangeMatch     string `json:"range_match"`
+	PresentMatch   string `json:"present_match"`
+	PrefixMatch    string `json:"prefix_match"`
+	SuffixMatch    string `json:"suffix_match"`
+	InvertMatch    string `json:"invert_match"`
+}
+
+type HTTPSpec struct {
+	// Selector is used to select pods that are used to inject chaos action.
+	Selector SelectorSpec `json:"selector"`
+
+	// Scheduler defines some schedule rules to
+	// control the running time of the chaos experiment about pods.
+	Scheduler *SchedulerSpec `json:"scheduler,omitempty"`
+
+	// Action defines the specific pod chaos action.
+	// Supported action: delay | abort | mixed
+	// Default action: delay
+	// +kubebuilder:validation:Enum=delay;abort;mixed
+	Action HTTPAction `json:"action"`
+
+	// Mode defines the mode to run chaos action.
+	// Supported mode: one
+	// +kubebuilder:validation:Enum=one
+	Mode PodMode `json:"mode"`
+
+	// Value is required when the mode is set to `FixedPodMode` / `FixedPercentPodMod` / `RandomMaxPercentPodMod`.
+	// If `FixedPodMode`, provide an integer of pods to do chaos action.
+	// If `FixedPercentPodMod`, provide a number from 0-100 to specify the percent of pods the server can do chaos action.
+	// IF `RandomMaxPercentPodMod`,  provide a number from 0-100 to specify the max percent of pods to do chaos action
+	// +optional
+	Value string `json:"value"`
+
+	// Duration represents the duration of the chaos action.
+	// It is required when the action is `PodFailureAction`.
+	// A duration string is a possibly signed sequence of
+	// decimal numbers, each with optional fraction and a unit suffix,
+	// such as "300ms", "-1.5h" or "2h45m".
+	// Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".
+	// +optional
+	Duration *string `json:"duration,omitempty"`
+
+	// Percent defines the percentage of injection errors and provides a number from 0-100.
+	// default: 100.
+	// +optional
+	Percent string `json:"percent,omitempty"`
+
+	// Specifies how the header match will be performed to route the request.
+	Headers []Matcher `json:"headers,omitempty"`
+}
+
+func (in *HTTPSpec) GetHeaders() []Matcher {
+	return in.Headers
+}
+
+func (in *HTTPSpec) GetMode() PodMode {
+	return in.Mode
+}
+
+func (in *HTTPSpec) GetValue() string {
+	return in.Value
+}
+
+func (in *HTTPSpec) GetSelector() SelectorSpec {
+	return in.Selector
+}
+
+type HTTPChaosStatus struct {
+	ChaosStatus `json:",inline"`
+}
+
+// +kubebuilder:object:root=true
+
+// HTTPChaos is the Schema for the HTTPchaos API
+type HTTPChaos struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HTTPSpec      `json:"spec,omitempty"`
+	Status IoChaosStatus `json:"status,omitempty"`
+}
+
+func (in *HTTPChaos) GetStatus() *ChaosStatus {
+	return &in.Status.ChaosStatus
+}
+
+// IsDeleted returns whether this resource has been deleted
+func (in *HTTPChaos) IsDeleted() bool {
+	return !in.DeletionTimestamp.IsZero()
+}
+
+// IsPaused returns whether this resource has been paused
+func (in *HTTPChaos) IsPaused() bool {
+	if in.Annotations == nil || in.Annotations[PauseAnnotationKey] != "true" {
+		return false
+	}
+	return true
+}
+
+// GetDuration would return the duration for chaos
+func (in *HTTPChaos) GetDuration() (*time.Duration, error) {
+	if in.Spec.Duration == nil {
+		return nil, nil
+	}
+	duration, err := time.ParseDuration(*in.Spec.Duration)
+	if err != nil {
+		return nil, err
+	}
+	return &duration, nil
+}
+
+func (in *HTTPChaos) GetNextStart() time.Time {
+	if in.Status.Scheduler.NextStart == nil {
+		return time.Time{}
+	}
+	return in.Status.Scheduler.NextStart.Time
+}
+
+func (in *HTTPChaos) SetNextStart(t time.Time) {
+	if t.IsZero() {
+		in.Status.Scheduler.NextStart = nil
+		return
+	}
+
+	if in.Status.Scheduler.NextStart == nil {
+		in.Status.Scheduler.NextStart = &metav1.Time{}
+	}
+	in.Status.Scheduler.NextStart.Time = t
+}
+
+func (in *HTTPChaos) GetNextRecover() time.Time {
+	if in.Status.Scheduler.NextRecover == nil {
+		return time.Time{}
+	}
+	return in.Status.Scheduler.NextRecover.Time
+}
+
+func (in *HTTPChaos) SetNextRecover(t time.Time) {
+	if t.IsZero() {
+		in.Status.Scheduler.NextRecover = nil
+		return
+	}
+
+	if in.Status.Scheduler.NextRecover == nil {
+		in.Status.Scheduler.NextRecover = &metav1.Time{}
+	}
+	in.Status.Scheduler.NextRecover.Time = t
+}
+
+// GetScheduler would return the scheduler for chaos
+func (in *HTTPChaos) GetScheduler() *SchedulerSpec {
+	return in.Spec.Scheduler
+}
+
+func (in *HTTPChaos) GetChaos() *ChaosInstance {
+	instance := &ChaosInstance{
+		Name:      in.Name,
+		Namespace: in.Namespace,
+		Kind:      KindHTTPChaos,
+		StartTime: in.CreationTimestamp.Time,
+		Action:    string(in.Spec.Action),
+		UID:       string(in.UID),
+	}
+	if in.Spec.Duration != nil {
+		instance.Duration = *in.Spec.Duration
+	}
+	if in.DeletionTimestamp != nil {
+		instance.EndTime = in.DeletionTimestamp.Time
+	}
+	return instance
+}
+
+// +kubebuilder:object:root=true
+
+// HTTPChaosList contains a list of HTTPChaos
+type HTTPChaosList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []HTTPChaos `json:"items"`
+}
+
+// ListChaos returns a list of io chaos
+func (in *HTTPChaosList) ListChaos() []*ChaosInstance {
+	res := make([]*ChaosInstance, 0, len(in.Items))
+	for _, item := range in.Items {
+		res = append(res, item.GetChaos())
+	}
+	return res
+}
+
+func init() {
+	SchemeBuilder.Register(&HTTPChaos{}, &HTTPChaosList{})
+}
---
+++ controllers/httpchaos/types.go
@@ -0,0 +1,123 @@
+// Copyright 2020 Chaos Mesh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package httpchaos
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"net/http"
+
+	"github.com/go-logr/logr"
+	"golang.org/x/sync/errgroup"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
+	"github.com/chaos-mesh/chaos-mesh/controllers/common"
+	"github.com/chaos-mesh/chaos-mesh/pkg/utils"
+)
+
+type Reconciler struct {
+	client.Client
+	record.EventRecorder
+	Log logr.Logger
+}
+
+func (r *Reconciler) Apply(ctx context.Context, req ctrl.Request, chaos v1alpha1.InnerObject) error {
+	HttpFaultChaos, ok := chaos.(*v1alpha1.HTTPChaos)
+	if !ok {
+		err := errors.New("chaos is not HttpFaultChaos")
+		r.Log.Error(err, "chaos is not HttpFaultChaos", "chaos", chaos)
+		return err
+	}
+
+	pods, err := utils.SelectAndFilterPods(ctx, r.Client, &HttpFaultChaos.Spec)
+	if err != nil {
+		r.Log.Error(err, "failed to select and filter pods")
+		return err
+	}
+	if err = r.applyAllPods(ctx, pods, HttpFaultChaos); err != nil {
+		r.Log.Error(err, "failed to apply chaos on all pods")
+		return err
+	}
+	return nil
+}
+
+func (r *Reconciler) Recover(ctx context.Context, req ctrl.Request, chaos v1alpha1.InnerObject) error {
+	HttpFaultChaos, ok := chaos.(*v1alpha1.KernelChaos)
+	if !ok {
+		err := errors.New("chaos is not KernelChaos")
+		r.Log.Error(err, "chaos is not KernelChaos", "chaos", chaos)
+		return err
+	}
+	r.Event(HttpFaultChaos, v1.EventTypeNormal, utils.EventChaosRecovered, "")
+	return nil
+}
+
+func (r *Reconciler) Object() v1alpha1.InnerObject {
+	return &v1alpha1.HTTPChaos{}
+}
+
+func (r *Reconciler) Reconcile(req ctrl.Request, chaos *v1alpha1.HTTPChaos) (ctrl.Result, error) {
+	r.Log.Info("Reconciling HttpFaultChaos")
+	duration, err := chaos.GetDuration()
+	if err != nil {
+		msg := fmt.Sprintf("unable to get iochaos[%s/%s]'s duration",
+			req.Namespace, req.Name)
+		r.Log.Error(err, msg)
+		return ctrl.Result{}, err
+	}
+
+	if duration != nil {
+		return r.commonHttpFaultChaos(chaos, req)
+	}
+	err = fmt.Errorf("HttpFaultChaos[%s/%s] spec invalid", req.Namespace, req.Name)
+	r.Log.Error(err, "scheduler and duration should be omitted or defined at the same time")
+	return ctrl.Result{}, err
+}
+
+func (r *Reconciler) commonHttpFaultChaos(HttpFaultChaos *v1alpha1.HTTPChaos, req ctrl.Request) (ctrl.Result, error) {
+	cr := common.NewReconciler(r, r.Client, r.Log)
+	return cr.Reconcile(req)
+}
+
+func (r *Reconciler) applyAllPods(ctx context.Context, pods []v1.Pod, chaos *v1alpha1.HTTPChaos) error {
+	g := errgroup.Group{}
+	for index := range pods {
+		pod := &pods[index]
+
+		key, err := cache.MetaNamespaceKeyFunc(pod)
+		if err != nil {
+			return err
+		}
+		chaos.Finalizers = utils.InsertFinalizer(chaos.Finalizers, key)
+
+		g.Go(func() error {
+			return r.applyPod(ctx, pod, chaos)
+		})
+	}
+
+	return g.Wait()
+}
+
+func (r *Reconciler) applyPod(ctx context.Context, pod *v1.Pod, chaos *v1alpha1.HTTPChaos) error {
+	r.Log.Info("Try to inject kernel on pod", "namespace", pod.Namespace, "name", pod.Name)
+	_, err := http.Get("http://" + pod.Status.PodIP + ":15000/delay?" + "open=true&duration=" + *chaos.Spec.Duration)
+	return err
+}
