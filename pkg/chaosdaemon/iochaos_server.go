d04fe03e88e7647093e43e86898e1f385dca26d8
Yang Keao <keao.yang@yahoo.com>
2020-09-09 14:41:40 +0800

    change log level
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- pkg/chaosdaemon/iochaos_server.go
+++ pkg/chaosdaemon/iochaos_server.go
@@ -57,7 +57,8 @@ func (s *daemonServer) ApplyIoChaos(ctx context.Context, in *pb.ApplyIoChaosRequ
 		return nil, err
 	}
 
-	args := fmt.Sprintf("--path %s --pid %d --verbose trace", in.Volume, pid)
+	// TODO: make this log level configurable
+	args := fmt.Sprintf("--path %s --pid %d --verbose info", in.Volume, pid)
 	log.Info("executing", "cmd", todaBin+" "+args)
 	cmd := bpm.DefaultProcessBuilder(todaBin, strings.Split(args, " ")...).
 		EnableSuicide().
