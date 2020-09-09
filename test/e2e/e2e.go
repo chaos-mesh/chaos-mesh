9d6e5c6ebf99675bab8a10d3135b98e9fd298121
Yang Keao <keao.yang@yahoo.com>
2020-09-09 19:05:30 +0800

    restore e2e test
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- test/e2e/e2e.go
+++ test/e2e/e2e.go
@@ -20,13 +20,18 @@ import (
 
 	"github.com/onsi/ginkgo"
 	v1 "k8s.io/api/core/v1"
+	apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog"
+	aggregatorclientset "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset"
 	"k8s.io/kubernetes/test/e2e/framework"
 	e2elog "k8s.io/kubernetes/test/e2e/framework/log"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
 	utilnet "k8s.io/utils/net"
+
+	"github.com/chaos-mesh/chaos-mesh/test"
+	e2econfig "github.com/chaos-mesh/chaos-mesh/test/e2e/config"
 
 	// ensure auth plugins are loaded
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -123,26 +128,26 @@ var _ = ginkgo.SynchronizedBeforeSuite(func() []byte {
 	setupSuite()
 
 	// Get clients
-	// config, err := framework.LoadConfig()
-	// framework.ExpectNoError(err, "failed to load config")
-	// kubeCli, err := kubernetes.NewForConfig(config)
-	// framework.ExpectNoError(err, "failed to create clientset")
-	// aggrCli, err := aggregatorclientset.NewForConfig(config)
-	// framework.ExpectNoError(err, "failed to create clientset")
-	// apiExtCli, err := apiextensionsclientset.NewForConfig(config)
-	// framework.ExpectNoError(err, "failed to create clientset")
-	// oa := test.NewOperatorAction(kubeCli, aggrCli, apiExtCli, e2econfig.TestConfig)
-	// ocfg := test.NewDefaultOperatorConfig()
-	// ocfg.Manager.Image = e2econfig.TestConfig.ManagerImage
-	// ocfg.Manager.Tag = e2econfig.TestConfig.ManagerTag
-	// ocfg.Daemon.Image = e2econfig.TestConfig.DaemonImage
-	// ocfg.Daemon.Tag = e2econfig.TestConfig.DaemonTag
+	config, err := framework.LoadConfig()
+	framework.ExpectNoError(err, "failed to load config")
+	kubeCli, err := kubernetes.NewForConfig(config)
+	framework.ExpectNoError(err, "failed to create clientset")
+	aggrCli, err := aggregatorclientset.NewForConfig(config)
+	framework.ExpectNoError(err, "failed to create clientset")
+	apiExtCli, err := apiextensionsclientset.NewForConfig(config)
+	framework.ExpectNoError(err, "failed to create clientset")
+	oa := test.NewOperatorAction(kubeCli, aggrCli, apiExtCli, e2econfig.TestConfig)
+	ocfg := test.NewDefaultOperatorConfig()
+	ocfg.Manager.Image = e2econfig.TestConfig.ManagerImage
+	ocfg.Manager.Tag = e2econfig.TestConfig.ManagerTag
+	ocfg.Daemon.Image = e2econfig.TestConfig.DaemonImage
+	ocfg.Daemon.Tag = e2econfig.TestConfig.DaemonTag
 
-	// oa.CleanCRDOrDie()
-	// err = oa.InstallCRD(ocfg)
-	// framework.ExpectNoError(err, "failed to install crd")
-	// err = oa.DeployOperator(ocfg)
-	// framework.ExpectNoError(err, "failed to install chaos-mesh")
+	oa.CleanCRDOrDie()
+	err = oa.InstallCRD(ocfg)
+	framework.ExpectNoError(err, "failed to install crd")
+	err = oa.DeployOperator(ocfg)
+	framework.ExpectNoError(err, "failed to install chaos-mesh")
 	return nil
 }, func(data []byte) {
 	// Run on all Ginkgo nodes
