d675c06b95673dfc1320eeeb10c2d51c829695b7
“fewdan” <fewdan@hotmail.com>
2020-09-17 01:52:43 +0800

    update
    Signed-off-by: “fewdan” <fewdan@hotmail.com>

--- api/v1alpha1/networkchaos_webhook.go
+++ api/v1alpha1/networkchaos_webhook.go
@@ -281,7 +281,7 @@ func (in *BandwidthSpec) validateBandwidth(bandwidth *field.Path) field.ErrorLis
 
 // validateTarget validates the target
 func (in *Target) validateTarget(target *field.Path) field.ErrorList {
-	if in != nil && in.TargetMode != OnePodMode && in.TargetMode != AllPodMode && in.TargetMode != FixedPodMode && in.TargetMode != FixedPercentPodMode && in.TargetMode != RandomMaxPercentPodMode {
+	if in.TargetMode != OnePodMode && in.TargetMode != AllPodMode && in.TargetMode != FixedPodMode && in.TargetMode != FixedPercentPodMode && in.TargetMode != RandomMaxPercentPodMode {
 		return field.ErrorList{field.Invalid(target.Child("mode"), in.TargetMode,
 			fmt.Sprintf("mode %s not supported", in.TargetMode))}
 	}
--- install.sh
+++ install.sh
@@ -1077,7 +1077,7 @@ spec:
       containers:
         - name: chaos-daemon
           image: pingcap/chaos-daemon:latest
-          imagePullPolicy: IfNotPresent
+          imagePullPolicy: Never
           command:
             - /usr/local/bin/chaos-daemon
             - --runtime
@@ -1202,7 +1202,7 @@ spec:
       containers:
       - name: chaos-mesh
         image: pingcap/chaos-mesh:latest
-        imagePullPolicy: IfNotPresent
+        imagePullPolicy: Never
         resources:
             limits: {}
             requests:
