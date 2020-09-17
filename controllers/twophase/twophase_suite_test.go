e082715bfe7bbe29541ab102e3f120d29175b774
“fewdan” <fewdan@hotmail.com>
2020-09-17 15:32:56 +0800

    fix CI
    Signed-off-by: “fewdan” <fewdan@hotmail.com>

--- controllers/twophase/twophase_suite_test.go
+++ controllers/twophase/twophase_suite_test.go
@@ -414,7 +414,7 @@ var _ = Describe("TwoPhase", func() {
 
 			Expect(err).ToNot(HaveOccurred())
 			_chaos := r.Object()
-			err = r.Get(context.TODO(), req.NamespacedName, _chaos)
+			err = r.Client.Get(context.TODO(), req.NamespacedName, _chaos)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(_chaos.(v1alpha1.InnerSchedulerObject).GetStatus().Experiment.Phase).To(Equal(v1alpha1.ExperimentPhaseRunning))
 
@@ -425,7 +425,7 @@ var _ = Describe("TwoPhase", func() {
 
 			_, err = r.Reconcile(req)
 			Expect(err).ToNot(HaveOccurred())
-			err = r.Get(context.TODO(), req.NamespacedName, _chaos)
+			err = r.Client.Get(context.TODO(), req.NamespacedName, _chaos)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(_chaos.(v1alpha1.InnerSchedulerObject).GetStatus().Experiment.Phase).To(Equal(v1alpha1.ExperimentPhaseRunning))
 			d, _ := time.ParseDuration("10h")
