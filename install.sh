cc288f001b5ef37114ba0ea46a14e339797e1222
CWen <cwenyin0@gmail.com>
2020-10-13 23:46:26 +0800

    cherry-pick pr 954 (#1044)
    * add host-network flag in install.sh (#954)
    
    * fix install shell bug (#1028)
    
    Co-authored-by: Ge Song <soog2008@hotmail.com>
    Co-authored-by: Mike <842725815@qq.com>

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ FLAGS:
         --local-registry     Deploy local docker registry in local Kubernetes cluster
         --template           Locally render templates
         --k3s                Install chaos-mesh in k3s environment
+        --host-network       Install chaos-mesh using hostNetwork
 OPTIONS:
     -v, --version            Version of chaos-mesh, default value: v1.0.0
     -l, --local [kind]       Choose a way to run a local kubernetes cluster, supported value: kind,
@@ -79,6 +80,7 @@ main() {
     local template=false
     local install_dependency_only=false
     local k3s=false
+    local host_network=false
 
     while [[ $# -gt 0 ]]
     do
@@ -115,7 +117,6 @@ main() {
                 ;;
             -d|--dependency-only)
                 install_dependency_only=true
-                shift
                 shift
                 ;;
             --force)
@@ -190,6 +191,9 @@ main() {
             --k3s)
                 k3s=true
                 shift
+                ;;
+            --host-network)
+                host_network=true
                 shift
                 ;;
             --timezone)
@@ -229,7 +233,7 @@ main() {
 
     if $template; then
         ensure gen_crd_manifests "${crd}"
-        ensure gen_chaos_mesh_manifests "${runtime}" "${k3s}" "${cm_version}" "${timezone}"
+        ensure gen_chaos_mesh_manifests "${runtime}" "${k3s}" "${cm_version}" "${timezone}" "${host_network}"
         exit 0
     fi
 
@@ -625,7 +629,7 @@ install_chaos_mesh() {
     fi
 
     gen_crd_manifests "${crd}" | kubectl apply -f - || exit 1
-    gen_chaos_mesh_manifests "${runtime}" "${k3s}" "${version}" "${timezone}" | kubectl apply -f - || exit 1
+    gen_chaos_mesh_manifests "${runtime}" "${k3s}" "${version}" "${timezone}" "${host_network}" | kubectl apply -f - || exit 1
 }
 
 version_lt() {
@@ -813,6 +817,7 @@ gen_chaos_mesh_manifests() {
     local k3s=$2
     local version=$3
     local timezone=$4
+    local host_network=$5
 
     local socketPath="/var/run/docker.sock"
     local mountPath="/var/run/docker.sock"
@@ -1090,6 +1095,7 @@ spec:
         app.kubernetes.io/instance: chaos-mesh
         app.kubernetes.io/component: chaos-daemon
     spec:
+      hostNetwork: ${host_network}
       hostIPC: true
       hostPID: true
       containers:
@@ -1216,6 +1222,7 @@ spec:
       annotations:
         rollme: "install.sh"
     spec:
+      hostNetwork: ${host_network}
       serviceAccount: chaos-controller-manager
       containers:
       - name: chaos-mesh
