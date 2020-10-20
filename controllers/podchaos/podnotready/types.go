efacf6bc3a463791b0045ecfa9aa836350e08931
mikechengwei <842725815@qq.com>
2020-10-21 00:16:50 +0800

    notready init
    Signed-off-by: mikechengwei <842725815@qq.com>

--- controllers/podchaos/podnotready/types.go
+++ controllers/podchaos/podnotready/types.go
@@ -17,22 +17,22 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/go-logr/logr"
+	"github.com/chaos-mesh/chaos-mesh/pkg/router"
+	ctx "github.com/chaos-mesh/chaos-mesh/pkg/router/context"
+	end "github.com/chaos-mesh/chaos-mesh/pkg/router/endpoint"
 	"github.com/hashicorp/go-multierror"
 	v1 "k8s.io/api/core/v1"
 	k8serror "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"time"
 
 	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
 	"github.com/chaos-mesh/chaos-mesh/controllers/common"
-	"github.com/chaos-mesh/chaos-mesh/controllers/twophase"
 	"github.com/chaos-mesh/chaos-mesh/pkg/utils"
 )
 
@@ -42,41 +42,17 @@ const (
 	ChaosMeshInjectNotReady v1.PodConditionType = "ChaosMeshInjectNotReady"
 )
 
-// NewTwoPhaseReconciler would create Reconciler for twophase package
-func NewTwoPhaseReconciler(c client.Client, reader client.Reader, log logr.Logger, recorder record.EventRecorder) *twophase.Reconciler {
-	r := newReconciler(c, reader, log, recorder)
-	return twophase.NewReconciler(r, r.Client, r.Reader, r.Log)
-}
-
-// NewCommonReconciler would create Reconciler for common package
-func NewCommonReconciler(c client.Client, reader client.Reader, log logr.Logger, recorder record.EventRecorder) *common.Reconciler {
-	r := newReconciler(c, reader, log, recorder)
-	return common.NewReconciler(r, r.Client, r.Reader, r.Log)
-}
-
-func newReconciler(c client.Client, r client.Reader, log logr.Logger, recorder record.EventRecorder) *Reconciler {
-	return &Reconciler{
-		Client:        c,
-		Reader:        r,
-		EventRecorder: recorder,
-		Log:           log,
-	}
-}
-
-type Reconciler struct {
-	client.Client
-	client.Reader
-	record.EventRecorder
-	Log logr.Logger
+type endpoint struct {
+	ctx.Context
 }
 
 // Object implements the reconciler.InnerReconciler.Object
-func (r *Reconciler) Object() v1alpha1.InnerObject {
+func (r *endpoint) Object() v1alpha1.InnerObject {
 	return &v1alpha1.PodChaos{}
 }
 
 // Apply implements the reconciler.InnerReconciler.Apply
-func (r *Reconciler) Apply(ctx context.Context, req ctrl.Request, chaos v1alpha1.InnerObject) error {
+func (r *endpoint) Apply(ctx context.Context, req ctrl.Request, chaos v1alpha1.InnerObject) error {
 
 	podchaos, ok := chaos.(*v1alpha1.PodChaos)
 	if !ok {
@@ -113,7 +89,7 @@ func (r *Reconciler) Apply(ctx context.Context, req ctrl.Request, chaos v1alpha1
 	return nil
 }
 
-func (r *Reconciler) setAllPodsNotReady(ctx context.Context, pods []v1.Pod, podchaos *v1alpha1.PodChaos) error {
+func (r *endpoint) setAllPodsNotReady(ctx context.Context, pods []v1.Pod, podchaos *v1alpha1.PodChaos) error {
 	for index := range pods {
 		pod := &pods[index]
 		if containsReadinessGate(pod) {
@@ -231,7 +207,7 @@ func removeReadinessGate(pod *v1.Pod) {
 }
 
 // Recover implements the reconciler.InnerReconciler.Recover
-func (r *Reconciler) Recover(ctx context.Context, req ctrl.Request, obj v1alpha1.InnerObject) error {
+func (r *endpoint) Recover(ctx context.Context, req ctrl.Request, obj v1alpha1.InnerObject) error {
 
 	podchaos, ok := obj.(*v1alpha1.PodChaos)
 	if !ok {
@@ -248,7 +224,7 @@ func (r *Reconciler) Recover(ctx context.Context, req ctrl.Request, obj v1alpha1
 	return nil
 }
 
-func (r *Reconciler) setPodReadyAndRecover(ctx context.Context, podchaos *v1alpha1.PodChaos) error {
+func (r *endpoint) setPodReadyAndRecover(ctx context.Context, podchaos *v1alpha1.PodChaos) error {
 	var result error
 
 	for _, key := range podchaos.Finalizers {
@@ -293,7 +269,7 @@ func (r *Reconciler) setPodReadyAndRecover(ctx context.Context, podchaos *v1alph
 	return result
 }
 
-func (r *Reconciler) recoverPod(ctx context.Context, pod *v1.Pod, podchaos *v1alpha1.PodChaos) error {
+func (r *endpoint) recoverPod(ctx context.Context, pod *v1.Pod, podchaos *v1alpha1.PodChaos) error {
 	r.Log.Info("Recovering", "namespace", pod.Namespace, "name", pod.Name)
 	clonePod := &v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
@@ -353,4 +329,19 @@ func containsReadinessGate(pod *v1.Pod) bool {
 		}
 	}
 	return false
+}
+
+func init() {
+	router.Register("podchaos", &v1alpha1.PodChaos{}, func(obj runtime.Object) bool {
+		chaos, ok := obj.(*v1alpha1.PodChaos)
+		if !ok {
+			return false
+		}
+
+		return chaos.Spec.Action == v1alpha1.PodNotReadyAction
+	}, func(ctx ctx.Context) end.Endpoint {
+		return &endpoint{
+			Context: ctx,
+		}
+	})
 }
