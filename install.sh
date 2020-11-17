76230bf04a680dfc5f6aad42a1b2c8081e3ba783
YangKeao <keao.yang@yahoo.com>
2020-11-17 18:48:58 +0800

    Add microk8s support for chaos-mesh install.sh script (#1181)

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ FLAGS:
         --local-registry     Deploy local docker registry in local Kubernetes cluster
         --template           Locally render templates
         --k3s                Install chaos-mesh in k3s environment
+        --microk8s           Install chaos-mesh in microk8s environment
         --host-network       Install chaos-mesh using hostNetwork
 OPTIONS:
     -v, --version            Version of chaos-mesh, default value: latest
@@ -80,6 +81,7 @@ main() {
     local template=false
     local install_dependency_only=false
     local k3s=false
+    local microk8s=false
     local host_network=false
     local docker_registry="docker.io"
 
@@ -193,6 +195,10 @@ main() {
                 k3s=true
                 shift
                 ;;
+            --microk8s)
+                microk8s=true
+                shift
+                ;;
             --host-network)
                 host_network=true
                 shift
@@ -233,12 +239,16 @@ main() {
         runtime="containerd"
     fi
 
+    if [ "${microk8s}" == "true" ]; then
+        runtime="containerd"
+    fi
+
     if [ "${crd}" == "" ]; then
         crd="https://mirrors.chaos-mesh.org/${cm_version}/crd.yaml"
     fi
     if $template; then
         ensure gen_crd_manifests "${crd}"
-        ensure gen_chaos_mesh_manifests "${runtime}" "${k3s}" "${cm_version}" "${timezone}" "${host_network}" "${docker_registry}"
+        ensure gen_chaos_mesh_manifests "${runtime}" "${k3s}" "${cm_version}" "${timezone}" "${host_network}" "${docker_registry}" "${microk8s}"
         exit 0
     fi
 
@@ -259,7 +269,7 @@ main() {
     fi
 
     check_kubernetes
-    install_chaos_mesh "${release_name}" "${namespace}" "${local_kube}" ${force_chaos_mesh} ${docker_mirror} "${crd}" "${runtime}" "${k3s}" "${cm_version}" "${timezone}" "${docker_registry}"
+    install_chaos_mesh "${release_name}" "${namespace}" "${local_kube}" ${force_chaos_mesh} ${docker_mirror} "${crd}" "${runtime}" "${k3s}" "${cm_version}" "${timezone}" "${docker_registry}" "${microk8s}"
     ensure_pods_ready "${namespace}" "app.kubernetes.io/component=controller-manager" 100
     ensure_pods_ready "${namespace}" "app.kubernetes.io/component=chaos-daemon" 100
     ensure_pods_ready "${namespace}" "app.kubernetes.io/component=chaos-dashboard" 100
@@ -616,6 +626,7 @@ install_chaos_mesh() {
     local version=$9
     local timezone=${10}
     local docker_registry=${11}
+    local microk8s=${12}
     printf "Install Chaos Mesh %s\n" "${release_name}"
 
     local chaos_mesh_image="${docker_registry}/pingcap/chaos-mesh:${version}"
@@ -634,7 +645,7 @@ install_chaos_mesh() {
     fi
 
     gen_crd_manifests "${crd}" | kubectl apply -f - || exit 1
-    gen_chaos_mesh_manifests "${runtime}" "${k3s}" "${version}" "${timezone}" "${host_network}" "${docker_registry}" | kubectl apply -f - || exit 1
+    gen_chaos_mesh_manifests "${runtime}" "${k3s}" "${version}" "${timezone}" "${host_network}" "${docker_registry}" "${microk8s}" | kubectl apply -f - || exit 1
 }
 
 version_lt() {
@@ -824,6 +835,7 @@ gen_chaos_mesh_manifests() {
     local timezone=$4
     local host_network=$5
     local docker_registry=$6
+    local microk8s=$7
     local socketPath="/var/run/docker.sock"
     local mountPath="/var/run/docker.sock"
     if [ "${runtime}" == "containerd" ]; then
@@ -833,6 +845,11 @@ gen_chaos_mesh_manifests() {
 
     if [ "${k3s}" == "true" ]; then
         socketPath="/run/k3s/containerd/containerd.sock"
+        mountPath="/run/containerd/containerd.sock"
+    fi
+
+    if [ "${microk8s}" == "true" ]; then
+        socketPath="/var/snap/microk8s/common/run/containerd.sock"
         mountPath="/run/containerd/containerd.sock"
     fi
 
