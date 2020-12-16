deb6a82c09a58946c010169ca8b34c700e6b98dd
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2020-12-16 22:21:31 +0800

    fix: remove rbac deprecation (#1296)
    Signed-off-by: vincent178 <vh7157@gmail.com>
    
    Co-authored-by: vincent178 <vh7157@gmail.com>

--- install.sh
+++ install.sh
@@ -907,7 +907,7 @@ data:
 # Source: chaos-mesh/templates/controller-manager-rbac.yaml
 # roles
 kind: ClusterRole
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   name: chaos-mesh:chaos-controller-manager-target-namespace
   labels:
@@ -938,7 +938,7 @@ rules:
 ---
 # Source: chaos-mesh/templates/controller-manager-rbac.yaml
 kind: ClusterRole
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   name: chaos-mesh:chaos-controller-manager-cluster-level
   labels:
@@ -955,7 +955,7 @@ rules:
 # Source: chaos-mesh/templates/controller-manager-rbac.yaml
 # bindings cluster level
 kind: ClusterRoleBinding
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   name: chaos-mesh:chaos-controller-manager-cluster-level
   labels:
@@ -973,7 +973,7 @@ subjects:
 ---
 # Source: chaos-mesh/templates/controller-manager-rbac.yaml
 kind: ClusterRoleBinding
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   name: chaos-mesh:chaos-controller-manager-target-namespace
   namespace: chaos-testing
@@ -992,7 +992,7 @@ subjects:
 ---
 # Source: chaos-mesh/templates/controller-manager-rbac.yaml
 kind: Role
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   name: chaos-mesh:chaos-controller-manager-control-plane
   namespace: chaos-testing
@@ -1008,7 +1008,7 @@ rules:
 # Source: chaos-mesh/templates/controller-manager-rbac.yaml
 # binding for control plane namespace
 kind: RoleBinding
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   name: chaos-mesh:chaos-controller-manager-control-plane
   namespace: chaos-testing
