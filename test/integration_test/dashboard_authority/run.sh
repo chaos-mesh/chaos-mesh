1cfc7bbc1c8c059614f6b76df8099c1cd455f445
xiang <xiang13225080@163.com>
2020-12-31 12:41:05 +0800

    use funtion to send request and check
    Signed-off-by: xiang <xiang13225080@163.com>

--- test/integration_test/dashboard_authority/run.sh
+++ test/integration_test/dashboard_authority/run.sh
@@ -15,23 +15,26 @@
 
 set -eu
 
-echo $PATH
-
 cur=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $cur
 
 echo "deploy deployments for test"
-#kubectl apply -f https://raw.githubusercontent.com/chaos-mesh/apps/master/ping/busybox-statefulset.yaml
+kubectl apply -f https://raw.githubusercontent.com/chaos-mesh/apps/master/ping/busybox-statefulset.yaml
 
 echo "create rbac and get token"
 
-kubectl apply -f ./cluster-manager.yaml
-kubectl apply -f ./cluster-viewer.yaml
-kubectl apply -f ./busybox-manager.yaml
-kubectl apply -f ./busybox-viewer.yaml
+#kubectl delete -f ./cluster-manager.yaml
+#kubectl delete -f ./cluster-viewer.yaml
+#kubectl delete -f ./busybox-manager.yaml
+#kubectl delete -f ./busybox-viewer.yaml
+
+#kubectl apply -f ./cluster-manager.yaml
+#kubectl apply -f ./cluster-viewer.yaml
+#kubectl apply -f ./busybox-manager.yaml
+#kubectl apply -f ./busybox-viewer.yaml
 
 CLUSTER_MANAGER_TOKEN=`kubectl -n chaos-testing describe secret $(kubectl -n chaos-testing get secret | grep account-cluster-manager | awk '{print $1}') | grep "token:" | awk '{print $2}'`
-CLUSTER_VIEWER_TOKEN=`kubectl -n chaos-testing describe secret $(kubectl -n chaos-testing get secret | grep account-busybox-viewer | awk '{print $1}') | grep "token:" | awk '{print $2}'`
+CLUSTER_VIEWER_TOKEN=`kubectl -n chaos-testing describe secret $(kubectl -n chaos-testing get secret | grep account-cluster-viewer | awk '{print $1}') | grep "token:" | awk '{print $2}'`
 BUSYBOX_MANAGER_TOKEN=`kubectl -n busybox describe secret $(kubectl -n busybox get secret | grep account-busybox-manager | awk '{print $1}') | grep "token:" | awk '{print $2}'`
 BUSYBOX_VIEWER_TOKEN=`kubectl -n busybox describe secret $(kubectl -n busybox get secret | grep account-busybox-viewer | awk '{print $1}') | grep "token:" | awk '{print $2}'`
 
@@ -43,44 +46,77 @@ BUSYBOX_VIEW_TOKEN_LIST=($CLUSTER_MANAGER_TOKEN $CLUSTER_VIEWER_TOKEN $BUSYBOX_M
 
 CLUSTER_MANAGER_TOKEN_LIST=
 
-EXP_JSON='{"name": "test", "namespace": "busybox", "scope": {"mode":"one", "namespace_selectors": ["busybox"]}, "target": {"kind": "NetworkChaos", "network_chaos": {"action": "delay", "delay": {"latency": "1ms"}}}}'
+EXP_JSON='{"name": "ci-test", "namespace": "busybox", "scope": {"mode":"one", "namespace_selectors": ["busybox"]}, "target": {"kind": "NetworkChaos", "network_chaos": {"action": "delay", "delay": {"latency": "1ms"}}}}'
+
+function request() {
+    declare -a TOKEN_LIST=("${!1}")
+    #TOKEN_LIST=$1
+    METHOD=$2
+    URL=$3
+    #param2=("${!2}")
+    
+    CHECK_LOG=$4
+    echo $METHOD
+    echo $URL
+    echo $TOKEN_LIST
+    echo $CHECK_LOG
+
+    for(( i=0;i<${#TOKEN_LIST[@]};i++)) do
+        echo $i
+        echo ${TOKEN_LIST[i]}
+        curl -X POST "localhost:2333$URL" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN_LIST[i]}" -d "${EXP_JSON}"  > test.out
+        check_contains "$CHECK_LOG" "test.out"
+    done
+}
 
 echo "***** create chaos experiments *****"
 
 echo "viewer is forbidden to create experiments"
+
+request BUSYBOX_MANAGER_FORBIDDEN_TOKEN_LIST[@] "POST" "/api/experiments/new" "is forbidden" 
+
+exit 0
+
 for(( i=0;i<${#BUSYBOX_MANAGER_FORBIDDEN_TOKEN_LIST[@]};i++)) do
+    echo $i
     curl -X POST "localhost:2333/api/experiments/new" -H "Content-Type: application/json" -H "Authorization: Bearer ${BUSYBOX_MANAGER_FORBIDDEN_TOKEN_LIST[i]}" -d "${EXP_JSON}"  > create_exp.out
     check_contains "is forbidden" "create_exp.out"
 done
 
-#curl -X POST "localhost:2333/api/experiments/new" -H "Content-Type: application/json" -H "Authorization: Bearer ${CLUSTER_VIEWER_TOKEN}" -d "${EXP_JSON}"  > create_exp.out
-#check_contains "is forbidden" "create_exp.out"
-#curl -X POST "localhost:2333/api/experiments/new" -H "Content-Type: application/json" -H "Authorization: Bearer ${BUSYBOX_VIEWER_TOKEN}" -d "${EXP_JSON}"  > create_exp.out
-#check_contains "is forbidden" "create_exp.out"
-exit 0
 echo "only manager can create experiments success"
 curl -X POST "localhost:2333/api/experiments/new" -H "Content-Type: application/json" -H "Authorization: Bearer ${BUSYBOX_MANAGER_TOKEN}" -d "${EXP_JSON}" > create_exp.out
-check_contains '"name":"test"' "create_exp.out"
+check_contains '"name":"ci-test"' "create_exp.out"
 
 echo "***** list chaos experiments *****"
 
 echo "all token can list experiments under namespace busybox success"
-curl -X GET "localhost:2333/api/experiments?namespace=busybox" -H "Authorization: Bearer ${BUSYBOX_MANAGER_TOKEN}" > list_exp.out
-check_contains '"name":"test"' "list_exp.out"
+for(( i=0;i<${#BUSYBOX_VIEW_TOKEN_LIST[@]};i++)) do
+    curl -X GET "localhost:2333/api/experiments?namespace=busybox" -H "Authorization: Bearer ${BUSYBOX_VIEW_TOKEN_LIST[i]}" > list_exp.out
+    check_contains '"name":"ci-test"' "list_exp.out"
+done
+
 EXP_UID=`cat list_exp.out | sed 's/.*\"uid\":\"\([0-9,a-z,-]*\)\".*/\1/g'`
 
 echo "cluster manager and viewer can list all chaos experiments in the cluster"
-curl -X GET "localhost:2333/api/experiments?namespace=busybox" -H "Authorization: Bearer ${CLUSTER_VIEWER_TOKEN}" > list_exp.out
-check_contains '"name":"test"' "list_exp.out"
+for(( i=0;i<${#CLUSTER_VIEW_TOKEN_LIST[@]};i++)) do
+    curl -X GET "localhost:2333/api/experiments" -H "Authorization: Bearer ${CLUSTER_VIEW_TOKEN_LIST[i]}" > list_exp.out
+    check_contains '"name":"ci-test"' "list_exp.out"
+done
 
-curl -X GET "localhost:2333/api/experiments?namespace=busybox" -H "Authorization: Bearer ${BUSYBOX_MANAGER_TOKEN}" > list_exp.out
-check_contains '"name":"test"' "list_exp.out"
+echo "busybox manager and viewer is forbidden to list chaos experiments in the cluster or other namespace"
+for(( i=0;i<${#CLUSTER_VIEW_FORBIDDEN_TOKEN_LIST[@]};i++)) do
+    curl -X GET "localhost:2333/api/experiments" -H "Authorization: Bearer ${CLUSTER_VIEW_FORBIDDEN_TOKEN_LIST[i]}" > list_exp.out
+    check_contains "is forbidden" "list_exp.out"
 
+    curl -X GET "localhost:2333/api/experiments?namespace=default" -H "Authorization: Bearer ${CLUSTER_VIEW_FORBIDDEN_TOKEN_LIST[i]}" > list_exp.out
+    check_contains "is forbidden" "list_exp.out"
+done
 
-echo "get details of chaos experiments"
+echo "***** get details of chaos experiments *****"
+
 curl -X GET "localhost:2333/api/experiments/detail/${EXP_UID}" -H "Authorization: Bearer ${BUSYBOX_MANAGER_TOKEN}" > detail_exp.out
 check_contains "Running" "detail_exp.out"
-# {"kind":"","namespace":"busybox","name":"test","uid":"fc6bbe96-f251-47f2-a30c-bed1fe04bcf9","created":"2020-12-29T08:41:06Z","status":"Running","yaml":{"apiVersion":"","kind":"","metadata":{"name":"test","namespace":"busybox","labels":null,"annotations":{"experiment.chaos-mesh.org/pause":"false"}},"spec":{"action":"delay","mode":"one","value":"","selector":{"namespaces":["busybox"]},"delay":{"latency":"1ms","correlation":"0","jitter":"0ms"},"direction":"to"}}}
+# {"kind":"","namespace":"busybox","name":"ci-test","uid":"fc6bbe96-f251-47f2-a30c-bed1fe04bcf9","created":"2020-12-29T08:41:06Z","status":"Running","yaml":{"apiVersion":"","kind":"","metadata":{"name":"ci-test","namespace":"busybox","labels":null,"annotations":{"experiment.chaos-mesh.org/pause":"false"}},"spec":{"action":"delay","mode":"one","value":"","selector":{"namespaces":["busybox"]},"delay":{"latency":"1ms","correlation":"0","jitter":"0ms"},"direction":"to"}}}
 
 echo "get state"
 curl -X GET "localhost:2333/api/experiments/state?namespace=busybox" -H "Authorization: Bearer ${BUSYBOX_MANAGER_TOKEN}" > state.out
@@ -121,15 +157,15 @@ echo "list archive chaos experiments"
 
 curl -X GET "localhost:2333/api/archives?namespace=busybox" -H "Authorization: Bearer ${BUSYBOX_MANAGER_TOKEN}" > list_archives.out
 
-# [{"uid":"1fce3d8b-44d8-4906-ae31-eeaefd443a60","kind":"NetworkChaos","namespace":"busybox","name":"test","action":"delay","start_time":"2020-12-29T08:23:22Z","finish_time":"2020-12-29T08:40:08.0139053Z"},{"uid":"fc6bbe96-f251-47f2-a30c-bed1fe04bcf9","kind":"NetworkChaos","namespace":"busybox","name":"test","action":"delay","start_time":"2020-12-29T08:41:06Z","finish_time":"2020-12-29T09:14:57.6823095Z"}]
+# [{"uid":"1fce3d8b-44d8-4906-ae31-eeaefd443a60","kind":"NetworkChaos","namespace":"busybox","name":"ci-test","action":"delay","start_time":"2020-12-29T08:23:22Z","finish_time":"2020-12-29T08:40:08.0139053Z"},{"uid":"fc6bbe96-f251-47f2-a30c-bed1fe04bcf9","kind":"NetworkChaos","namespace":"busybox","name":"ci-test","action":"delay","start_time":"2020-12-29T08:41:06Z","finish_time":"2020-12-29T09:14:57.6823095Z"}]
 
 echo "get detail of archive chaos experiment"
 curl -X GET "localhost:2333/api/archives/detail?uid=${EXP_UID}&namespace=busybox" -H "Authorization: Bearer ${BUSYBOX_MANAGER_TOKEN}" > detail_archive.out
-# {"uid":"1fce3d8b-44d8-4906-ae31-eeaefd443a60","kind":"NetworkChaos","namespace":"busybox","name":"test","action":"delay","start_time":"2020-12-29T08:23:22Z","finish_time":"2020-12-29T08:40:08.0139053Z","yaml":{"apiVersion":"chaos-mesh.org/v1alpha1","kind":"NetworkChaos","metadata":{"name":"test","namespace":"busybox","labels":null,"annotations":null},"spec":{"action":"delay","mode":"one","value":"","selector":{"namespaces":["busybox"]},"delay":{"latency":"1ms","correlation":"0","jitter":"0ms"},"direction":"to"}}}
+# {"uid":"1fce3d8b-44d8-4906-ae31-eeaefd443a60","kind":"NetworkChaos","namespace":"busybox","name":"ci-test","action":"delay","start_time":"2020-12-29T08:23:22Z","finish_time":"2020-12-29T08:40:08.0139053Z","yaml":{"apiVersion":"chaos-mesh.org/v1alpha1","kind":"NetworkChaos","metadata":{"name":"ci-test","namespace":"busybox","labels":null,"annotations":null},"spec":{"action":"delay","mode":"one","value":"","selector":{"namespaces":["busybox"]},"delay":{"latency":"1ms","correlation":"0","jitter":"0ms"},"direction":"to"}}}
 
 echo "get report of archive chaos experiment"
 curl -X GET "localhost:2333/api/archives/report?uid=${EXP_UID}&namespace=busybox" -H "Authorization: Bearer ${BUSYBOX_MANAGER_TOKEN}" > report_archive.out
-# {"meta":{"uid":"1fce3d8b-44d8-4906-ae31-eeaefd443a60","kind":"NetworkChaos","namespace":"busybox","name":"test","action":"delay","start_time":"2020-12-29T08:23:22Z","finish_time":"2020-12-29T08:40:08.0139053Z"},"events":[{"id":2,"created_at":"2020-12-29T08:23:23.8662752Z","updated_at":"2020-12-29T08:40:07.9378684Z","deleted_at":null,"experiment":"test","namespace":"busybox","kind":"NetworkChaos","message":"","start_time":"2020-12-29T08:23:23Z","finish_time":"2020-12-29T08:40:07.9319133Z","duration":"","pods":[{"id":2,"created_at":"2020-12-29T08:23:23.8720842Z","updated_at":"2020-12-29T08:23:23.8720842Z","deleted_at":null,"event_id":2,"pod_ip":"10.244.0.38","pod_name":"busybox-0","namespace":"busybox","message":"This is a source pod.","action":"delay"}],"experiment_id":"1fce3d8b-44d8-4906-ae31-eeaefd443a60"}],"total_time":"16m46.0139053s","total_fault_time":"16m44.9319133s"}
+# {"meta":{"uid":"1fce3d8b-44d8-4906-ae31-eeaefd443a60","kind":"NetworkChaos","namespace":"busybox","name":"ci-test","action":"delay","start_time":"2020-12-29T08:23:22Z","finish_time":"2020-12-29T08:40:08.0139053Z"},"events":[{"id":2,"created_at":"2020-12-29T08:23:23.8662752Z","updated_at":"2020-12-29T08:40:07.9378684Z","deleted_at":null,"experiment":"test","namespace":"busybox","kind":"NetworkChaos","message":"","start_time":"2020-12-29T08:23:23Z","finish_time":"2020-12-29T08:40:07.9319133Z","duration":"","pods":[{"id":2,"created_at":"2020-12-29T08:23:23.8720842Z","updated_at":"2020-12-29T08:23:23.8720842Z","deleted_at":null,"event_id":2,"pod_ip":"10.244.0.38","pod_name":"busybox-0","namespace":"busybox","message":"This is a source pod.","action":"delay"}],"experiment_id":"1fce3d8b-44d8-4906-ae31-eeaefd443a60"}],"total_time":"16m46.0139053s","total_fault_time":"16m44.9319133s"}
 
 echo "delete archive chaos experiment"
 curl -X DELETE "localhost:2333/api/archives/${EXP_UID}?namespace=busybox" -H "Authorization: Bearer ${BUSYBOX_MANAGER_TOKEN}" > delete_archive.out
