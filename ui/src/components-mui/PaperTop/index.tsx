c8d8b8d05ffcefa45ab2549ed790cf525b315f94
Yue Yang <g1enyy0ung@gmail.com>
2021-01-21 02:44:55 +0800

    perf(UI): reduce bundle size (#1427)

--- ui/src/components-mui/PaperTop/index.tsx
+++ ui/src/components-mui/PaperTop/index.tsx
@@ -3,26 +3,36 @@ import { Box, Typography } from '@material-ui/core'
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     width: '100%',
-    height: 56,
+    padding: theme.spacing(3),
   },
-})
+}))
 
 interface PaperTopProps {
-  title?: string | JSX.Element
+  title: string | JSX.Element
+  subtitle?: string | JSX.Element
 }
 
-const PaperTop: React.FC<PaperTopProps> = ({ title, children }) => {
+const PaperTop: React.FC<PaperTopProps> = ({ title, subtitle, children }) => {
   const classes = useStyles()
 
   return (
     <Box className={classes.root} px={3}>
-      <Typography variant="h6">{title}</Typography>
+      <Box>
+        <Typography component="div" gutterBottom={subtitle ? true : false}>
+          {title}
+        </Typography>
+        {subtitle && (
+          <Typography variant="body2" color="textSecondary">
+            {subtitle}
+          </Typography>
+        )}
+      </Box>
       {children}
     </Box>
   )
