c312cab00f278cbfb897ee17e878a71b43070a6a
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-01-21 19:06:54 +0800

    fix: some bug fixes related to the UI (#1430) (#1438)

--- ui/src/components/FormField/TextField.tsx
+++ ui/src/components/FormField/TextField.tsx
@@ -1,11 +1,18 @@
 import { Box, TextField as MUITextField, TextFieldProps } from '@material-ui/core'
-import { Field, FieldValidator } from 'formik'
+import { FastField, Field, FieldValidator } from 'formik'
 
 import React from 'react'
 
-const TextField: React.FC<TextFieldProps & { validate?: FieldValidator }> = (props) => (
+const TextField: React.FC<TextFieldProps & { validate?: FieldValidator; fast?: boolean }> = ({
+  fast = false,
+  ...rest
+}) => (
   <Box mb={3}>
-    <Field {...props} as={MUITextField} variant="outlined" margin="dense" fullWidth />
+    {fast ? (
+      <FastField {...rest} as={MUITextField} variant="outlined" margin="dense" fullWidth />
+    ) : (
+      <Field {...rest} as={MUITextField} variant="outlined" margin="dense" fullWidth />
+    )}
   </Box>
 )
 
