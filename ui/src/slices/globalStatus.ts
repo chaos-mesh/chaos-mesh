0f9b8c0dfd08d69110690530b7f456b643743552
Yue Yang <g1enyy0ung@gmail.com>
2021-01-28 13:29:11 +0800

    chore: update
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- ui/src/slices/globalStatus.ts
+++ ui/src/slices/globalStatus.ts
@@ -1,5 +1,6 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
+import { Config } from 'api/common.type'
 import LS from 'lib/localStorage'
 import { TokenFormValues } from 'components/Token'
 
@@ -14,6 +15,8 @@ const initialState: {
   searchModalOpen: boolean
   namespace: string
   securityMode: boolean
+  dnsServerCreate: boolean
+  version: string
   tokens: TokenFormValues[]
   tokenName: string
 } = {
@@ -25,6 +28,8 @@ const initialState: {
   searchModalOpen: false,
   namespace: 'All',
   securityMode: true,
+  dnsServerCreate: false,
+  version: '',
   tokens: [],
   tokenName: '',
 }
@@ -49,8 +54,10 @@ const globalStatusSlice = createSlice({
 
       LS.set('global-namespace', ns)
     },
-    setSecurityMode(state, action: PayloadAction<boolean>) {
-      state.securityMode = action.payload
+    setConfig(state, action: PayloadAction<Config>) {
+      state.securityMode = action.payload.security_mode
+      state.dnsServerCreate = action.payload.dns_server_create
+      state.version = action.payload.version
     },
     setTokens(state, action: PayloadAction<TokenFormValues[]>) {
       const tokens = action.payload
@@ -74,7 +81,7 @@ export const {
   setAlertOpen,
   setSearchModalOpen,
   setNameSpace,
-  setSecurityMode,
+  setConfig,
   setTokens,
   setTokenName,
 } = globalStatusSlice.actions
