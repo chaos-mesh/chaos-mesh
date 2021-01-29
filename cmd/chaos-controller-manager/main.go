4d17862522f38132193db6b3a0dcfb326c831ba7
“fewdan” <fewdan@hotmail.com>
2021-01-29 17:27:36 +0800

    update
    Signed-off-by: “fewdan” <fewdan@hotmail.com>

--- cmd/chaos-controller-manager/main.go
+++ cmd/chaos-controller-manager/main.go
@@ -35,8 +35,8 @@ import (
 	"github.com/chaos-mesh/chaos-mesh/pkg/webhook/config/watcher"
 
 	_ "github.com/chaos-mesh/chaos-mesh/controllers/awschaos/detachvolume"
-	_ "github.com/chaos-mesh/chaos-mesh/controllers/awschaos/ec2stop"
 	_ "github.com/chaos-mesh/chaos-mesh/controllers/awschaos/ec2restart"
+	_ "github.com/chaos-mesh/chaos-mesh/controllers/awschaos/ec2stop"
 	_ "github.com/chaos-mesh/chaos-mesh/controllers/dnschaos"
 	_ "github.com/chaos-mesh/chaos-mesh/controllers/httpchaos"
 	_ "github.com/chaos-mesh/chaos-mesh/controllers/iochaos"
