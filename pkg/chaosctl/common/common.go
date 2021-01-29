44c599feaa8de566f214b12d869a0a338c889bf7
xiang <xiang13225080@163.com>
2021-01-29 11:13:19 +0800

    minor update
    Signed-off-by: xiang <xiang13225080@163.com>

--- pkg/chaosctl/common/common.go
+++ pkg/chaosctl/common/common.go
@@ -176,7 +176,7 @@ func GetPods(ctx context.Context, chaosName string, status v1alpha1.ChaosStatus,
 	nextStart := status.Scheduler.NextStart
 
 	if phase == v1alpha1.ExperimentPhaseWaiting {
-		waitTime := nextStart.Sub(time.Now())
+		waitTime := nextStart.Until()
 		L().WithName("GetPods").V(1).Info(fmt.Sprintf("Waiting for chaos %s to start, in %s\n", chaosName, waitTime))
 		time.Sleep(waitTime)
 	}
