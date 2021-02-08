46930889262c3334e857d3d00badfebb45cb8ff7
Yue Yang <g1enyy0ung@gmail.com>
2021-02-08 13:08:42 +0800

    fix: correct types
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- ui/src/components/EventsTable/index.tsx
+++ ui/src/components/EventsTable/index.tsx
@@ -42,9 +42,9 @@ const useStyles = makeStyles({
   },
 })
 
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (['CreateAt', 'UpdateAt', 'StartTime', 'EndTime'].includes(orderBy as string)) {
-    return comparator(a[orderBy] as any, b[orderBy] as any)
+function descendingComparator<T extends Record<string, any>>(a: T, b: T, orderBy: string) {
+  if (['StartTime', 'EndTime'].includes(orderBy)) {
+    return comparator(a[orderBy], b[orderBy])
   }
 
   if (a[orderBy] > b[orderBy]) {
@@ -60,17 +60,14 @@ function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
 
 type Order = 'asc' | 'desc'
 
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
+function getComparator<T>(order: Order, orderBy: string): (a: T, b: T) => number {
   return order === 'desc'
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy)
 }
 
 function stableSort<T>(data: T[], comparator: (a: T, b: T) => number) {
-  const indexed = data.map((el, index) => [el, index] as [T, number])
+  const indexed: [T, number][] = data.map((el, index) => [el, index])
 
   indexed.sort((a, b) => {
     const order = comparator(a[0], b[0])
@@ -79,7 +76,7 @@ function stableSort<T>(data: T[], comparator: (a: T, b: T) => number) {
       return order
     }
 
-    return a[1] - b[1]
+    return a[1] - b[1] // compare index
   })
 
   return indexed.map((el) => el[0])
