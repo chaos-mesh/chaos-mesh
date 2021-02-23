ace5c992c8177000779b9344c8647ddd925cb8e6
STRRL <str_ruiling@outlook.com>
2021-02-23 14:18:05 +0800

    chore: appending the boilerplate
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- test/e2e/chaos/networkchaos/network_peers_crossover.go
+++ test/e2e/chaos/networkchaos/network_peers_crossover.go
@@ -1,3 +1,16 @@
+// Copyright 2020 Chaos Mesh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package networkchaos
 
 import (
@@ -90,7 +103,7 @@ func TestcasePeersCrossover(
 	err = cli.Delete(ctx, networkDelay.DeepCopy())
 	framework.ExpectNoError(err, "delete network chaos error")
 
-	err=wait.Poll(time.Second, 15*time.Second, func() (done bool, err error) {
+	err = wait.Poll(time.Second, 15*time.Second, func() (done bool, err error) {
 		result = probeNetworkCondition(c, networkPeers, ports, false)
 		if len(result[networkConditionBlocked]) != 0 || len(result[networkConditionSlow]) != 0 {
 			return false, nil
