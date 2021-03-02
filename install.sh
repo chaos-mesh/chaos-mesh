0247f403c56a6833121649f24cd3c0ac8baf8e6f
Anurag Paliwal <apaliwal@adobe.com>
2021-03-02 16:04:00 +0530

    Adding parameterized timeoutSeconds for webhooks
    Signed-off-by: Anurag Paliwal <apaliwal@adobe.com>

--- install.sh
+++ install.sh
@@ -1163,7 +1163,7 @@ spec:
       serviceAccount: chaos-daemon
       hostIPC: true
       hostPID: true
-      priorityClassName: 
+      priorityClassName:
       containers:
         - name: chaos-daemon
           image: ${DOCKER_REGISTRY_PREFIX}/pingcap/chaos-daemon:${VERSION_TAG}
@@ -1229,7 +1229,7 @@ spec:
         app.kubernetes.io/component: chaos-dashboard
     spec:
       serviceAccount: chaos-controller-manager
-      priorityClassName: 
+      priorityClassName:
       containers:
         - name: chaos-dashboard
           image: ${DOCKER_REGISTRY_PREFIX}/pingcap/chaos-dashboard:${VERSION_TAG}
@@ -1299,7 +1299,7 @@ spec:
     spec:
       hostNetwork: ${host_network}
       serviceAccount: chaos-controller-manager
-      priorityClassName: 
+      priorityClassName:
       containers:
       - name: chaos-mesh
         image: ${DOCKER_REGISTRY_PREFIX}/pingcap/chaos-mesh:${VERSION_TAG}
@@ -1369,6 +1369,7 @@ metadata:
     app.kubernetes.io/component: admission-webhook
 webhooks:
   - name: admission-webhook.chaos-mesh.org
+    timeoutSeconds: 5
     clientConfig:
       caBundle: "${CA_BUNDLE}"
       service:
@@ -1392,6 +1393,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-podchaos
     failurePolicy: Fail
     name: mpodchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1410,6 +1412,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-iochaos
     failurePolicy: Fail
     name: miochaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1428,6 +1431,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-timechaos
     failurePolicy: Fail
     name: mtimechaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1446,6 +1450,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-networkchaos
     failurePolicy: Fail
     name: mnetworkchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1464,6 +1469,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-kernelchaos
     failurePolicy: Fail
     name: mkernelchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1482,6 +1488,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-stresschaos
     failurePolicy: Fail
     name: mstresschaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1500,6 +1507,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-awschaos
     failurePolicy: Fail
     name: mawschaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1518,6 +1526,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-podiochaos
     failurePolicy: Fail
     name: mpodiochaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1536,6 +1545,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-podnetworkchaos
     failurePolicy: Fail
     name: mpodnetworkchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1554,6 +1564,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-dnschaos
     failurePolicy: Fail
     name: mdnschaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1572,6 +1583,7 @@ webhooks:
         path: /mutate-chaos-mesh-org-v1alpha1-jvmchaos
     failurePolicy: Fail
     name: mjvmchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1601,6 +1613,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-podchaos
     failurePolicy: Fail
     name: vpodchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1619,6 +1632,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-iochaos
     failurePolicy: Fail
     name: viochaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1637,6 +1651,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-timechaos
     failurePolicy: Fail
     name: vtimechaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1655,6 +1670,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-networkchaos
     failurePolicy: Fail
     name: vnetworkchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1673,6 +1689,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-kernelchaos
     failurePolicy: Fail
     name: vkernelchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1691,6 +1708,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-stresschaos
     failurePolicy: Fail
     name: vstresschaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1709,6 +1727,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-awschaos
     failurePolicy: Fail
     name: vawschaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1727,6 +1746,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-podnetworkchaos
     failurePolicy: Fail
     name: vpodnetworkchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1745,6 +1765,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-dnschaos
     failurePolicy: Fail
     name: vdnschaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
@@ -1763,6 +1784,7 @@ webhooks:
         path: /validate-chaos-mesh-org-v1alpha1-jvmchaos
     failurePolicy: Fail
     name: vjvmchaos.kb.io
+    timeoutSeconds: 5
     rules:
       - apiGroups:
           - chaos-mesh.org
