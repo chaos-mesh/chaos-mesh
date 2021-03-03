c66572adea79749a0245fb24d51fe82ef27df012
“fewdan” <fewdan@hotmail.com>
2021-03-04 00:37:39 +0800

    update
    Signed-off-by: “fewdan” <fewdan@hotmail.com>

---
+++ api/v1alpha1/gcpchaos_webhook_test.go
@@ -0,0 +1,112 @@
+// Copyright 2020 Chaos Mesh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("gcpchaos_webhook", func() {
+	Context("ChaosValidator of gcpchaos", func() {
+		It("Validate", func() {
+
+			type TestCase struct {
+				name    string
+				chaos   GcpChaos
+				execute func(chaos *GcpChaos) error
+				expect  string
+			}
+			duration := "400s"
+			testDeviceName := "testDeviceName"
+			tcs := []TestCase{
+				{
+					name: "simple ValidateCreate for DiskLoss",
+					chaos: GcpChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo1",
+						},
+						Spec: GcpChaosSpec{
+							Action: DiskLoss,
+						},
+					},
+					execute: func(chaos *GcpChaos) error {
+						return chaos.ValidateCreate()
+					},
+					expect: "error",
+				},
+				{
+					name: "only define the Scheduler and execute NodeStop",
+					chaos: GcpChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo4",
+						},
+						Spec: GcpChaosSpec{
+							Scheduler: &SchedulerSpec{
+								Cron: "@every 10m",
+							},
+							Action: NodeStop,
+						},
+					},
+					execute: func(chaos *GcpChaos) error {
+						return chaos.ValidateCreate()
+					},
+					expect: "error",
+				},
+				{
+					name: "only define the Duration and execute NodeStop",
+					chaos: GcpChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo5",
+						},
+						Spec: GcpChaosSpec{
+							Action:   NodeStop,
+							Duration: &duration,
+						},
+					},
+					execute: func(chaos *GcpChaos) error {
+						return chaos.ValidateCreate()
+					},
+					expect: "error",
+				},
+				{
+					name: "unknow action",
+					chaos: GcpChaos{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: metav1.NamespaceDefault,
+							Name:      "foo6",
+						},
+					},
+					execute: func(chaos *GcpChaos) error {
+						return chaos.ValidateCreate()
+					},
+					expect: "error",
+				},
+			}
+
+			for _, tc := range tcs {
+				err := tc.execute(&tc.chaos)
+				if tc.expect == "error" {
+					Expect(err).To(HaveOccurred())
+				} else {
+					Expect(err).NotTo(HaveOccurred())
+				}
+			}
+		})
+	})
+})
