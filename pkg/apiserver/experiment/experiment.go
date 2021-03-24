ca95d92640138411445be5dbec801054ae5ac873
AsterNighT <klxjt99@outlook.com>
2021-03-24 11:23:05 +0800

    fix:make up a missing return
    Signed-off-by: AsterNighT <klxjt99@outlook.com>

--- pkg/apiserver/experiment/experiment.go
+++ pkg/apiserver/experiment/experiment.go
@@ -1186,6 +1186,7 @@ func (s *Service) updateExperiment(c *gin.Context) {
 			c.Status(http.StatusInternalServerError)
 			_ = c.Error(utils.ErrInternalServer.WrapWithNoMessage(err))
 		}
+		return
 	}
 	c.JSON(http.StatusOK, exp)
 }
