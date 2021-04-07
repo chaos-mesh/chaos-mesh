936d7646c72631b54e561a1c34b5aa3ddbe3cef8
STRRL <str_ruiling@outlook.com>
2021-04-07 16:48:32 +0800

    feat: apply multi type of chaos nodes with code generation (#1621)
    * feat: apply multi type of chaos nodes with code generation
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * test: unit tests about the coupling with `all` chaosKindMap
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * feat: fix the bug with unused chaosObject;
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * feat: filtering with only ChaosTemplateType in chaos_node_reconciler
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * feat: appending stress-chaos within example workflow
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: revert the modification on helm
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: update install.sh
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * feat: support fot gcp chaos
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: make linters happy
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: make linters happy
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    Co-authored-by: Ti Chi Robot <71242396+ti-chi-bot@users.noreply.github.com>

--- cmd/chaos-builder/main.go
+++ cmd/chaos-builder/main.go
@@ -57,6 +57,9 @@ func main() {
 	initImpl := ""
 	allTypes := make([]string, 0, 10)
 
+	workflowGenerator := newWorkflowCodeGenerator(nil)
+	workflowTestGenerator := newWorkflowTestCodeGenerator(nil)
+
 	filepath.Walk("./api/v1alpha1", func(path string, info os.FileInfo, err error) error {
 		log := log.WithValues("file", path)
 
@@ -110,6 +113,8 @@ func main() {
 						implCode += generateImpl(baseType.Name.Name)
 						testCode += generateTest(baseType.Name.Name)
 						initImpl += generateInit(baseType.Name.Name)
+						workflowGenerator.AppendTypes(baseType.Name.Name)
+						workflowTestGenerator.AppendTypes(baseType.Name.Name)
 						allTypes = append(allTypes, baseType.Name.Name)
 						continue out
 					}
@@ -142,4 +147,19 @@ func init() {
 		os.Exit(1)
 	}
 	fmt.Fprint(file, testCode)
+
+	file, err = os.Create("./api/v1alpha1/zz_generated.workflow.chaosmesh.go")
+	if err != nil {
+		log.Error(err, "fail to create file")
+		os.Exit(1)
+	}
+	fmt.Fprint(file, workflowGenerator.Render())
+
+	file, err = os.Create("./api/v1alpha1/zz_generated.workflow.chaosmesh_test.go")
+	if err != nil {
+		log.Error(err, "fail to create file")
+		os.Exit(1)
+	}
+	fmt.Fprint(file, workflowTestGenerator.Render())
+
 }
