5497620dfbc95e0fbd8ced982a837a2e205afe38
Yue Yang <g1enyy0ung@gmail.com>
2021-05-06 18:13:17 +0800

    chore(update): workflows api (#1758)
    * chore(update): workflows api
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: json tag invalid
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: test
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: zz_generated
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: crd
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: sync the newest types
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- ui/src/api/experiments.ts
+++ ui/src/api/experiments.ts
@@ -30,4 +30,4 @@ export const start = (uuid: uuid) => http.put(`/experiments/start/${uuid}`)
 export const del = (uuid: uuid) => http.delete(`/experiments/${uuid}`)
 export const delMulti = (uuids: uuid[]) => http.delete(`/experiments?uids=${uuids.join(',')}`)
 
-export const update = (data: ExperimentDetail['yaml']) => http.put('/experiments/update', data)
+export const update = (data: ExperimentDetail['kube_object']) => http.put('/experiments/update', data)
