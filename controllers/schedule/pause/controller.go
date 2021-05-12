3d4bc1671b9ac2f3f881323dcc0904e0cd5bb365
YangKeao <keao.yang@yahoo.com>
2021-05-12 20:56:44 +0800

    format the codes
    Signed-off-by: YangKeao <keao.yang@yahoo.com>

--- controllers/schedule/pause/controller.go
+++ controllers/schedule/pause/controller.go
@@ -18,9 +18,6 @@ import (
 	"reflect"
 	"strconv"
 
-	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
-	"github.com/chaos-mesh/chaos-mesh/controllers/schedule/utils"
-	"github.com/chaos-mesh/chaos-mesh/controllers/types"
 	"github.com/go-logr/logr"
 	k8sError "k8s.io/apimachinery/pkg/api/errors"
 	k8sTypes "k8s.io/apimachinery/pkg/types"
@@ -28,6 +25,10 @@ import (
 	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
+	"github.com/chaos-mesh/chaos-mesh/controllers/schedule/utils"
+	"github.com/chaos-mesh/chaos-mesh/controllers/types"
 )
 
 type Reconciler struct {
