544356eba63a4ee06983ab199488a5693439ff9f
“fewdan” <fewdan@hotmail.com>
2021-05-13 23:50:06 +0800

    update
    Signed-off-by: “fewdan” <fewdan@hotmail.com>

--- pkg/apiserver/common/common.go
+++ pkg/apiserver/common/common.go
@@ -31,7 +31,7 @@ import (
 	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
 	"github.com/chaos-mesh/chaos-mesh/pkg/apiserver/utils"
 	"github.com/chaos-mesh/chaos-mesh/pkg/clientpool"
-	"github.com/chaos-mesh/chaos-mesh/pkg/config"
+	"github.com/chaos-mesh/chaos-mesh/pkg/config/dashboard"
 	"github.com/chaos-mesh/chaos-mesh/pkg/core"
 )
 
