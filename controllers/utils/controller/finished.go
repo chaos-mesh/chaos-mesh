091e742b62fcb290267991e3bb3498a40e30ba64
YangKeao <keao.yang@yahoo.com>
2021-05-24 19:44:06 +0800

    modify finished assertion for one-shot chaos
    Signed-off-by: YangKeao <keao.yang@yahoo.com>

--- controllers/utils/controller/finished.go
+++ controllers/utils/controller/finished.go
@@ -31,14 +31,26 @@ func IsChaosFinished(obj v1alpha1.InnerObject, now time.Time) bool {
 		}
 	}
 
-	// If the duration hasn't exceeded, it's not finished
-	duration, err := obj.GetDuration()
+	durationExceeded, untilStop, err := obj.DurationExceeded(time.Now())
 	if err != nil {
 		return finished
 	}
-	if obj.GetObjectMeta().CreationTimestamp.Add(*duration).After(now) {
-		finished = false
+	if durationExceeded {
+		return finished
 	}
 
+	if untilStop != 0 {
+		return false
+	}
+
+	// duration is not Exceeded, but untilStop is 0
+	// which means the current object is one-shot (like PodKill)
+	// then if all records are injected, they are finished
+	finished = true
+	for _, record := range status.Experiment.Records {
+		if record.Phase != v1alpha1.Injected {
+			finished = false
+		}
+	}
 	return finished
 }
