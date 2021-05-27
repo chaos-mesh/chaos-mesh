9cc53175df417178be705dd9ca6ece9d9ceacbfa
AsterNighT <klxjt99@outlook.com>
2021-05-27 14:41:52 +0800

    Make it runnable
    Signed-off-by: AsterNighT <klxjt99@outlook.com>

--- controllers/suit_test.go
+++ controllers/suit_test.go
@@ -11,56 +11,58 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package finalizers
+package controllers
 
 import (
-	"context"
+	"bytes"
+	"encoding/gob"
 	"os"
 	"path/filepath"
 	"testing"
-
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 
 	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"go.uber.org/fx"
-
-	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
-	"github.com/chaos-mesh/chaos-mesh/controllers/schedule/utils"
-	"github.com/chaos-mesh/chaos-mesh/controllers/types"
-	"github.com/chaos-mesh/chaos-mesh/controllers/utils/test"
-
 	"k8s.io/client-go/rest"
 	"k8s.io/kubectl/pkg/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
+	"github.com/chaos-mesh/chaos-mesh/controllers/schedule/utils"
+	"github.com/chaos-mesh/chaos-mesh/controllers/types"
+	"github.com/chaos-mesh/chaos-mesh/controllers/utils/test"
+	"github.com/chaos-mesh/chaos-mesh/pkg/workflow/controllers"
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var app *fx.App
 var k8sClient client.Client
 var lister *utils.ActiveLister
 var config *rest.Config
 var testEnv *envtest.Environment
 var setupLog = ctrl.Log.WithName("setup")
 
-func TestSchedule(t *testing.T) {
+func TestControllers(t *testing.T) {
 	RegisterFailHandler(Fail)
 
 	RunSpecsWithDefaultAndCustomReporters(t,
+<<<<<<< HEAD:controllers/schedule/suit_test.go
 		"Schedule suit",
 		[]Reporter{printer.NewlineReporter{}})
+=======
+		"controller test suit",
+		[]Reporter{envtest.NewlineReporter{}})
+>>>>>>> 3ba000f6 (Make it runnable):controllers/suit_test.go
 }
 
-var _ = BeforeSuite(func() {
-	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
-	By("bootstrapping test environment")
+var _ = SynchronizedBeforeSuite(func() []byte {
 	t := true
 	if os.Getenv("TEST_USE_EXISTING_CLUSTER") == "true" {
 		testEnv = &envtest.Environment{
@@ -68,31 +70,25 @@ var _ = BeforeSuite(func() {
 		}
 	} else {
 		testEnv = &envtest.Environment{
-			CRDDirectoryPaths: []string{filepath.Join("..", "..", "config", "crd", "bases")},
+			CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
 		}
 	}
 
-	err := v1alpha1.SchemeBuilder.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	config, err = testEnv.Start()
+	config, err := testEnv.Start()
 	Expect(err).ToNot(HaveOccurred())
 	Expect(config).ToNot(BeNil())
 
-	k8sClient, err = client.New(config, client.Options{Scheme: scheme.Scheme})
+	var data bytes.Buffer
+	enc := gob.NewEncoder(&data)
+	enc.Encode(config)
 	Expect(err).ToNot(HaveOccurred())
-	Expect(k8sClient).ToNot(BeNil())
 
+<<<<<<< HEAD:controllers/schedule/suit_test.go
 	app = fx.New(
 		fx.Options(
 			test.Module,
-			fx.Provide(
-				fx.Annotated{
-					Group:  "controller",
-					Target: NewController,
-				},
-			),
 			fx.Supply(config),
+			Module,
 			types.ChaosObjects,
 		),
 		fx.Invoke(Run),
@@ -103,21 +99,32 @@ var _ = BeforeSuite(func() {
 	if err := app.Start(startCtx); err != nil {
 		setupLog.Error(err, "fail to start manager")
 	}
+=======
+	return data.Bytes()
+}, func(confBytes []byte) {
+	data := bytes.NewBuffer(confBytes)
+	dec := gob.NewDecoder(data)
+	var gotConfig rest.Config
+	err := dec.Decode(&gotConfig)
+>>>>>>> 3ba000f6 (Make it runnable):controllers/suit_test.go
+	Expect(err).ToNot(HaveOccurred())
+	config = &gotConfig
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	By("bootstrapping test environment")
+
+	err = v1alpha1.SchemeBuilder.AddToScheme(scheme.Scheme)
 	Expect(err).ToNot(HaveOccurred())
 
+	k8sClient, err = client.New(config, client.Options{Scheme: scheme.Scheme})
+	Expect(err).ToNot(HaveOccurred())
+	Expect(k8sClient).ToNot(BeNil())
 }, 60)
 
-var _ = AfterSuite(func() {
+var _ = SynchronizedAfterSuite(func() {}, func() {
 	By("tearing down the test environment")
-	stopCtx, cancel := context.WithTimeout(context.Background(), app.StopTimeout())
-	defer cancel()
-
-	if err := app.Stop(stopCtx); err != nil {
-		setupLog.Error(err, "fail to stop manager")
-	}
 	err := testEnv.Stop()
 	Expect(err).ToNot(HaveOccurred())
-})
+}, 60)
 
 type RunParams struct {
 	fx.In
@@ -131,5 +138,6 @@ type RunParams struct {
 
 func Run(params RunParams) error {
 	lister = utils.NewActiveLister(k8sClient, params.Logger)
-	return nil
+	err := controllers.BootstrapWorkflowControllers(params.Mgr, params.Logger)
+	return err
 }
