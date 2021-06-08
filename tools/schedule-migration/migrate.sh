91b1a5b33c7acc0f83459c33d6e462a74646f38e
AsterNighT <klxjt99@outlook.com>
2021-06-08 16:47:07 +0800

    Update boilerplate
    Signed-off-by: AsterNighT <klxjt99@outlook.com>

--- tools/schedule-migration/migrate.sh
+++ tools/schedule-migration/migrate.sh
@@ -1,3 +1,18 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 Chaos Mesh Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 NAMESPACES=$(kubectl get namespace | sed '1d' | awk '{print $1}')
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 CRDS="awschaos
