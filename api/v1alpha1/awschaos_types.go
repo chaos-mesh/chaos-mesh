72748f32bcad08f302dd2ea4823878793737c34a
YangKeao <keao.yang@yahoo.com>
2021-06-16 18:05:44 +0800

    add IsOneShot function to fit PodKill and AwsRestart
    Signed-off-by: YangKeao <keao.yang@yahoo.com>

--- api/v1alpha1/awschaos_types.go
+++ api/v1alpha1/awschaos_types.go
@@ -21,6 +21,7 @@ import (
 
 // +kubebuilder:object:root=true
 // +chaos-mesh:base
+// +chaos-mesh:oneshot=.Spec.Action==Ec2Restart
 
 // AwsChaos is the Schema for the awschaos API
 type AwsChaos struct {
