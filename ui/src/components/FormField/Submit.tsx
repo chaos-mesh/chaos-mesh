06ddb7356ceef584c91407a3994761e764580528
Yue Yang <g1enyy0ung@gmail.com>
2021-06-24 10:20:36 +0800

    fix: missing onClick
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- ui/src/components/FormField/Submit.tsx
+++ ui/src/components/FormField/Submit.tsx
@@ -6,7 +6,13 @@ import T from 'components/T'
 export default function Submit({ mt = 6, onClick, ...rest }: ButtonProps & { mt?: number }) {
   return (
     <Box mt={mt} textAlign="right">
-      <Button type={onClick ? undefined : 'submit'} variant="contained" startIcon={<PublishIcon />} {...rest}>
+      <Button
+        type={onClick ? undefined : 'submit'}
+        variant="contained"
+        startIcon={<PublishIcon />}
+        onClick={onClick}
+        {...rest}
+      >
         {T('common.submit')}
       </Button>
     </Box>
