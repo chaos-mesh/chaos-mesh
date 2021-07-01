4c5bd57e945df7a10dcb514fa2da817a1cb2ac86
Yue Yang <g1enyy0ung@gmail.com>
2021-07-01 17:56:13 +0800

    fix(ci): imports
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- pkg/core/experiment.go
+++ pkg/core/experiment.go
@@ -20,8 +20,9 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
 	"github.com/jinzhu/gorm"
+
+	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
 )
 
 // ExperimentStore defines operations for working with experiments.
