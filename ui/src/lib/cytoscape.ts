9d7441bc05d3b823ce16ca3beaeb9a652bb2717d
STRRL <str_ruiling@outlook.com>
2021-07-07 17:39:50 +0800

    chore: show children of task node
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- ui/src/lib/cytoscape.ts
+++ ui/src/lib/cytoscape.ts
@@ -21,8 +21,6 @@ function generateWorkflowNodes(detail: WorkflowSingle) {
       return [n.name, n]
     })
   )
-  const mainChildren = entryNode!.serial?.children
-
   function toCytoscapeNode(node: Node): RecursiveNodeDefinition {
     const { name, type, state, template } = node
 
@@ -38,6 +36,12 @@ function generateWorkflowNodes(detail: WorkflowSingle) {
         node.parallel!.children.filter((d) => d.name).map((d) => toCytoscapeNode(nodeMap.get(d.name)!)),
         node.name,
       ]
+    } else if (type === 'TaskNode' && node.conditional_branches!.length) {
+      return [
+        type,
+        node.conditional_branches!.filter((d) => d.name).map((d) => toCytoscapeNode(nodeMap.get(d.name)!)),
+        node.name,
+      ]
     } else {
       return {
         data: {
@@ -51,10 +55,7 @@ function generateWorkflowNodes(detail: WorkflowSingle) {
     }
   }
 
-  return mainChildren!
-    .filter((d) => d.name)
-    .map((d) => nodeMap.get(d.name))
-    .map((d) => toCytoscapeNode(d!))
+  return [toCytoscapeNode(entryNode!)]
 }
 
 function mergeStates(nodes: NodeDefinition[]) {
