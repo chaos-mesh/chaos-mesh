3608d9f67c1342847b5846dae140975ecf153a10
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-07-07 14:51:29 +0800

    fix: duplicated field created_at with which in gorm.Model (#2081) (#2082)
    * fix: duplicated field created_at with which in gorm.Model
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: related updates
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    Co-authored-by: STRRL <str_ruiling@outlook.com>
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- pkg/core/workflow.go
+++ pkg/core/workflow.go
@@ -55,7 +55,6 @@ type WorkflowMeta struct {
 	Namespace string         `json:"namespace"`
 	Name      string         `json:"name"`
 	Entry     string         `json:"entry"` // the entry node name
-	CreatedAt string         `json:"created_at"`
 	EndTime   string         `json:"end_time"`
 	Status    WorkflowStatus `json:"status,omitempty"`
 	Archived  bool           `json:"-"`
@@ -261,7 +260,7 @@ func convertWorkflow(kubeWorkflow v1alpha1.Workflow) WorkflowMeta {
 	}
 
 	if kubeWorkflow.Status.StartTime != nil {
-		result.CreatedAt = kubeWorkflow.Status.StartTime.Format(time.RFC3339)
+		result.CreatedAt = kubeWorkflow.Status.StartTime.Time
 	}
 
 	if kubeWorkflow.Status.EndTime != nil {
