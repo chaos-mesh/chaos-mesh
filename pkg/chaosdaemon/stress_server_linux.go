9fec6ad115b542da54c1e73c51721f7db7b53543
YangKeao <keao.yang@yahoo.com>
2021-07-14 11:23:32 +0800

    use cgroups load to modify the cgroup (#2092)
    * use cgroups load to modify the cgroup
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * fix mod tidy
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * update go tools
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * go mod tidy
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * run check in the build environment
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * run check in the build environment
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * fix go.mod in e2e test image
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * mount sys on another point
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * run go mod tidy
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * update install.sh
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * upgrade goimports
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * set goimports version
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    * run make check
    
    Signed-off-by: YangKeao <keao.yang@yahoo.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    Signed-off-by: Nikolas Skoufis <nskoufis@seek.com.au>

--- pkg/chaosdaemon/stress_server_linux.go
+++ pkg/chaosdaemon/stress_server_linux.go
@@ -14,12 +14,7 @@
 package chaosdaemon
 
 import (
-	"bufio"
 	"context"
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"syscall"
@@ -27,17 +22,10 @@ import (
 
 	"github.com/containerd/cgroups"
 	"github.com/golang/protobuf/ptypes/empty"
-	"github.com/pkg/errors"
 
 	"github.com/chaos-mesh/chaos-mesh/pkg/bpm"
+	daemonCgroups "github.com/chaos-mesh/chaos-mesh/pkg/chaosdaemon/cgroups"
 	pb "github.com/chaos-mesh/chaos-mesh/pkg/chaosdaemon/pb"
-)
-
-var (
-	// Possible cgroup subsystems
-	cgroupSubsys = []string{"cpu", "memory", "systemd", "net_cls",
-		"net_prio", "freezer", "blkio", "perf_event", "devices",
-		"cpuset", "cpuacct", "pids", "hugetlb"}
 )
 
 func (s *DaemonServer) ExecStressors(ctx context.Context,
@@ -47,19 +35,7 @@ func (s *DaemonServer) ExecStressors(ctx context.Context,
 	if err != nil {
 		return nil, err
 	}
-	path := pidPath(int(pid))
-	id, err := s.crClient.FormatContainerID(ctx, req.Target)
-	if err != nil {
-		return nil, err
-	}
-	cgroup, err := findValidCgroup(path, id)
-	if err != nil {
-		return nil, err
-	}
-	if req.Scope == pb.ExecStressRequest_POD {
-		cgroup, _ = filepath.Split(cgroup)
-	}
-	control, err := cgroups.Load(cgroups.V1, cgroups.StaticPath(cgroup))
+	control, err := cgroups.Load(daemonCgroups.V1, daemonCgroups.PidPath(int(pid)))
 	if err != nil {
 		return nil, err
 	}
@@ -128,127 +104,4 @@ func (s *DaemonServer) CancelStressors(ctx context.Context,
 	}
 	log.Info("killing stressor successfully")
 	return &empty.Empty{}, nil
-}
-
-func findValidCgroup(path cgroups.Path, target string) (string, error) {
-	for _, subsys := range cgroupSubsys {
-		p, err := path(cgroups.Name(subsys))
-		if err != nil {
-			log.Error(err, "Failed to retrieve the cgroup path", "subsystem", subsys, "target", target)
-			continue
-		}
-		if strings.Contains(p, target) {
-			return p, nil
-		}
-	}
-	return "", fmt.Errorf("never found valid cgroup for %s", target)
-}
-
-// pidPath will return the correct cgroup paths for an existing process running inside a cgroup
-// This is commonly used for the Load function to restore an existing container.
-//
-// Note: it is migrated from cgroups.pidPath since it will find mountinfo incorrectly inside
-// the daemonset. Hope we can fix it in official cgroups repo to solve it.
-func pidPath(pid int) cgroups.Path {
-	p := fmt.Sprintf("/proc/%d/cgroup", pid)
-	paths, err := parseCgroupFile(p)
-	if err != nil {
-		return errorPath(errors.Wrapf(err, "parse cgroup file %s", p))
-	}
-	return existingPath(paths, pid, "")
-}
-
-func errorPath(err error) cgroups.Path {
-	return func(_ cgroups.Name) (string, error) {
-		return "", err
-	}
-}
-
-func existingPath(paths map[string]string, pid int, suffix string) cgroups.Path {
-	// localize the paths based on the root mount dest for nested cgroups
-	for n, p := range paths {
-		dest, err := getCgroupDestination(pid, string(n))
-		if err != nil {
-			return errorPath(err)
-		}
-		rel, err := filepath.Rel(dest, p)
-		if err != nil {
-			return errorPath(err)
-		}
-		if rel == "." {
-			rel = dest
-		}
-		paths[n] = filepath.Join("/", rel)
-	}
-	return func(name cgroups.Name) (string, error) {
-		root, ok := paths[string(name)]
-		if !ok {
-			if root, ok = paths[fmt.Sprintf("name=%s", name)]; !ok {
-				return "", cgroups.ErrControllerNotActive
-			}
-		}
-		if suffix != "" {
-			return filepath.Join(root, suffix), nil
-		}
-		return root, nil
-	}
-}
-
-func parseCgroupFile(path string) (map[string]string, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	return parseCgroupFromReader(f)
-}
-
-func parseCgroupFromReader(r io.Reader) (map[string]string, error) {
-	var (
-		cgroups = make(map[string]string)
-		s       = bufio.NewScanner(r)
-	)
-	for s.Scan() {
-		var (
-			text  = s.Text()
-			parts = strings.SplitN(text, ":", 3)
-		)
-		if len(parts) < 3 {
-			return nil, fmt.Errorf("invalid cgroup entry: %q", text)
-		}
-		for _, subs := range strings.Split(parts[1], ",") {
-			if subs != "" {
-				cgroups[subs] = parts[2]
-			}
-		}
-	}
-
-	if err := s.Err(); err != nil {
-		return nil, err
-	}
-
-	return cgroups, nil
-}
-
-func getCgroupDestination(pid int, subsystem string) (string, error) {
-	// use the process's mount info
-	p := fmt.Sprintf("/proc/%d/mountinfo", pid)
-	f, err := os.Open(p)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		fields := strings.Fields(s.Text())
-		for _, opt := range strings.Split(fields[len(fields)-1], ",") {
-			if opt == subsystem {
-				return fields[3], nil
-			}
-		}
-	}
-	if err := s.Err(); err != nil {
-		return "", err
-	}
-	return "", fmt.Errorf("never found desct for %s", subsystem)
 }
