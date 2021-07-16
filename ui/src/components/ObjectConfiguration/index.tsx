cbc43b0a324dc48e86654989f1c568ae241822c1
Yue Yang <g1enyy0ung@gmail.com>
2021-07-16 17:34:05 +0800

    chore: show status in single page
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- ui/src/components/ObjectConfiguration/index.tsx
+++ ui/src/components/ObjectConfiguration/index.tsx
@@ -3,6 +3,8 @@ import { Grid, Table, TableBody, TableRow, Typography } from '@material-ui/core'
 
 import { ArchiveSingle } from 'api/archives.type'
 import { ExperimentSingle } from 'api/experiments.type'
+import Space from 'components-mui/Space'
+import StatusLabel from 'components-mui/StatusLabel'
 import T from 'components/T'
 import { format } from 'lib/luxon'
 import { toCamelCase } from 'lib/utils'
@@ -14,14 +16,16 @@ interface ObjectConfigurationProps {
   config: Config
   inNode?: boolean
   inSchedule?: boolean
+  inArchive?: boolean
   vertical?: boolean
 }
 
 const ObjectConfiguration: React.FC<ObjectConfigurationProps> = ({
   config,
-  inNode = false,
-  inSchedule = false,
-  vertical = false,
+  inNode,
+  inSchedule,
+  inArchive,
+  vertical,
 }) => {
   const { lang } = useStoreSelector((state) => state.settings)
 
@@ -30,141 +34,138 @@ const ObjectConfiguration: React.FC<ObjectConfigurationProps> = ({
     inSchedule || inNode ? spec[toCamelCase(inSchedule ? spec.type : (config as any).templateType)] : spec
 
   return (
-    <Grid container spacing={vertical ? 3 : 0}>
-      <Grid item xs={vertical ? 12 : 3}>
-        <Typography variant="subtitle2" gutterBottom>
-          {T('newE.steps.basic')}
-        </Typography>
+    <>
+      {!inNode && (
+        <Space direction="row" mb={3}>
+          <Typography>{config.name}</Typography>
 
-        <Table size="small">
-          <TableBody>
-            {!inNode && (
-              <TableRow>
-                <TableCell>{T('k8s.namespace')}</TableCell>
-                <TableCell>
-                  <Typography variant="body2" color="textSecondary">
-                    {config.namespace}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            )}
+          {!inArchive && <StatusLabel status={(config as any).status} />}
+        </Space>
+      )}
 
-            <TableRow>
-              <TableCell>{T('common.name')}</TableCell>
-              <TableCell>
-                <Typography variant="body2" color="textSecondary">
-                  {config.name}
-                </Typography>
-              </TableCell>
-            </TableRow>
+      <Grid container spacing={vertical ? 3 : 0}>
+        {!inNode && (
+          <Grid item xs={vertical ? 12 : 3}>
+            <Typography variant="subtitle2" gutterBottom>
+              {T('newE.steps.basic')}
+            </Typography>
 
-            {!inNode && (
-              <TableRow>
-                <TableCell>{T('common.uuid')}</TableCell>
-                <TableCell>
-                  <Typography variant="body2" color="textSecondary">
-                    {config.uid}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            )}
-
-            {!inNode && (
-              <TableRow>
-                <TableCell>{T('table.created')}</TableCell>
-                <TableCell>
-                  <Typography variant="body2" color="textSecondary">
-                    {format(config.created_at, lang)}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </Grid>
-
-      <Grid item xs={vertical ? 12 : 3}>
-        <Typography variant="subtitle2" gutterBottom>
-          {T('newE.steps.scope')}
-        </Typography>
-
-        <Selector data={experiment.selector} />
-      </Grid>
-
-      <Grid item xs={vertical ? 12 : 3}>
-        <Typography variant="subtitle2" gutterBottom>
-          {T('experiments.single')}
-        </Typography>
-
-        <Experiment
-          kind={inNode ? (config as any).templateType : inSchedule ? spec.type : config.kind}
-          data={experiment}
-        />
-      </Grid>
-
-      <Grid item xs={vertical ? 12 : 3}>
-        <Typography variant="subtitle2" gutterBottom>
-          {T('newE.steps.run')}
-        </Typography>
-
-        <Table size="small">
-          <TableBody>
-            {!inSchedule && (
-              <TableRow>
-                <TableCell>{T(inNode ? 'newW.node.deadline' : 'common.duration')}</TableCell>
-                <TableCell>
-                  <Typography variant="body2" color="textSecondary">
-                    {inNode ? (config as any).deadline : spec.duration ? spec.duration : T('newE.run.continuous')}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            )}
-            {inSchedule && (
-              <>
+            <Table size="small">
+              <TableBody>
                 <TableRow>
-                  <TableCell>{T('schedules.single')}</TableCell>
+                  <TableCell>{T('k8s.namespace')}</TableCell>
                   <TableCell>
                     <Typography variant="body2" color="textSecondary">
-                      {spec.schedule}
+                      {config.namespace}
                     </Typography>
                   </TableCell>
                 </TableRow>
-                {spec.startingDeadlineSeconds && (
+
+                <TableRow>
+                  <TableCell>{T('common.uuid')}</TableCell>
+                  <TableCell>
+                    <Typography variant="body2" color="textSecondary">
+                      {config.uid}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>{T('table.created')}</TableCell>
+                  <TableCell>
+                    <Typography variant="body2" color="textSecondary">
+                      {format(config.created_at, lang)}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Grid>
+        )}
+
+        <Grid item xs={vertical ? 12 : 3}>
+          <Typography variant="subtitle2" gutterBottom>
+            {T('newE.steps.scope')}
+          </Typography>
+
+          <Selector data={experiment.selector} />
+        </Grid>
+
+        <Grid item xs={vertical ? 12 : 3}>
+          <Typography variant="subtitle2" gutterBottom>
+            {T('experiments.single')}
+          </Typography>
+
+          <Experiment
+            kind={inNode ? (config as any).templateType : inSchedule ? spec.type : config.kind}
+            data={experiment}
+          />
+        </Grid>
+
+        <Grid item xs={vertical ? 12 : 3}>
+          <Typography variant="subtitle2" gutterBottom>
+            {T('newE.steps.run')}
+          </Typography>
+
+          <Table size="small">
+            <TableBody>
+              {!inSchedule && (
+                <TableRow>
+                  <TableCell>{T(inNode ? 'newW.node.deadline' : 'common.duration')}</TableCell>
+                  <TableCell>
+                    <Typography variant="body2" color="textSecondary">
+                      {inNode ? (config as any).deadline : spec.duration ? spec.duration : T('newE.run.continuous')}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              )}
+              {inSchedule && (
+                <>
                   <TableRow>
-                    <TableCell>{T('newS.basic.startingDeadlineSeconds')}</TableCell>
+                    <TableCell>{T('schedules.single')}</TableCell>
                     <TableCell>
                       <Typography variant="body2" color="textSecondary">
-                        {spec.startingDeadlineSeconds}
+                        {spec.schedule}
                       </Typography>
                     </TableCell>
                   </TableRow>
-                )}
-                {spec.concurrencyPolicy && (
-                  <TableRow>
-                    <TableCell>{T('newS.basic.concurrencyPolicy')}</TableCell>
-                    <TableCell>
-                      <Typography variant="body2" color="textSecondary">
-                        {spec.concurrencyPolicy}
-                      </Typography>
-                    </TableCell>
-                  </TableRow>
-                )}
-                {spec.historyLimit && (
-                  <TableRow>
-                    <TableCell>{T('newS.basic.historyLimit')}</TableCell>
-                    <TableCell>
-                      <Typography variant="body2" color="textSecondary">
-                        {spec.historyLimit}
-                      </Typography>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </>
-            )}
-          </TableBody>
-        </Table>
+                  {spec.startingDeadlineSeconds && (
+                    <TableRow>
+                      <TableCell>{T('newS.basic.startingDeadlineSeconds')}</TableCell>
+                      <TableCell>
+                        <Typography variant="body2" color="textSecondary">
+                          {spec.startingDeadlineSeconds}
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  )}
+                  {spec.concurrencyPolicy && (
+                    <TableRow>
+                      <TableCell>{T('newS.basic.concurrencyPolicy')}</TableCell>
+                      <TableCell>
+                        <Typography variant="body2" color="textSecondary">
+                          {spec.concurrencyPolicy}
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  )}
+                  {spec.historyLimit && (
+                    <TableRow>
+                      <TableCell>{T('newS.basic.historyLimit')}</TableCell>
+                      <TableCell>
+                        <Typography variant="body2" color="textSecondary">
+                          {spec.historyLimit}
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  )}
+                </>
+              )}
+            </TableBody>
+          </Table>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   )
 }
 
