588ac79dfbef2b5223475bfea2bd8a94e4efa7c2
FingerLeader <wanxfinger@gmail.com>
2021-07-28 11:25:03 +0800

    change ParseNamespacedNameContainr to ParseNamespacedName in recover
    Signed-off-by: FingerLeader <wanxfinger@gmail.com>

--- controllers/chaosimpl/iochaos/impl.go
+++ controllers/chaosimpl/iochaos/impl.go
@@ -160,7 +160,7 @@ func (impl *Impl) Recover(ctx context.Context, index int, records []*v1alpha1.Re
 		return waitForRecoverSync, nil
 	}
 
-	podId, _ := controller.ParseNamespacedNameContainer(records[index].Id)
+	podId := controller.ParseNamespacedName(records[index].Id)
 	var pod v1.Pod
 	err := impl.Client.Get(ctx, podId, &pod)
 	if err != nil {
