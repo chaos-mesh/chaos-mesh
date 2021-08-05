a74046f20966824dae5c847fe8b6ea0cdc5dd899
STRRL <str_ruiling@outlook.com>
2021-08-05 12:16:02 +0800

    chore: bump controller-runtime for api
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- api/v1alpha1/iochaos_webhook.go
+++ api/v1alpha1/iochaos_webhook.go
@@ -21,7 +21,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
