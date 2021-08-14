c2a95fd0f2fa442149747b5a9605d1a9ab18c830
qiao <qiao@qiaodeMacBook-Air.local>
2021-08-15 02:33:09 +0800

    Check other package static interface  include delegate
    Signed-off-by: qiao <qiao@qiaodeMacBook-Air.local>

--- controllers/chaosimpl/awschaos/ec2restart/impl.go
+++ controllers/chaosimpl/awschaos/ec2restart/impl.go
@@ -26,6 +26,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
+	"github.com/chaos-mesh/chaos-mesh/controllers/common"
 )
 
 type Impl struct {
@@ -33,6 +34,8 @@ type Impl struct {
 
 	Log logr.Logger
 }
+
+var _ common.ChaosImpl = (*Impl)(nil)
 
 func (impl *Impl) Apply(ctx context.Context, index int, records []*v1alpha1.Record, obj v1alpha1.InnerObject) (v1alpha1.Phase, error) {
 	awschaos := obj.(*v1alpha1.AWSChaos)
