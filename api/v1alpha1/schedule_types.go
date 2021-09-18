7bf1f9e5b0ec381521b18ec11391e5da902ade8a
YangKeao <keao.yang@yahoo.com>
2021-09-18 18:10:43 +0800

    upgrade cron to v3 (#2315)
    * upgrade cron to v3
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * run make check
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- api/v1alpha1/schedule_types.go
+++ api/v1alpha1/schedule_types.go
@@ -14,6 +14,7 @@
 package v1alpha1
 
 import (
+	"github.com/robfig/cron/v3"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -99,6 +100,23 @@ type ScheduleList struct {
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []Schedule `json:"items"`
 }
+
+func (in *ScheduleList) GetItems() []GenericChaos {
+	var result []GenericChaos
+	for _, item := range in.Items {
+		item := item
+		result = append(result, &item)
+	}
+	return result
+}
+
+func (in *ScheduleList) DeepCopyList() GenericChaosList {
+	return in.DeepCopy()
+}
+
+var StandardCronParser = cron.NewParser(
+	cron.SecondOptional | cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.Descriptor,
+)
 
 func init() {
 	SchemeBuilder.Register(&Schedule{})
