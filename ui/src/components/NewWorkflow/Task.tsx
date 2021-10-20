641e56b0cbc38677e91cc594dc8f5e3109859c73
STRRL <str_ruiling@outlook.com>
2021-10-21 00:23:36 +0800

    chore: fix license checker
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- ui/src/components/NewWorkflow/Task.tsx
+++ ui/src/components/NewWorkflow/Task.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Chaos Mesh Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 import { Autocomplete, Box, IconButton, TextField as MUITextField, Typography } from '@material-ui/core'
 import { Branch, Template, TemplateType } from 'slices/workflows'
 import { Form, Formik } from 'formik'
@@ -18,6 +34,7 @@ import T from 'components/T'
 import { resetNewExperiment } from 'slices/experiments'
 import { setAlert } from 'slices/globalStatus'
 import { useIntl } from 'react-intl'
+
 interface TaskProps extends FormProps {
   childrenCount: number
   submitTemplate: (template: Template) => void
