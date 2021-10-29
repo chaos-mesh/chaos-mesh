f2cf0b2f185720b9965bbd13b66f895167d6f7c9
YangKeao <yangkeao@chunibyo.icu>
2021-10-29 17:09:03 +1100

    add an example in the comments
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- pkg/time/time_linux_amd64.go
+++ pkg/time/time_linux_amd64.go
@@ -105,6 +105,17 @@ func init() {
 						continue
 					}
 
+					// The relocation of a X86 image is like:
+					// Relocation section '.rela.text' at offset 0x288 contains 3 entries:
+					// Offset          Info           Type           Sym. Value    Sym. Name + Addend
+					// 000000000016  000900000002 R_X86_64_PC32     0000000000000000 CLOCK_IDS_MASK - 4
+					// 00000000001f  000a00000002 R_X86_64_PC32     0000000000000008 TV_NSEC_DELTA - 4
+					// 00000000002a  000b00000002 R_X86_64_PC32     0000000000000010 TV_SEC_DELTA - 4
+					//
+					// For example, we need to write the offset of `CLOCK_IDS_MASK` - 4 in 0x16 of the section
+					// If we want to put the `CLOCK_IDS_MASK` at the end of the section, it will be
+					// len(fakeImage.content) - 4 - 0x16
+
 					sym := &syms[symNo-1]
 					fakeImage.offset[sym.Name] = len(fakeImage.content)
 					targetOffset := uint32(len(fakeImage.content)) - uint32(rela.Off) + uint32(rela.Addend)
