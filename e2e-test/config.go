bc81078f94608b90a1b5fd157a1d94d113905f94
STRRL <str_ruiling@outlook.com>
2021-11-18 12:39:47 +0800

    chore: restructure image for primiary components in helm (#2496)
    * chore: restructure image for primiary components in helm
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: update local-up-chaos-mesh.sh
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: setup global tag and registry
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: apply customized registry for each component
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * fix(helm): incorrect reference to registry
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: reformat helm charts
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: fix e2e installation
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: fix e2e installation
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    * chore: remove refernce with registry-prefix
    
    Signed-off-by: STRRL <str_ruiling@outlook.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- e2e-test/config.go
+++ e2e-test/config.go
@@ -17,32 +17,36 @@ package test
 
 // Config describe the basic config for the operator test
 type Config struct {
-	ChartDir         string
-	ManifestDir      string
-	Tag              string
-	ManagerImage     string
-	ManagerTag       string
-	DaemonImage      string
-	DaemonTag        string
-	E2EImage         string
-	ChaosDNSImage    string
-	InstallChaosMesh bool
-	EnableDashboard  bool
+	ChartDir             string
+	ManifestDir          string
+	Tag                  string
+	ManagerImageRegistry string
+	ManagerImage         string
+	ManagerTag           string
+	DaemonImageRegistry  string
+	DaemonImage          string
+	DaemonTag            string
+	E2EImage             string
+	ChaosDNSImage        string
+	InstallChaosMesh     bool
+	EnableDashboard      bool
 }
 
 // NewDefaultConfig describe the default configuration for operator test
 func NewDefaultConfig() *Config {
 	return &Config{
-		ChartDir:         "/charts",
-		ManifestDir:      "/manifests",
-		Tag:              "e2e",
-		ManagerImage:     "localhost:5000/pingcap/chaos-mesh",
-		ManagerTag:       "latest",
-		DaemonImage:      "localhost:5000/pingcap/chaos-daemon",
-		DaemonTag:        "latest",
-		E2EImage:         "localhost:5000/pingcap/e2e-helper:latest",
-		ChaosDNSImage:    "localhost:5000/pingcap/chaos-dns:latest",
-		InstallChaosMesh: false,
-		EnableDashboard:  false,
+		ChartDir:             "/charts",
+		ManifestDir:          "/manifests",
+		Tag:                  "e2e",
+		ManagerImageRegistry: "localhost:5000",
+		ManagerImage:         "pingcap/chaos-mesh",
+		ManagerTag:           "latest",
+		DaemonImageRegistry:  "localhost:5000",
+		DaemonImage:          "pingcap/chaos-daemon",
+		DaemonTag:            "latest",
+		E2EImage:             "localhost:5000/pingcap/e2e-helper:latest",
+		ChaosDNSImage:        "localhost:5000/pingcap/chaos-dns:latest",
+		InstallChaosMesh:     false,
+		EnableDashboard:      false,
 	}
 }
