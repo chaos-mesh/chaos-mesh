41a25aeb7fef780fa8e376ffb98f0520ae5bb675
TangliziGit <tanglizimail@foxmail.com>
2021-11-23 10:42:44 +0800

    chore: update image project name in e2e-test
    Signed-off-by: TangliziGit <tanglizimail@foxmail.com>

--- hack/run-e2e.sh
+++ hack/run-e2e.sh
@@ -66,8 +66,8 @@ echo "DOCKER_REGISTRY: $DOCKER_REGISTRY"
 
 function e2e::image_load() {
     local images=(
-        pingcap/chaos-mesh
-        pingcap/chaos-daemon
+        chaos-mesh/chaos-mesh
+        chaos-mesh/chaos-daemon
         pingcap/e2e-helper
     )
     if [ "$PROVIDER" == "kind" ]; then
