b1b0647c63af9e8eb68257edd29825d7bcef222c
YangKeao <yangkeao@chunibyo.icu>
2021-11-24 15:58:30 +0800

    avoid tls downgrade attack by setting min version of tls
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- pkg/chaosdaemon/server.go
+++ pkg/chaosdaemon/server.go
@@ -131,6 +131,7 @@ func newGRPCServer(containerRuntime string, reg prometheus.Registerer, tlsConf t
 			Certificates: []tls.Certificate{serverCert},
 			ClientCAs:    caCertPool,
 			ClientAuth:   tls.RequireAndVerifyClientCert,
+			MinVersion:   tls.VersionTLS13,
 		})
 
 		grpcOpts = append(grpcOpts, grpc.Creds(creds))
