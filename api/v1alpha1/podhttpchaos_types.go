608600b38d1cbc5677025e195193f316f105509d
andrewmatilde <davis6813585853062@outlook.com>
2021-12-08 17:46:24 +0800

    try
    Signed-off-by: andrewmatilde <davis6813585853062@outlook.com>

--- api/v1alpha1/podhttpchaos_types.go
+++ api/v1alpha1/podhttpchaos_types.go
@@ -17,6 +17,7 @@ package v1alpha1
 
 import (
 	"encoding/json"
+	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -106,6 +107,8 @@ func (p PodHttpChaosReplaceBodyAction) MarshalJSON() ([]byte, error) {
 }
 
 func (p *PodHttpChaosReplaceBodyAction) UnmarshalJSON(data []byte) error {
+	fmt.Println("fffffffffffffffffffffaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+	fmt.Println(string(data))
 	var pp PodHttpChaosPatchBodyAction
 	err := json.Unmarshal(data, &pp)
 	if err == nil {
