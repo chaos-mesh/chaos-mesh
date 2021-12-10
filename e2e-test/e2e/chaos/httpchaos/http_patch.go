63345a39946779dabe0774d5843dca26f82e5772
andrewmatilde <davis6813585853062@outlook.com>
2021-12-10 17:33:43 +0800

    rollback unpause in pause test
    Signed-off-by: andrewmatilde <davis6813585853062@outlook.com>

--- e2e-test/e2e/chaos/httpchaos/http_patch.go
+++ e2e-test/e2e/chaos/httpchaos/http_patch.go
@@ -340,6 +340,55 @@ func TestcaseHttpPatchPauseAndUnPause(
 	})
 	framework.ExpectNoError(err, "fail to recover http chaos")
 
+	By("resume http patch chaos experiment")
+	// resume experiment
+	err = util.UnPauseChaos(ctx, cli, httpChaos)
+	framework.ExpectNoError(err, "resume chaos error")
+
+	By("assert that http patch is effective again")
+	err = wait.Poll(1*time.Second, 1*time.Minute, func() (done bool, err error) {
+		chaos := &v1alpha1.HTTPChaos{}
+		err = cli.Get(ctx, chaosKey, chaos)
+		framework.ExpectNoError(err, "get http chaos error")
+
+		for _, c := range chaos.GetStatus().Conditions {
+			if c.Type == v1alpha1.ConditionAllInjected {
+				if c.Status != corev1.ConditionTrue {
+					return false, nil
+				}
+			} else if c.Type == v1alpha1.ConditionSelected {
+				if c.Status != corev1.ConditionTrue {
+					return false, nil
+				}
+			}
+		}
+
+		return true, err
+	})
+	framework.ExpectNoError(err, "check resumed chaos failed")
+
+	err = wait.PollImmediate(1*time.Second, 1*time.Minute, func() (bool, error) {
+		resp, err := getPodHttp(c, port, secret, body)
+		if err != nil {
+			return false, err
+		}
+		defer resp.Body.Close()
+
+		s := strings.Join(resp.Header[SECRET], "; ")
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return false, err
+		}
+
+		klog.Infof("Status(%d), Secret(%s), Body(%s)", resp.StatusCode, s, string(b))
+
+		if s == expectedSecret && string(b) == expectedBody {
+			return true, nil
+		}
+		return false, nil
+	})
+	framework.ExpectNoError(err, "HTTP chaos doesn't work as expected")
+
 	By("cleanup")
 	// cleanup
 	cli.Delete(ctx, httpChaos)
--- e2e-test/e2e/chaos/httpchaos/http_replace.go
+++ e2e-test/e2e/chaos/httpchaos/http_replace.go
@@ -327,6 +327,54 @@ func TestcaseHttpReplacePauseAndUnPause(
 		return false, nil
 	})
 	framework.ExpectNoError(err, "fail to recover http chaos")
+	By("resume http replace chaos experiment")
+	// resume experiment
+	err = util.UnPauseChaos(ctx, cli, httpChaos)
+	framework.ExpectNoError(err, "resume chaos error")
+
+	By("assert that http replace is effective again")
+	err = wait.Poll(1*time.Second, 1*time.Minute, func() (done bool, err error) {
+		chaos := &v1alpha1.HTTPChaos{}
+		err = cli.Get(ctx, chaosKey, chaos)
+		framework.ExpectNoError(err, "get http chaos error")
+
+		for _, c := range chaos.GetStatus().Conditions {
+			if c.Type == v1alpha1.ConditionAllInjected {
+				if c.Status != corev1.ConditionTrue {
+					return false, nil
+				}
+			} else if c.Type == v1alpha1.ConditionSelected {
+				if c.Status != corev1.ConditionTrue {
+					return false, nil
+				}
+			}
+		}
+
+		return true, err
+	})
+	framework.ExpectNoError(err, "check resumed chaos failed")
+
+	err = wait.PollImmediate(1*time.Second, 1*time.Minute, func() (bool, error) {
+		resp, err := getPodHttp(c, port, secret, body)
+		if err != nil {
+			return false, err
+		}
+		defer resp.Body.Close()
+
+		s := resp.Header.Get(SECRET)
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return false, err
+		}
+
+		klog.Infof("Status(%d), Secret(%s), Body(%s)", resp.StatusCode, s, string(b))
+
+		if s == replaceSecret && string(b) == replacebody {
+			return true, nil
+		}
+		return false, nil
+	})
+	framework.ExpectNoError(err, "HTTP chaos doesn't work as expected")
 
 	By("cleanup")
 	// cleanup
