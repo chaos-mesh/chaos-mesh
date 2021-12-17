f990aa5e32e5ee2b324bde5778bcf8ab2bda474c
Yue Yang <g1enyy0ung@gmail.com>
2021-12-17 13:58:36 +0800

    feat!: UI monorepo (#2590) (#2653)
    * feat!: UI monorepo (#2590)
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: ci
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: add missing changes
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

---
+++ ui/app/src/api/auth.ts
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2021 Chaos Mesh Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import http from 'api/http'
+
+let tokenInterceptorId: number
+
+interface GCPToken {
+  accessToken: string
+  expiry: string
+}
+
+export const token = (token: string | GCPToken) => {
+  if (tokenInterceptorId !== undefined) {
+    http.interceptors.request.eject(tokenInterceptorId)
+  }
+
+  const headers: {
+    Authorization?: string
+    'X-Authorization-Method'?: string
+    'X-Authorization-AccessToken'?: string
+    'X-Authorization-Expiry'?: string
+  } =
+    typeof token === 'string'
+      ? {
+          Authorization: `Bearer ${token}`,
+        }
+      : {
+          'X-Authorization-Method': 'gcp',
+          'X-Authorization-AccessToken': token.accessToken,
+          'X-Authorization-Expiry': token.expiry,
+        }
+
+  tokenInterceptorId = http.interceptors.request.use((config) => {
+    config.headers = {
+      ...config.headers,
+      ...headers,
+    }
+
+    return config
+  })
+}
+
+export const resetToken = () => http.interceptors.request.eject(tokenInterceptorId)
+
+let nsInterceptorId: number
+
+export const namespace = (ns: string) => {
+  if (nsInterceptorId !== undefined) {
+    http.interceptors.request.eject(nsInterceptorId)
+  }
+
+  nsInterceptorId = http.interceptors.request.use((config) => {
+    if (/state|workflows|schedules|experiments|events|archives$/g.test(config.url!)) {
+      config.params = {
+        ...config.params,
+        namespace: ns === 'All' ? null : ns,
+      }
+    }
+
+    return config
+  })
+}
---
+++ ui/packages/mui-extends/src/PaperTop/index.tsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Chaos Mesh Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import { Box, BoxProps, Divider, Typography } from '@mui/material'
+
+interface PaperTopProps {
+  title: string | JSX.Element
+  subtitle?: string | JSX.Element
+  divider?: boolean
+  boxProps?: BoxProps
+}
+
+const PaperTop: React.FC<PaperTopProps> = ({ title, subtitle, divider, boxProps, children }) => (
+  <Box {...boxProps} display="flex" justifyContent="space-between" width="100%">
+    <Box flex={1}>
+      <Typography variant="h3" gutterBottom={subtitle || divider ? true : false}>
+        {title}
+      </Typography>
+      {subtitle && (
+        <Typography variant="body2" color="textSecondary">
+          {subtitle}
+        </Typography>
+      )}
+      {divider && <Divider />}
+    </Box>
+    {children}
+  </Box>
+)
+
+export default PaperTop
