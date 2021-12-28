bbbe80a595a5f88cc45ff4b79611432ec5841971
xixi <i@hexilee.me>
2021-12-28 14:24:29 +0800

    print spec in debugging iochaos and httpchaos
    Signed-off-by: xixi <i@hexilee.me>

--- pkg/chaosctl/debug/httpchaos/debug.go
+++ pkg/chaosctl/debug/httpchaos/debug.go
@@ -1,3 +1,18 @@
+// Copyright 2021 Chaos Mesh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package httpchaos
 
 import (
@@ -7,6 +22,7 @@ import (
 
 	"github.com/hasura/go-graphql-client"
 
+	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
 	"github.com/chaos-mesh/chaos-mesh/pkg/chaosctl/common"
 	ctrlclient "github.com/chaos-mesh/chaos-mesh/pkg/ctrl/client"
 )
@@ -28,6 +44,7 @@ func Debug(ctx context.Context, namespace, chaosName string, client *ctrlclient.
 				Podhttp []struct {
 					Namespace string
 					Name      string
+					Spec      *v1alpha1.PodHttpChaosSpec
 					Pod       struct {
 						Iptables  string
 						Processes []struct {
@@ -78,6 +95,11 @@ func Debug(ctx context.Context, namespace, chaosName string, client *ctrlclient.
 					Value: strings.Join(fds, "\n"),
 				})
 			}
+			output, err := common.MarshalChaos(podhttpchaos.Spec)
+			if err != nil {
+				return nil, err
+			}
+			podResult.Items = append(podResult.Items, common.ItemResult{Name: "podhttpchaos", Value: output})
 			result.Pods = append(result.Pods, podResult)
 		}
 
--- pkg/chaosctl/debug/iochaos/debug.go
+++ pkg/chaosctl/debug/iochaos/debug.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/hasura/go-graphql-client"
 
+	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
 	"github.com/chaos-mesh/chaos-mesh/pkg/chaosctl/common"
 	ctrlclient "github.com/chaos-mesh/chaos-mesh/pkg/ctrl/client"
 )
@@ -43,7 +44,20 @@ func Debug(ctx context.Context, namespace, chaosName string, client *ctrlclient.
 				Podios []struct {
 					Namespace string
 					Name      string
-					Pod       struct {
+					Spec      struct {
+						VolumeMountPath string
+						Container       *string
+						Actions         []struct {
+							Type            v1alpha1.IOChaosType
+							v1alpha1.Filter `json:",inline"`
+							Faults          []v1alpha1.IoFault
+							Latency         string
+							// *v1alpha1.AttrOverrideSpec `json:",inline"`
+							// *v1alpha1.MistakeSpec      `json:",inline"`
+							// Source                     string
+						}
+					}
+					Pod struct {
 						Mounts    []string
 						Processes []struct {
 							Pid     string
@@ -93,6 +107,11 @@ func Debug(ctx context.Context, namespace, chaosName string, client *ctrlclient.
 					Value: strings.Join(fds, "\n"),
 				})
 			}
+			output, err := common.MarshalChaos(podiochaos.Spec)
+			if err != nil {
+				return nil, err
+			}
+			podResult.Items = append(podResult.Items, common.ItemResult{Name: "podiochaos", Value: output})
 			result.Pods = append(result.Pods, podResult)
 		}
 
