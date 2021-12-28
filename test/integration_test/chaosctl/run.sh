a6e848d4bc082f80e0ceba25494116c5bbfa8096
xixi <i@hexilee.me>
2021-12-28 16:59:35 +0800

    fix chaosctl integration test
    Signed-off-by: xixi <i@hexilee.me>

--- test/integration_test/chaosctl/run.sh
+++ test/integration_test/chaosctl/run.sh
@@ -46,9 +46,9 @@ kubectl apply -f delay.yaml
 
 echo "Checking chaosctl function"
 ./chaosctl logs 1>/dev/null
-status=$(./chaosctl debug -i networkchaos web-show-network-delay | grep "Execute as expected")
+status=$(./chaosctl debug networkchaos web-show-network-delay | grep "Execute as expected")
 if [[ -z "$status" ]]; then
-    ./chaosctl debug -i networkchaos web-show-network-delay
+    ./chaosctl debug networkchaos web-show-network-delay
     echo "Chaos is not running as expected"
     code=1
 fi
