7787b3c9f82e3ad3fc83000ea2749389e989b8b1
Craig Seeman <cseeman@zendesk.com>
2022-01-04 17:08:11 -0600

    Basic changes

--- pkg/grpc/utils.go
+++ pkg/grpc/utils.go
@@ -37,7 +37,7 @@ const DefaultRPCTimeout = 60 * time.Second
 // RPCTimeout specifies timeout of RPC between controller and chaos-operator
 var RPCTimeout = DefaultRPCTimeout
 
-var ChaosDaemonServerName = config.TLSConfig.ChaosDaemonServerName
+var ChaosDaemonServerName = config.TLSConfig.ChaosDaemonCertServerName
 
 type TLSRaw struct {
 	CaCert []byte
