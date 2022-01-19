67205a39db8c5fc81e95c411a17ff575978f03fa
STRRL <str_ruiling@outlook.com>
2022-01-19 18:53:42 +0800

    chore: use development logger for chaos-builder
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- cmd/chaos-builder/main.go
+++ cmd/chaos-builder/main.go
@@ -17,6 +17,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-logr/zapr"
+	"go.uber.org/zap"
 	"go/ast"
 	"go/parser"
 	"go/token"
@@ -25,11 +27,11 @@ import (
 	"strings"
 
 	"github.com/pingcap/errors"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 var (
-	log = zap.New(zap.UseDevMode(true))
+	zapLogger, _ = zap.NewDevelopment()
+	log          = zapr.NewLogger(zapLogger)
 )
 
 type metadata struct {
