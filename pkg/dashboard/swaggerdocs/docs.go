618a4cb8e697d7545b211e0a86aa2bbdb018c22a
YangKeao <keao.yang@yahoo.com>
2022-01-19 11:21:45 +0800

    blockchaos: api definition and selector implementation (#2739)
    * add basic type for blockchaos
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * bootstrap blockchaos and nodevolumepath selector
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * generate files from blockchaos
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * add licenses for examples
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * group the imports
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * register an unknown blockchaos
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * try to add dashboard for blockchaos
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * add webhook
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * regenerate yaml files
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * frontend implementation of blockchaos
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * make check
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- pkg/dashboard/swaggerdocs/docs.go
+++ pkg/dashboard/swaggerdocs/docs.go
@@ -2756,6 +2756,70 @@ var doc = `{
                 }
             }
         },
+        "v1alpha1.BlockChaosSpec": {
+            "type": "object",
+            "properties": {
+                "action": {
+                    "description": "Action defines the specific block chaos action.\nSupported action: limit / delay\n+kubebuilder:validation:Enum=limit;delay",
+                    "type": "string"
+                },
+                "containerNames": {
+                    "description": "ContainerNames indicates list of the name of affected container.\nIf not set, the first container will be injected\n+optional",
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
+                },
+                "delay": {
+                    "description": "Delay defines the delay distribution.\n+optional",
+                    "type": "object",
+                    "$ref": "#/definitions/v1alpha1.BlockDelaySpec"
+                },
+                "duration": {
+                    "description": "Duration represents the duration of the chaos action.\n+optional",
+                    "type": "string"
+                },
+                "iops": {
+                    "description": "IOPS defines the limit of IO frequency.\n+optional",
+                    "type": "integer"
+                },
+                "mode": {
+                    "description": "Mode defines the mode to run chaos action.\nSupported mode: one / all / fixed / fixed-percent / random-max-percent\n+kubebuilder:validation:Enum=one;all;fixed;fixed-percent;random-max-percent",
+                    "type": "string"
+                },
+                "selector": {
+                    "description": "Selector is used to select pods that are used to inject chaos action.",
+                    "type": "object",
+                    "$ref": "#/definitions/v1alpha1.PodSelectorSpec"
+                },
+                "value": {
+                    "description": "Value is required when the mode is set to ` + "`" + `FixedMode` + "`" + ` / ` + "`" + `FixedPercentMode` + "`" + ` / ` + "`" + `RandomMaxPercentMode` + "`" + `.\nIf ` + "`" + `FixedMode` + "`" + `, provide an integer of pods to do chaos action.\nIf ` + "`" + `FixedPercentMode` + "`" + `, provide a number from 0-100 to specify the percent of pods the server can do chaos action.\nIF ` + "`" + `RandomMaxPercentMode` + "`" + `,  provide a number from 0-100 to specify the max percent of pods to do chaos action\n+optional",
+                    "type": "string"
+                },
+                "volumeName": {
+                    "type": "string"
+                }
+            }
+        },
+        "v1alpha1.BlockDelaySpec": {
+            "type": "object",
+            "properties": {
+                "correlation": {
+                    "description": "+optional",
+                    "type": "string",
+                    "default": "0"
+                },
+                "jitter": {
+                    "description": "+optional",
+                    "type": "string",
+                    "default": "0ms"
+                },
+                "latency": {
+                    "description": "Latency defines the latency of every io request.",
+                    "type": "string"
+                }
+            }
+        },
         "v1alpha1.CPUStressor": {
             "type": "object",
             "properties": {
@@ -2783,6 +2847,11 @@ var doc = `{
                     "description": "+optional",
                     "type": "object",
                     "$ref": "#/definitions/v1alpha1.AWSChaosSpec"
+                },
+                "blockChaos": {
+                    "description": "+optional",
+                    "type": "object",
+                    "$ref": "#/definitions/v1alpha1.BlockChaosSpec"
                 },
                 "concurrencyPolicy": {
                     "description": "+optional\n+kubebuilder:validation:Enum=Forbid;Allow",
@@ -4264,6 +4333,11 @@ var doc = `{
                     "type": "object",
                     "$ref": "#/definitions/v1alpha1.AWSChaosSpec"
                 },
+                "blockChaos": {
+                    "description": "+optional",
+                    "type": "object",
+                    "$ref": "#/definitions/v1alpha1.BlockChaosSpec"
+                },
                 "concurrencyPolicy": {
                     "description": "+optional\n+kubebuilder:validation:Enum=Forbid;Allow",
                     "type": "string"
@@ -4461,6 +4535,11 @@ var doc = `{
                     "description": "+optional",
                     "type": "object",
                     "$ref": "#/definitions/v1alpha1.AWSChaosSpec"
+                },
+                "blockChaos": {
+                    "description": "+optional",
+                    "type": "object",
+                    "$ref": "#/definitions/v1alpha1.BlockChaosSpec"
                 },
                 "children": {
                     "description": "Children describes the children steps of serial or parallel node. Only used when Type is TypeSerial or TypeParallel.\n+optional",
