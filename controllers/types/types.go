4e2ce01d5ee20e34d9478c5aac2b2e205dc3adcd
“fewdan” <fewdan@hotmail.com>
2022-01-20 22:21:06 +0800

    make check
    Signed-off-by: “fewdan” <fewdan@hotmail.com>

--- controllers/types/types.go
+++ controllers/types/types.go
@@ -128,13 +128,16 @@ var ChaosObjects = fx.Supply(
 	fx.Annotated{
 		Group: "objs",
 		Target: Object{
-<<<<<<< HEAD
 			Name:   "azurechaos",
 			Object: &v1alpha1.AzureChaos{},
-=======
+		},
+	},
+
+	fx.Annotated{
+		Group: "objs",
+		Target: Object{
 			Name:   "blockchaos",
 			Object: &v1alpha1.BlockChaos{},
->>>>>>> upstream/master
 		},
 	},
 )
