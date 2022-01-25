0e771cfd3afd01f98a53a04a212c56b0231f015b
rksharma95 <nikhilrk13jan@gmail.com>
2022-01-25 12:53:39 +0530

    azk8spull function removed
    Signed-off-by: rksharma95 <nikhilrk13jan@gmail.com>

--- install.sh
+++ install.sh
@@ -721,60 +721,6 @@ ensure_pods_ready() {
     done
 }
 
-azk8spull() {
-	image=$1
-	if [ -z $image ]; then
-		echo "## azk8spull image name cannot be null."
-	else
-		array=(`echo $image | tr '/' ' '` )
-
-		domainName=""
-		repoName=""
-		imageName=""
-
-		if [ ${#array[*]} -eq 3 ]; then
-			repoName=${array[1]}
-			imageName=${array[2]}
-			if [ "${array[0]}"x = "docker.io"x ]; then
-				domainName="dockerhub.azk8s.cn"
-			elif [ "${array[0]}"x = "gcr.io"x ]; then
-				domainName="gcr.azk8s.cn"
-			elif [ "${array[0]}"x = "quay.io"x ]; then
-				domainName="quay.azk8s.cn"
-			else
-				echo "## azk8spull can not support pulling $image right now."
-			fi
-		elif [ ${#array[*]} -eq 2 ]; then
-			if [ "${array[0]}"x = "k8s.gcr.io"x ]; then
-				domainName="gcr.azk8s.cn"
-				repoName="google_containers"
-				imageName=${array[1]}
-			else
-				domainName="dockerhub.azk8s.cn"
-				repoName=${array[0]}
-				imageName=${array[1]}
-			fi
-		elif [ ${#array[*]} -eq 1 ]; then
-				domainName="dockerhub.azk8s.cn"
-				repoName="library"
-				imageName=${array[0]}
-		else
-			echo '## azk8spull can not support pulling $image right now.'
-		fi
-		if [ "$domainName" != "" ]; then
-			echo "## azk8spull try to pull image from mirror $domainName/$repoName/$imageName."
-			docker pull  $domainName/$repoName/$imageName
-			if [ $? -eq 0 ]; then
-				echo "## azk8spull try to tag $domainName/$repoName/$imageName to $image."
-				docker tag $domainName/$repoName/$imageName $image
-				if [ $? -eq 0 ]; then
-					echo '## azk8spull finish pulling. '
-				fi
-			fi
-		fi
-	fi
-}
-
 gen_crd_manifests() {
     local crd=$1
 
