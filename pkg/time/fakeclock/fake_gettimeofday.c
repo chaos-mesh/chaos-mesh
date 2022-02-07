5944ba90922c820a1115842c2302a364c11d8bba
STRRL <str_ruiling@outlook.com>
2022-02-07 20:51:06 +0800

    chore: remove double underscore with variable tz
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- pkg/time/fakeclock/fake_gettimeofday.c
+++ pkg/time/fakeclock/fake_gettimeofday.c
@@ -24,23 +24,23 @@ extern int64_t TV_SEC_DELTA;
 extern int64_t TV_NSEC_DELTA;
 
 #if defined(__amd64__)
-inline int real_gettimeofday(struct timeval *tv, struct timezone *__tz)
+inline int real_gettimeofday(struct timeval *tv, struct timezone *tz)
 {
     int ret;
     asm volatile(
         "syscall"
         : "=a"(ret)
-        : "0"(__NR_gettimeofday), "D"(tv), "S"(__tz)
+        : "0"(__NR_gettimeofday), "D"(tv), "S"(tz)
         : "memory");
 
     return ret;
 }
 
 #elif defined(__aarch64__)
-inline int real_gettimeofday(struct timeval *tv, struct timezone *__tz)
+inline int real_gettimeofday(struct timeval *tv, struct timezone *tz)
 {
     register struct timeval *x0 __asm__("x0") = tv;
-    register struct timezone *x1 __asm__("x1") = __tz;
+    register struct timezone *x1 __asm__("x1") = tz;
     register uint64_t w8 __asm__("w8") = __NR_gettimeofday; /* syscall number */
     __asm__ __volatile__(
         "svc 0;"
@@ -52,9 +52,9 @@ inline int real_gettimeofday(struct timeval *tv, struct timezone *__tz)
 }
 #endif
 
-int gettimeofday(struct timeval *tv, struct timezone *__tz)
+int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
-    int ret = real_gettimeofday(tv, __tz);
+    int ret = real_gettimeofday(tv, tz);
 
     int64_t sec_delta = TV_SEC_DELTA;
     int64_t nsec_delta = TV_NSEC_DELTA;
