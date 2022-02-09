799dc598160d63ad35f08044ae100d05617019bb
Yue Yang <g1enyy0ung@gmail.com>
2022-02-09 20:17:36 +0800

    fix: some errors in install.sh #2858 (#2860)
    * fix: some errors in install.sh #2858
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: ci
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- install.sh
+++ install.sh
@@ -216,7 +216,7 @@ main() {
     done
 
     if [ "${runtime}" != "docker" ] && [ "${runtime}" != "containerd" ]; then
-        printf "container runtime %s is not supported\n" "${local_kube}"
+        printf "container runtime %s is not supported\n" "${runtime}"
         exit 1
     fi
 
@@ -430,6 +430,10 @@ EOF
     ensure kind create cluster --config "${config_file}" --image="${kind_image}" --name="${cluster_name}" --retain -v 1
     ensure kind get kubeconfig --name="${cluster_name}" > "${kubeconfig_path}"
     ensure export KUBECONFIG="${kubeconfig_path}"
+
+    if [ "$volume_provisioner" == "true" ]; then
+        deploy_volume_provisioner "${work_dir}"
+    fi
 }
 
 deploy_volume_provisioner() {
@@ -606,11 +610,8 @@ install_chaos_mesh() {
     local timezone=${9}
     local docker_registry=${10}
     local microk8s=${11}
-    printf "Install Chaos Mesh %s\n" "${release_name}"
 
-    local chaos_mesh_image="${docker_registry}/pingcap/chaos-mesh:${version}"
-    local chaos_daemon_image="${docker_registry}/pingcap/chaos-daemon:${version}"
-    local chaos_dashboard_image="${docker_registry}/pingcap/chaos-dashboard:${version}"
+    printf "Install Chaos Mesh %s\n" "${release_name}"
 
     gen_crd_manifests "${crd}" | kubectl create --validate=false -f - || exit 1
     gen_chaos_mesh_manifests "${runtime}" "${k3s}" "${version}" "${timezone}" "${host_network}" "${docker_registry}" "${microk8s}" | kubectl apply -f - || exit 1
