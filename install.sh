572e30f6541b7afc15469f9c671d4a3e918ae4c7
Yue Yang <g1enyy0ung@gmail.com>
2022-02-10 11:14:10 +0800

    fix: remove empty priorityClassName (#2863)
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- install.sh
+++ install.sh
@@ -1128,7 +1128,6 @@ spec:
       serviceAccountName: chaos-daemon
       hostIPC: true
       hostPID: true
-      priorityClassName: 
       containers:
         - name: chaos-daemon
           image: ${DOCKER_REGISTRY_PREFIX}/pingcap/chaos-daemon:${VERSION_TAG}
@@ -1200,7 +1199,6 @@ spec:
         rollme: "install.sh"
     spec:
       serviceAccountName: chaos-controller-manager
-      priorityClassName: 
       containers:
         - name: chaos-dashboard
           image: ${DOCKER_REGISTRY_PREFIX}/pingcap/chaos-dashboard:${VERSION_TAG}
@@ -1276,7 +1274,6 @@ spec:
     spec:
       hostNetwork: ${host_network}
       serviceAccountName: chaos-controller-manager
-      priorityClassName: 
       containers:
       - name: chaos-mesh
         image: ${DOCKER_REGISTRY_PREFIX}/pingcap/chaos-mesh:${VERSION_TAG}
