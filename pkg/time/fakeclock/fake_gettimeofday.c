87345c265c40bc6e23edf2c8edfaf742e8fb11b4
YangKeao <yangkeao@chunibyo.icu>
2022-02-11 07:39:31 +0000

    use an extra w0 register
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- pkg/time/fakeclock/fake_gettimeofday.c
+++ pkg/time/fakeclock/fake_gettimeofday.c
@@ -39,16 +39,18 @@ inline int real_gettimeofday(struct timeval *tv, struct timezone *tz)
 #elif defined(__aarch64__)
 inline int real_gettimeofday(struct timeval *tv, struct timezone *tz)
 {
+    register int w0 __asm__("w0");
+
     register struct timeval *x0 __asm__("x0") = tv;
     register struct timezone *x1 __asm__("x1") = tz;
     register uint64_t w8 __asm__("w8") = __NR_gettimeofday; /* syscall number */
     __asm__ __volatile__(
         "svc 0;"
-        : "+r"(x0)
+        : "+r"(w0)
         : "r"(x0), "r" (x1), "r"(w8)
         : "memory");
 
-    return x0;
+    return w0;
 }
 #endif
 
