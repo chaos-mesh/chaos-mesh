a575905039835da9d3b13b6b36a940de02df1702
YangKeao <yangkeao@chunibyo.icu>
2022-02-23 15:46:26 +0800

    auto enable ioem scheduler
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- pkg/chaosdaemon/blockchaos_server.go
+++ pkg/chaosdaemon/blockchaos_server.go
@@ -18,27 +18,36 @@ package chaosdaemon
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/chaos-mesh/chaos-driver/pkg/client"
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/pkg/errors"
 
+	"github.com/chaos-mesh/chaos-mesh/pkg/bpm"
 	pb "github.com/chaos-mesh/chaos-mesh/pkg/chaosdaemon/pb"
 )
+
+const chaosDaemonHelperCommand = "cdh"
 
 const ioemPeriodUs = 10000
 
 func (s *DaemonServer) ApplyBlockChaos(ctx context.Context, req *pb.ApplyBlockChaosRequest) (*pb.ApplyBlockChaosResponse, error) {
-	volumeName, err := normalizeVolumeName(req.VolumePath)
+	volumeName, err := normalizeVolumeName(ctx, req.VolumePath)
 	if err != nil {
 		log.Error(err, "normalize volume name", "volumePath", req.VolumePath)
 		return nil, err
 	}
 
-	// TODO: automatically modify the elevator to the `ioem` or `ioem-mq`
+	err = enableIOEMElevator(volumeName)
+	if err != nil {
+		log.Error(err, "error while enabling ioem elevator", "volumeName", volumeName)
+		return nil, errors.Wrapf(err, "enable ioem elevator for volume %s", volumeName)
+	}
 
 	volumePath := "/dev/" + volumeName
 	if _, err := os.Stat(volumePath); err != nil {
+		log.Error(err, "error while getting stat of volume", "volumePath", volumePath)
 		return nil, errors.Wrapf(err, "volume path %s does not exist", volumePath)
 	}
 
@@ -77,6 +86,48 @@ func (s *DaemonServer) ApplyBlockChaos(ctx context.Context, req *pb.ApplyBlockCh
 		}, nil
 	} else {
 		return nil, errors.New("unknown action")
+	}
+}
+
+func normalizeVolumeName(ctx context.Context, volumePath string) (string, error) {
+	volumeName, err := bpm.DefaultProcessBuilder(chaosDaemonHelperCommand, "normalize-volume-name", volumePath).SetContext(ctx).SetNS(1, bpm.PidNS).Build().Output()
+	if err != nil {
+		return "", errors.Wrapf(err, "normalize volume name %s", volumePath)
+	}
+
+	return strings.Trim(string(volumeName), "\n\x00"), nil
+}
+
+func enableIOEMElevator(volumeName string) error {
+	schedulerPath := "/sys/block/" + volumeName + "/queue/scheduler"
+	rawSchedulers, err := os.ReadFile(schedulerPath)
+	if err != nil {
+		return errors.Wrapf(err, "reading schedulers %s", schedulerPath)
+	}
+
+	schedulers := strings.Split(strings.Trim(string(rawSchedulers), " \x00\n"), " ")
+	choosenScheduler := ""
+	for _, scheduler := range schedulers {
+		// TODO: record the current scheduler, and recover in the future
+		// Bue it's hard to decide whether a block device is affected by chaos
+
+		// The ioem scheduler without fault injection is only a simple FIFO,
+		// which is nearly the same with none scheduler. But for HDD, the
+		// none scheduler is much slower than other schedulers. For NVMe,
+		// the default scheduler is none, and it's fine to keep ioem without
+		// significant overhead.
+
+		// The current scheduler should be surrounded by "[]"
+
+		if strings.HasPrefix(scheduler, "ioem") {
+			choosenScheduler = scheduler // it's either ioem or ioem-mq
+		}
+	}
+
+	// it doesn't matter to pass any permission, because the file must exist
+	err = os.WriteFile(schedulerPath, []byte(choosenScheduler), 0000)
+	if err != nil {
+		return errors.Wrapf(err, "writing %s to %s", choosenScheduler, schedulerPath)
 	}
 }
 
