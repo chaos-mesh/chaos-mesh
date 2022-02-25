52c2f4ae83f87e1e341b7519a0e74e0a8084d1ad
Yue Yang <g1enyy0ung@gmail.com>
2022-02-25 13:43:45 +0800

    feat: OpenAPI to TypeScript API Client and Form Data (#2770)
    * feat: OpenAPI to TypeScript API Client and Forms
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: license checker
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: go (verify)
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: add comments
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: reuse kubebuilder marks
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: update according to RFC
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: prevent ui:form appearing in crds
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: add missing actions
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: update lockfile
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: support PhysicalMachineChaos
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: merge commands into codegen
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: license checker
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: ci
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: supplement another ignore check
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: update package info
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: update README
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: update
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: gen HTTPChaos
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * chore: update changelog
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: remove nested ignores
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: remove remaining markers
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: typo
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: supplement comments
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

---
+++ ui/packages/openapi/factory.test.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 Chaos Mesh Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import { isHTTTPChaosPatchHeadersOrQueries, typeTextToFieldType, typeTextToInitialValue } from './factory.js'
+
+import ts from 'typescript'
+import { visitFilter } from './utils.js'
+
+describe('openapi => factory', () => {
+  test('typeTextToFieldType', () => {
+    expect(typeTextToFieldType('string')).toBe('text')
+    expect(typeTextToFieldType('Array<string>')).toBe('label')
+    expect(typeTextToFieldType('Array<number>')).toBe('numbers')
+    expect(typeTextToFieldType('{ [key: string]: string }')).toBe('text-text')
+    expect(typeTextToFieldType('{ [key: string]: Array<string> }')).toBe('text-label')
+    expect(typeTextToFieldType('number')).toBe('number')
+    expect(typeTextToFieldType('boolean')).toBe('select')
+    expect(() => typeTextToFieldType('unknown')).toThrowError()
+  })
+
+  test('typeTextToInitialValue', () => {
+    expect(typeTextToInitialValue('string').text).toBe('')
+    expect(typeTextToInitialValue('Array<string>').elements).toBeInstanceOf(Array)
+    expect(typeTextToInitialValue('Array<number>').elements).toBeInstanceOf(Array)
+    expect(typeTextToInitialValue('{ [key: string]: string }').properties.length).toBe(0)
+    expect(typeTextToInitialValue('number').text).toBe('0')
+    expect(typeTextToInitialValue('boolean').elements).toBeInstanceOf(Array)
+    expect(() => typeTextToInitialValue('other')).toThrowError()
+  })
+
+  test('isHTTTPChaosPatchHeadersOrQueries finds headers and queries', () => {
+    const text = `export interface V1alpha1PodHttpChaosPatchActions {
+      headers?: Array<Array<string>>
+      queries?: Array<Array<string>>
+    }`
+    const sourceFile = ts.createSourceFile('tmp', text, ts.ScriptTarget.ES2015, true)
+    const nodes = visitFilter(sourceFile, [], ts.isPropertySignature)
+
+    nodes.forEach((node) => {
+      expect(isHTTTPChaosPatchHeadersOrQueries(node)).toBe(true)
+    })
+  })
+})
