90e3b540d53fe07b6f0a88aa1a54ca3732827d2e
STRRL <str_ruiling@outlook.com>
2022-03-28 17:10:31 +0800

    fix: use PROJECT_DIR
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- hack/env-image-tag.sh
+++ hack/env-image-tag.sh
@@ -35,7 +35,7 @@ if [ "$#" -eq  "0" ]; then
 fi
 
 if [[ "$1" == 'dev-env' || "$1" == 'build-env'  ]]; then
-  cat ./env-images.yaml | docker run -i mikefarah/yq:latest ".$1.tag"
+  cat $PROJECT_DIR/env-images.yaml | docker run -i mikefarah/yq:latest ".$1.tag"
   exit 0
 fi
 
