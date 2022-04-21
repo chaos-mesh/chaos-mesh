0efe93e9180ff76f3575afa2630d726de6cb43a5
YangKeao <yangkeao@chunibyo.icu>
2022-04-21 17:53:22 +0800

    fix compile error on arm
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- pkg/time/fake_image_linux_arm64.go
+++ pkg/time/fake_image_linux_arm64.go
@@ -17,8 +17,8 @@ package time
 
 import (
 	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
 
-	"github.com/chaos-mesh/chaos-mesh/pkg/cerr"
 	"github.com/chaos-mesh/chaos-mesh/pkg/mapreader"
 )
 
@@ -46,9 +46,9 @@ type FakeImage struct {
 }
 
 func (it *FakeImage) AttachToProcess(pid int, variables map[string]uint64) (err error) {
-	return cerr.NotImplemented("fake image arm64")
+	return errors.New("fake image arm64")
 }
 
 func (it *FakeImage) Recover(pid int, vars map[string]uint64) error {
-	return cerr.NotImplemented("fake image arm64")
+	return errors.New("fake image arm64")
 }
