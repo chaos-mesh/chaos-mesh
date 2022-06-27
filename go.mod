464a6c79a0a18530b545a3b9c947bb6ca1b93589
STRRL <im@strrl.dev>
2022-06-27 19:20:21 +0800

    chore: complete the implementation of ReleaseService
    Signed-off-by: STRRL <im@strrl.dev>

--- go.mod
+++ go.mod
@@ -71,11 +71,11 @@ require (
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8
 	golang.org/x/tools v0.1.11-0.20220322213029-87a8611856c1
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
-	google.golang.org/api v0.61.0
+	google.golang.org/api v0.62.0
 	google.golang.org/grpc v1.43.0
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/yaml.v2 v2.4.0
-	helm.sh/helm/v3 v3.9.0
+	helm.sh/helm/v3 v3.8.1
 	k8s.io/api v0.24.0
 	k8s.io/apimachinery v0.24.0
 	k8s.io/apiserver v0.24.0
@@ -167,7 +167,6 @@ require (
 	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/btree v1.0.1 // indirect
-	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
@@ -202,7 +201,7 @@ require (
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/moby/spdystream v0.2.0 // indirect
 	github.com/moby/sys/mountinfo v0.5.0 // indirect
@@ -263,7 +262,7 @@ require (
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	k8s.io/apiextensions-apiserver v0.24.0 // indirect
 	k8s.io/component-base v0.24.0 // indirect
-	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
+	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
 	nhooyr.io/websocket v1.8.6 // indirect
 	oras.land/oras-go v1.1.0 // indirect
 	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
--- pkg/helm/helm_client.go
+++ pkg/helm/helm_client.go
@@ -1,31 +1,94 @@
 package helm
 
 import (
+	"fmt"
 	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart"
+	"helm.sh/helm/v3/pkg/chartutil"
+	"helm.sh/helm/v3/pkg/kube"
+	"helm.sh/helm/v3/pkg/registry"
 	"helm.sh/helm/v3/pkg/release"
+	"helm.sh/helm/v3/pkg/storage"
+	"helm.sh/helm/v3/pkg/storage/driver"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
 type HelmClient struct {
 	restClientGetter genericclioptions.RESTClientGetter
 	logger           logr.Logger
-
-	helmConfiguration *action.Configuration
 }
 
-func (h *HelmClient) GetRelease(namespace string, releaseName string) (release.Release, error) {
-	//TODO implement me
-	panic("implement me")
+func NewHelmClient(restClientGetter genericclioptions.RESTClientGetter, logger logr.Logger) (*HelmClient, error) {
+	return &HelmClient{restClientGetter: restClientGetter, logger: logger}, nil
 }
 
-func (h *HelmClient) UpgradeOrInstall(namespace string, releaseName string, chart *chart.Chart, values map[string]interface{}) error {
-	//TODO implement me
-	panic("implement me")
+func (h HelmClient) spawnConfigurationWithNamespace(namespace string) (*action.Configuration, error) {
+	registryClient, err := registry.NewClient()
+	if err != nil {
+		return nil, errors.Wrap(err, "create helm registry client")
+	}
+	kubeclient := kube.New(h.restClientGetter)
+	if err != nil {
+		return nil, errors.Wrap(err, "create kubernetes client set")
+	}
+	clientset, err := kubeclient.Factory.KubernetesClientSet()
+	if err != nil {
+		return nil, errors.Wrap(err, "create kubernetes client set")
+	}
+	secretInterface := clientset.CoreV1().Secrets(namespace)
+	helmConfiguration := action.Configuration{
+		Releases:         storage.Init(driver.NewSecrets(secretInterface)),
+		KubeClient:       kubeclient,
+		Capabilities:     chartutil.DefaultCapabilities,
+		RegistryClient:   registryClient,
+		RESTClientGetter: h.restClientGetter,
+		Log: func(format string, v ...interface{}) {
+			h.logger.Info(fmt.Sprintf(format, v...))
+		},
+	}
+	return &helmConfiguration, nil
 }
 
-func (h *HelmClient) UninstallRelease(namespace string, releaseName string) error {
-	//TODO implement me
-	panic("implement me")
+func (h *HelmClient) GetRelease(namespace string, releaseName string) (*release.Release, error) {
+	configurationWithNamespace, err := h.spawnConfigurationWithNamespace(namespace)
+	if err != nil {
+		return nil, errors.Wrap(err, "create helm configuration")
+	}
+	getAction := action.NewGet(configurationWithNamespace)
+	result, err := getAction.Run(releaseName)
+	if err != nil {
+		return nil, errors.Wrapf(err, "get release %s, in namespace %s", releaseName, namespace)
+	}
+	return result, nil
+}
+
+func (h *HelmClient) UpgradeOrInstall(namespace string, releaseName string, chart *chart.Chart, values map[string]interface{}) (*release.Release, error) {
+	configurationWithNamespace, err := h.spawnConfigurationWithNamespace(namespace)
+	if err != nil {
+		return nil, errors.Wrap(err, "create helm configuration")
+	}
+	actionInstall := action.NewInstall(configurationWithNamespace)
+	actionInstall.ReleaseName = releaseName
+	actionInstall.Namespace = namespace
+	actionInstall.CreateNamespace = true
+	result, err := actionInstall.Run(chart, values)
+	if err != nil {
+		return nil, errors.Wrapf(err, "install release %s, with chart %s, with values %v", releaseName, chart.Metadata.Name, values)
+	}
+	return result, nil
+}
+
+func (h *HelmClient) UninstallRelease(namespace string, releaseName string) (*release.UninstallReleaseResponse, error) {
+	configurationWithNamespace, err := h.spawnConfigurationWithNamespace(namespace)
+	if err != nil {
+		return nil, errors.Wrap(err, "create helm configuration")
+	}
+	uninstallAction := action.NewUninstall(configurationWithNamespace)
+	response, err := uninstallAction.Run(releaseName)
+	if err != nil {
+		return nil, errors.Wrapf(err, "uninstall release %s, in namespace %s", releaseName, namespace)
+	}
+	return response, nil
 }
