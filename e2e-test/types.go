13054d000a6d39fea82dded73d33a443588fce9e
STRRL <im@strrl.dev>
2022-09-13 17:44:36 +0800

    chore: bump the version of chaos-coredns to v0.2.2
    Signed-off-by: STRRL <im@strrl.dev>

--- e2e-test/types.go
+++ e2e-test/types.go
@@ -81,7 +81,7 @@ func NewDefaultOperatorConfig() OperatorConfig {
 			Runtime:         "containerd",
 			SocketPath:      "/run/containerd/containerd.sock",
 		},
-		DNSImage: "ghcr.io/chaos-mesh/chaos-coredns:v0.2.1-r1",
+		DNSImage: "ghcr.io/chaos-mesh/chaos-coredns:v0.2.3",
 	}
 }
 
