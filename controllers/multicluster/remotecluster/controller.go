2617080f404efa31d3adcf293fcdef7ac8b37179
YangKeao <yangkeao@chunibyo.icu>
2022-11-15 09:53:55 -0500

    multicluster: auto install and uninstall the remote helm release (#3414)
    * add remotecluster type
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * add the infrastructure to operate multiple clusters
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * remove clientset
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * modify changelog
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * add license to the remote-cluster.yaml
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * fix Wrapf arguments
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * make check
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * make the lock private
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * chore: helm chart integration and basic interface
    
    Signed-off-by: STRRL <im@strrl.dev>
    
    * chore: complete the implementation of ReleaseService
    
    Signed-off-by: STRRL <im@strrl.dev>
    
    * chore: update changelog
    
    Signed-off-by: STRRL <im@strrl.dev>
    
    * install chaos mesh
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * add CHANGELOG
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * chore: helm chart integration and basic interface
    
    Signed-off-by: STRRL <im@strrl.dev>
    
    * chore: complete the implementation of ReleaseService
    
    Signed-off-by: STRRL <im@strrl.dev>
    
    * chore: update changelog
    
    Signed-off-by: STRRL <im@strrl.dev>
    
    * chore: fix make check
    
    Signed-off-by: STRRL <im@strrl.dev>
    
    * chore: update lincense checker config file
    
    Signed-off-by: STRRL <im@strrl.dev>
    
    * test: mark examples of helm as integration test
    
    Signed-off-by: STRRL <im@strrl.dev>
    
    * add condition initialization for remote cluster
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    * upgrade chaosmesh release version to 2.4.1
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    Signed-off-by: STRRL <im@strrl.dev>
    Co-authored-by: STRRL <im@strrl.dev>
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- controllers/multicluster/remotecluster/controller.go
+++ controllers/multicluster/remotecluster/controller.go
@@ -17,13 +17,14 @@ package remotecluster
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
+	"helm.sh/helm/v3/pkg/storage/driver"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -31,9 +32,12 @@ import (
 
 	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
 	"github.com/chaos-mesh/chaos-mesh/controllers/multicluster/clusterregistry"
+	"github.com/chaos-mesh/chaos-mesh/pkg/helm"
 )
 
 const remoteClusterControllerFinalizer = "chaos-mesh/remotecluster-controllers"
+const chaosMeshReleaseName = "chaos-mesh"
+const chaosMeshReleaseVersion = "2.4.1"
 
 type Reconciler struct {
 	Log      logr.Logger
@@ -42,7 +46,7 @@ type Reconciler struct {
 	client.Client
 }
 
-func (r *Reconciler) getRestConfig(ctx context.Context, secretRef v1alpha1.RemoteClusterSecretRef) (*rest.Config, error) {
+func (r *Reconciler) getRestConfig(ctx context.Context, secretRef v1alpha1.RemoteClusterSecretRef) (clientcmd.ClientConfig, error) {
 	var secret corev1.Secret
 	err := r.Client.Get(ctx, types.NamespacedName{
 		Namespace: secretRef.Namespace,
@@ -59,7 +63,7 @@ func (r *Reconciler) getRestConfig(ctx context.Context, secretRef v1alpha1.Remot
 		return nil, errors.Wrap(err, "load kubeconfig")
 	}
 
-	return clientcmd.NewDefaultClientConfig(*config, nil).ClientConfig()
+	return clientcmd.NewDefaultClientConfig(*config, nil), nil
 }
 
 func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
@@ -75,6 +79,13 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 		return ctrl.Result{}, nil
 	}
 
+	clientConfig, err := r.getRestConfig(ctx, obj.Spec.KubeConfig.SecretRef)
+	if err != nil {
+		r.Log.Error(err, "fail to get clientConfig from secret")
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	// if the remoteCluster itself is being deleted, we should remove the cluster controller manager
 	if !obj.DeletionTimestamp.IsZero() {
 		err := r.registry.Stop(ctx, obj.Name)
 		if err != nil {
@@ -84,9 +95,20 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 			}
 		}
 
+		err = r.uninstallHelmRelease(ctx, &obj, clientConfig)
+		if err != nil {
+			r.Log.Error(err, "fail to uninstall helm release")
+			return ctrl.Result{Requeue: true}, nil
+		}
+
 		err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
-			obj.Finalizers = []string{}
-			return r.Client.Update(ctx, &obj)
+			var newObj v1alpha1.RemoteCluster
+			r.Client.Get(ctx, req.NamespacedName, &newObj)
+
+			newObj.Finalizers = []string{}
+			setRemoteClusterCondition(&newObj, v1alpha1.RemoteClusterConditionInstalled, corev1.ConditionFalse, "")
+
+			return r.Client.Update(ctx, &newObj)
 		})
 		if err != nil {
 			r.Log.Error(err, "fail to update finalizer", "name", obj.Name)
@@ -95,23 +117,33 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 		return ctrl.Result{}, nil
 	}
 
-	restConfig, err := r.getRestConfig(ctx, obj.Spec.KubeConfig.SecretRef)
+	err = r.ensureHelmRelease(ctx, &obj, clientConfig)
 	if err != nil {
-		r.Log.Error(err, "fail to get rest config")
+		r.Log.Error(err, "fail to list or install remote helm release")
 		return ctrl.Result{Requeue: true}, nil
 	}
 
-	err = r.registry.Spawn(obj.Name, restConfig)
+	err = r.ensureClusterControllerManager(ctx, &obj, clientConfig)
 	if err != nil {
-		if !errors.Is(err, clusterregistry.ErrAlreadyExist) {
-			r.Log.Error(err, "fail to spawn controllers", "name", obj.Name)
-			return ctrl.Result{Requeue: true}, nil
-		}
+		r.Log.Error(err, "fail to boot remote cluster controller manager")
+		return ctrl.Result{Requeue: true}, nil
+	}
+	obj.Finalizers = []string{remoteClusterControllerFinalizer}
+
+	if err != nil {
+		r.Log.Error(err, "fail to operate the helm release in remote cluster")
+		return ctrl.Result{Requeue: true}, nil
 	}
 
 	err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		obj.Finalizers = []string{remoteClusterControllerFinalizer}
-		return r.Client.Update(ctx, &obj)
+		var newObj v1alpha1.RemoteCluster
+		r.Client.Get(ctx, req.NamespacedName, &newObj)
+
+		newObj.Finalizers = obj.Finalizers
+		setRemoteClusterCondition(&newObj, v1alpha1.RemoteClusterConditionInstalled, corev1.ConditionTrue, "")
+		// TODO: do auto config migration
+		newObj.Status.CurrentVersion = chaosMeshReleaseVersion
+		return r.Client.Update(ctx, &newObj)
 	})
 	if err != nil {
 		r.Log.Error(err, "fail to update finalizer", "name", obj.Name)
@@ -119,4 +151,91 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *Reconciler) ensureClusterControllerManager(ctx context.Context, obj *v1alpha1.RemoteCluster, config clientcmd.ClientConfig) error {
+	restConfig, err := config.ClientConfig()
+	if err != nil {
+		return errors.Wrap(err, "get rest config from client config")
+	}
+
+	err = r.registry.Spawn(obj.Name, restConfig)
+	if err != nil {
+		if !errors.Is(err, clusterregistry.ErrAlreadyExist) {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *Reconciler) getHelmClient(ctx context.Context, clientConfig clientcmd.ClientConfig) (*helm.HelmClient, error) {
+	restClientGetter := helm.NewRESTClientGetter(clientConfig)
+
+	helmClient, err := helm.NewHelmClient(restClientGetter, r.Log)
+	if err != nil {
+		return nil, err
+	}
+
+	return helmClient, nil
+}
+
+func (r *Reconciler) ensureHelmRelease(ctx context.Context, obj *v1alpha1.RemoteCluster, clientConfig clientcmd.ClientConfig) error {
+	helmClient, err := r.getHelmClient(ctx, clientConfig)
+	if err != nil {
+		return err
+	}
+
+	_, err = helmClient.GetRelease(obj.Spec.Namespace, chaosMeshReleaseName)
+	if err != nil {
+		if errors.Is(err, driver.ErrReleaseNotFound) {
+			chart, err := helm.FetchChaosMeshChart(ctx, chaosMeshReleaseVersion)
+			if err != nil {
+				return err
+			}
+
+			values := make(map[string]interface{})
+			if obj.Spec.ConfigOverride != nil {
+				err = json.Unmarshal(obj.Spec.ConfigOverride, &values)
+				if err != nil {
+					return err
+				}
+			}
+			_, err = helmClient.UpgradeOrInstall(obj.Spec.Namespace, chaosMeshReleaseName, chart, values)
+			if err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *Reconciler) uninstallHelmRelease(ctx context.Context, obj *v1alpha1.RemoteCluster, clientConfig clientcmd.ClientConfig) error {
+	helmClient, err := r.getHelmClient(ctx, clientConfig)
+	if err != nil {
+		return err
+	}
+
+	_, err = helmClient.GetRelease(obj.Spec.Namespace, chaosMeshReleaseName)
+	if err != nil {
+		if errors.Is(err, driver.ErrReleaseNotFound) {
+			return nil
+		}
+
+		return err
+	}
+
+	// the release still exist
+	_, err = helmClient.UninstallRelease(obj.Spec.Namespace, chaosMeshReleaseName)
+	if err != nil {
+		if errors.Is(err, driver.ErrReleaseNotFound) {
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
 }
