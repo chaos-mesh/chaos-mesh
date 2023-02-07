ea19ed8673546bc07ce3221aa47b21b66a5da1c0
andrewmatilde <davis6813585853062@outlook.com>
2023-02-07 11:38:19 +0800

    update to master
    Signed-off-by: andrewmatilde <davis6813585853062@outlook.com>

--- api/v1alpha1/diskchaos_types.go
+++ api/v1alpha1/diskchaos_types.go
@@ -53,21 +53,32 @@ type DiskChaosSpec struct {
 	// +kubebuilder:validation:Enum=fill;write;read
 	Action DiskChaosAction `json:"action" webhook:"DiskAction"`
 
-	Path    string `json:"path,omitempty"`
-	Size    string `json:"size,omitempty"`
+	// 'path' specifies the location to fill data in.
+	// If path not provided, a temp file will be generated and deleted immediately after data filled in or allocated.
+	// +optional
+	Path string `json:"path,omitempty"`
+	// 'size' specifies how many units of data will fill in the file path.
+	// 'unit' specifies the unit of data, support c=1, w=2, b=512, kB=1000, K=1024, MB=1000*1000, M=1024*1024,
+	// GB=1000*1000*1000, G=1024*1024*1024 BYTESexample : 1M | 512kB
+	// One of percent and size must not be empty when action is `fill`.
+	// +optional
+	Size string `json:"size,omitempty"`
+	// 'percent' how many percent data of disk will fill in the file path.
 	Percent string `json:"percent,omitempty"`
 
 	// SpaceLockSize keeps a part of disk space before disk chaos execute and
 	// delete at first when we recover the disk chaos.
 	SpaceLockSize string `json:"space_lock_size,omitempty"`
 
+	// Fill disk by fallocate instead of dd
+	// +kubebuilder:default=true
 	FillByFAllocate bool `json:"fill_by_fallocate,omitempty"`
-
+	// 'process-num' specifies the number of process work on reading, only 1-255 is valid value.
 	ProcessNum uint8 `json:"process_num,omitempty" webhook:"ProcessNum"`
 	// Not implement.
 	LoopExecution bool `json:"loop_execution,omitempty"`
 
-	// Duration represents the duration of the chaos action
+	// Duration represents the duration of the chaos action.
 	Duration *string `json:"duration,omitempty"`
 }
 
