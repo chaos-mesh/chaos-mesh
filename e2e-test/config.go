8f20f5d8499491ece61377728b72d12f9083c9d8
Yue Yang <g1enyy0ung@gmail.com>
2023-03-09 18:21:49 +0800

    Rename ChaosDNSImage to ChaosCoreDNSImage
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- e2e-test/config.go
+++ e2e-test/config.go
@@ -28,7 +28,7 @@ type Config struct {
 	DaemonTag            string
 	E2EImage             string
 	PauseImage           string
-	ChaosDNSImage        string
+	ChaosCoreDNSImage    string
 	InstallChaosMesh     bool
 	EnableDashboard      bool
 }
@@ -46,7 +46,7 @@ func NewDefaultConfig() *Config {
 		DaemonImage:          "chaos-mesh/chaos-daemon",
 		DaemonTag:            "latest",
 		E2EImage:             "ghcr.io/chaos-mesh/e2e-helper:latest",
-		ChaosDNSImage:        "ghcr.io/chaos-mesh/chaos-coredns:latest",
+		ChaosCoreDNSImage:    "ghcr.io/chaos-mesh/chaos-coredns:latest",
 		PauseImage:           "gcr.io/google-containers/pause:latest",
 		InstallChaosMesh:     false,
 		EnableDashboard:      false,
