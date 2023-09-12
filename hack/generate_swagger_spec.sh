aaa436bfd436efdf0c412602a81c8fe0ab4defdd
Matthias <5011972+fasmat@users.noreply.github.com>
2023-09-12 17:25:01 +0000

    Update swag dependency
    Signed-off-by: Matthias <5011972+fasmat@users.noreply.github.com>

--- hack/generate_swagger_spec.sh
+++ hack/generate_swagger_spec.sh
@@ -29,7 +29,7 @@ export GOBIN=$PROJECT_DIR/bin
 export PATH=$GOBIN:$PATH
 
 echo "+ Install swaggo/swag"
-go install github.com/swaggo/swag/cmd/swag
+go install github.com/swaggo/swag/cmd/swag@v1.16.2
 
 echo "+ Generate swagger spec"
 swag init -g cmd/chaos-dashboard/main.go --output pkg/dashboard/swaggerdocs --pd --parseInternal
