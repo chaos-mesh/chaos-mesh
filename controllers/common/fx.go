05c0b5963f70da5d65f7a60ff3751d3fe7db6424
Yue Yang <g1enyy0ung@gmail.com>
2024-03-05 12:06:25 +0800

    chore: update
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- controllers/common/fx.go
+++ controllers/common/fx.go
@@ -81,55 +81,54 @@ func Bootstrap(params Params) error {
 
 		// Add owning resources
 		if len(pair.Controlls) > 0 {
-			pair := pair
 			for _, obj := range pair.Controlls {
-				builder.Watches(obj,
-					handler.EnqueueRequestsFromMapFunc(func(ctx context.Context, obj client.Object) []reconcile.Request {
-						reqs := []reconcile.Request{}
-						objName := k8sTypes.NamespacedName{
-							Namespace: obj.GetNamespace(),
-							Name:      obj.GetName(),
-						}
+				builder.Watches(obj, handler.EnqueueRequestsFromMapFunc(func(ctx context.Context, obj client.Object) []reconcile.Request {
+					reqs := []reconcile.Request{}
+					objName := k8sTypes.NamespacedName{
+						Namespace: obj.GetNamespace(),
+						Name:      obj.GetName(),
+					}
+					kind := obj.GetObjectKind().GroupVersionKind().Kind
 
-						list := pair.ObjectList.DeepCopyList()
-						err := kubeclient.List(context.TODO(), list)
-						if err != nil {
-							setupLog.Error(err, "fail to list object")
-						}
+					list := pair.ObjectList.DeepCopyList()
+					err := kubeclient.List(context.TODO(), list)
+					if err != nil {
+						setupLog.Error(err, "fail to list object")
+					}
 
-						items := reflect.ValueOf(list).Elem().FieldByName("Items")
-						for i := 0; i < items.Len(); i++ {
-							item := items.Index(i).Addr().Interface().(v1alpha1.InnerObjectWithSelector)
+					items := reflect.ValueOf(list).Elem().FieldByName("Items")
+					for i := 0; i < items.Len(); i++ {
+						item := items.Index(i).Addr().Interface().(v1alpha1.InnerObjectWithSelector)
 
-							for _, record := range item.GetStatus().Experiment.Records {
-								namespacedName, containerName, err := controller.ParseNamespacedNameContainer(record.Id)
-								if err != nil {
-									setupLog.Error(err, "failed to parse record", "record", record.Id)
-									continue
+						for _, record := range item.GetStatus().Experiment.Records {
+							namespacedName, containerName, err := controller.ParseNamespacedNameContainer(record.Id)
+							if err != nil {
+								setupLog.Error(err, "failed to parse record", "record", record.Id)
+								continue
+							}
+
+							objNameString := objName.String()
+							namespacedNameString := namespacedName.String()
+							namespacedNameContainer := k8sTypes.NamespacedName{
+								Namespace: namespacedName.Namespace,
+								Name:      namespacedName.Name + "-" + containerName,
+							}
+							namespacedNameContainerString := namespacedNameContainer.String()
+
+							if namespacedNameString == objNameString || (kind == "PodIOChaos" && namespacedNameContainerString == objNameString) {
+								id := k8sTypes.NamespacedName{
+									Namespace: item.GetNamespace(),
+									Name:      item.GetName(),
 								}
-
-								objNameString := objName.String()
-								namespacedNameString := namespacedName.String()
-								namespacedNameContainer := k8sTypes.NamespacedName{
-									Namespace: namespacedName.Namespace,
-									Name:      namespacedName.Name + "-" + containerName,
-								}
-								namespacedNameContainerString := namespacedNameContainer.String()
-
-								if namespacedNameString == objNameString || namespacedNameContainerString == objNameString {
-									id := k8sTypes.NamespacedName{
-										Namespace: item.GetNamespace(),
-										Name:      item.GetName(),
-									}
-									setupLog.Info("mapping requests", "source", objName, "target", id)
-									reqs = append(reqs, reconcile.Request{
-										NamespacedName: id,
-									})
-								}
+								setupLog.Info("mapping requests", "source", objName, "target", id)
+								reqs = append(reqs, reconcile.Request{
+									NamespacedName: id,
+								})
 							}
 						}
-						return reqs
-					}),
+					}
+					return reqs
+				}),
 				)
 			}
 			predicaters = append(predicaters, PickChildCRDPredicate{})
