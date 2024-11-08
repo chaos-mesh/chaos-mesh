269528a6da3ca3872f14e313b914931372850e21
STRRL <im@strrl.dev>
2024-11-07 21:23:00 -0800

    feat: complete oidc exchange with custom CA
    Signed-off-by: STRRL <im@strrl.dev>

--- pkg/dashboard/apiserver/auth/oidc/service.go
+++ pkg/dashboard/apiserver/auth/oidc/service.go
@@ -16,16 +16,21 @@
 package oidc
 
 import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"path"
-
-	"github.com/gin-gonic/gin"
-	"github.com/go-logr/logr"
-	"golang.org/x/oauth2"
 
 	config "github.com/chaos-mesh/chaos-mesh/pkg/config"
 	"github.com/chaos-mesh/chaos-mesh/pkg/dashboard/apiserver/utils"
+	gooidc "github.com/coreos/go-oidc/v3/oidc"
+	"github.com/gin-gonic/gin"
+	"github.com/go-logr/logr"
+	"golang.org/x/oauth2"
 )
 
 type Service struct {
@@ -34,6 +39,7 @@ type Service struct {
 	clientSecret string
 	rootUrl      *url.URL
 	logger       logr.Logger
+	caPEMBytes   []byte
 }
 
 // NewService returns an experiment service instance.
@@ -48,6 +54,20 @@ func NewService(
 	if rootUrl.Path == "" {
 		rootUrl.Path = "/"
 	}
+	var caPEMBytes []byte
+	caFilePath := conf.OidcCaFile
+	if caFilePath != "" {
+		caFile, err := os.Open(caFilePath)
+		if err != nil {
+			return nil, err
+		}
+		caFileBytes, err := io.ReadAll(caFile)
+		if err != nil {
+			return nil, err
+		}
+		caPEMBytes = caFileBytes
+		caFile.Close()
+	}
 
 	return &Service{
 		clientId:     conf.OidcClientId,
@@ -55,6 +75,7 @@ func NewService(
 		issuerUrl:    conf.OidcIssuerURL,
 		rootUrl:      rootUrl,
 		logger:       logger.WithName("oidc auth api"),
+		caPEMBytes:   caPEMBytes,
 	}, nil
 }
 
@@ -73,22 +94,35 @@ func Register(r *gin.RouterGroup, s *Service, conf *config.ChaosDashboardConfig)
 }
 
 func (s *Service) getOauthConfig(c *gin.Context) oauth2.Config {
+	ctx := c.Request.Context()
 	url := *s.rootUrl
 	url.Path = path.Join(s.rootUrl.Path, "./api/auth/oidc/callback")
 
-	var Endpoint = oauth2.Endpoint{
-		AuthURL:  s.issuerUrl + "/oauth2/authorize",
-		TokenURL: s.issuerUrl + "/oauth2/token",
+	if s.caPEMBytes != nil {
+		// load self-signed CA
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(s.caPEMBytes)
+		trustSelfSignedCAHTTPClient := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{RootCAs: caCertPool},
+			},
+		}
+		ctx = gooidc.ClientContext(ctx, trustSelfSignedCAHTTPClient)
 	}
 
+	provider, err := gooidc.NewProvider(ctx, s.issuerUrl)
+	if err != nil {
+		s.logger.Error(err, "failed to create oidc provider")
+	}
+	endpoint := provider.Endpoint()
 	return oauth2.Config{
 		ClientID:     s.clientId,
 		ClientSecret: s.clientSecret,
 		RedirectURL:  url.String(),
 		Scopes: []string{
-			"openid",
+			gooidc.ScopeOpenID,
 		},
-		Endpoint: Endpoint,
+		Endpoint: endpoint,
 	}
 }
 
@@ -105,6 +139,19 @@ func (s *Service) authCallback(c *gin.Context) {
 	ctx := c.Request.Context()
 
 	oauth := s.getOauthConfig(c)
+
+	if s.caPEMBytes != nil {
+		// load self-signed CA
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(s.caPEMBytes)
+		trustSelfSignedCAHTTPClient := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{RootCAs: caCertPool},
+			},
+		}
+		ctx = context.WithValue(ctx, oauth2.HTTPClient, trustSelfSignedCAHTTPClient)
+	}
+
 	oauth2Token, err := oauth.Exchange(ctx, c.Request.URL.Query().Get("code"), oauth2.AccessTypeOffline, oauth2.ApprovalForce)
 	if err != nil {
 		utils.SetAPIError(c, utils.ErrInternalServer.WrapWithNoMessage(err))
